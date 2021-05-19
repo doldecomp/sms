dba8d99baf4b2d6e126cc73772193c6466c9d4a7
farisawan-2000 <farisawan.2000@gmail.com>
2021-05-18 22:55:17 -0400

    Decompile NpcInitActionData, clean up a bit of NpcBalloon.hpp

---
+++ src/NPC/NpcBase.hpp
@@ -0,0 +1,16 @@
+#ifndef NPCBASE_HPP
+#define NPCBASE_HPP
+
+#include "types.h"
+
+class TBaseNPC {
+	void setKinoActionFlag_();
+	void setMareActionFlag_();
+	void setMonteActionFlag_();
+
+	char pad_000[0x16C];
+	s32 _16C;
+	u32 mActionFlag;
+};
+
+#endif
