d015e3068b9446bb5bdcd59af2c8ac15dca85465
RaichuBender <t-bender@hotmail.com>
2021-05-23 01:43:17 +0200

    Decompiled JDRResolution

--- src/JSystem/JDrama.hpp
+++ src/JSystem/JDrama.hpp
@@ -4,11 +4,10 @@
 namespace JDrama
 {
 //* JDRRenderMode:
-	/* TODO */bool IsEqualRenderModeVIParams(const GXRenderModeObj &, const GXRenderModeObj &);
-	/* TODO */bool IsRenderModeHalfAspectRatio(const GXRenderModeObj &);
-	/* TODO */f32 GetRenderModeYScale(const GXRenderModeObj &rmo);
-	/* TODO */void CalcRenderModeXFBHeight(GXRenderModeObj *rmo, u16);
-
+	bool IsEqualRenderModeVIParams(const GXRenderModeObj &, const GXRenderModeObj &);
+	bool IsRenderModeHalfAspectRatio(const GXRenderModeObj &);
+	f32 GetRenderModeYScale(const GXRenderModeObj &rmo);
+	void CalcRenderModeXFBHeight(GXRenderModeObj *rmo, u16);
 	void CalcRenderModeVIXOrigin(GXRenderModeObj *rmo);
 	void CalcRenderModeVIYOrigin(GXRenderModeObj *rmo);
 	void CopyRenderModeSamplePattern(GXRenderModeObj *rmo, const u8 (*s)[2]);
