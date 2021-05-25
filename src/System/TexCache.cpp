cf62bbf7067b228a288eeca4a9aca65088e3f237
farisawan-2000 <farisawan.2000@gmail.com>
2021-05-25 00:58:58 -0400

    more tab-space conversions

--- src/System/TexCache.cpp
+++ src/System/TexCache.cpp
@@ -4,6 +4,6 @@
 #include "JSystem/J3DSys.hpp"
 
 void SMS_ResetTexCacheRegion() {
-	j3dSys._034 |= 0x80000000;
-	j3dSys.setTexCacheRegion(GX_TEXCACHE_128K);
+    j3dSys._034 |= 0x80000000;
+    j3dSys.setTexCacheRegion(GX_TEXCACHE_128K);
 }
