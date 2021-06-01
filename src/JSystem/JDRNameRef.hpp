faa826fe7fcb29f9da9193ba696b0e0b2d38a609
QbeRoot <qbe.root@gmail.com>
2021-06-01 14:42:14 +0200

    JDRNameRef header

---
+++ src/JSystem/JDRNameRef.hpp
@@ -0,0 +1,28 @@
+#ifndef JDRNAMEREF_HPP
+#define JDRNAMEREF_HPP
+
+#include <dolphin.h>
+
+#include "JSystem/JSUMemoryStream.hpp"
+
+namespace JDrama {
+    class TNameRef {
+        public:
+            static void calcKeyCode(char const *);
+            static void getType(JSUMemoryInputStream &, JSUMemoryInputStream &);
+            static void genObject(JSUMemoryInputStream &, JSUMemoryInputStream &);
+
+            virtual ~TNameRef();
+            virtual void getType() const;
+            virtual void load(JSUMemoryInputStream &);
+            virtual void save(JSUMemoryOutputStream &);
+            virtual void loadAfter();
+            virtual void searchF(u16, char const *);
+        
+        private:
+            char *mName;
+            u16 mKeyCode;
+    };
+}
+
+#endif
