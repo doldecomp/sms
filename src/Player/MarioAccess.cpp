bee5a0976376202881659e0fbfed8965d8cea9bd
Faris Awan <farisawan.2000@gmail.com>
2021-11-25 03:12:32 -0500

    [MarioAccess.cpp] OK

--- src/Player/MarioAccess.cpp
+++ src/Player/MarioAccess.cpp
@@ -3,10 +3,22 @@
 #include "Player/MarioMain.hpp"
 #include "JSystem/JGeometry.hpp"
 
+// TODO: Move TLiveActor to correct header, then replace this with said header
+#include "types.h"
+
+TMario *gpMarioAddress;
+JGeometry::TVec3<float> *gpMarioPos;
+s16 *gpMarioAngleX, *gpMarioAngleY, *gpMarioAngleZ;
+f32 *gpMarioSpeedX, *gpMarioSpeedY, *gpMarioSpeedZ;
+u16 *gpMarioLightID;
+u32 *gpMarioFlag;
+f32 *gpMarioThrowPower;
+void **gpMarioGroundPlane; // TODO: TBGCheckData**
+
 bool SMS_IsMarioRoofing(void) {
     bool ret;
 
-    u32 val = gpMarioOriginal->_07C & 0x1FF;
+    u32 val = gpMarioOriginal->status & 0x1FF;
 
     if (val >= 0x147 and val <= 0x14A) {
         ret = true;
@@ -20,7 +32,7 @@ bool SMS_IsMarioRoofing(void) {
 bool SMS_IsMarioFencing(void) {
     bool ret;
 
-    u32 val = gpMarioOriginal->_07C & 0x1FF;
+    u32 val = gpMarioOriginal->status & 0x1FF;
 
     if (val >= 0x168 and val <= 0x16C) {
         ret = true;
@@ -32,23 +44,23 @@ bool SMS_IsMarioFencing(void) {
 }
 
 u32 SMS_GetMarioStatus(THitActor *actor) {
-    return ((TMario*) actor)->_07C;
+    return ((TMario*) actor)->status;
 }
 
 void *SMS_GetMarioRfPlane(void) {
-    return gpMarioOriginal->_0DC;
+    return gpMarioOriginal->ceil;
 }
 
 void *SMS_GetMarioWlPlane(void) {
-    return gpMarioOriginal->_0D8;
+    return gpMarioOriginal->wall;
 }
 
 void *SMS_GetMarioGrPlane(void) {
-    return gpMarioOriginal->_0E0;
+    return gpMarioOriginal->floor;
 }
 
 u32 SMS_GetMarioStatus(void) {
-    return gpMarioOriginal->_07C;
+    return gpMarioOriginal->status;
 }
 
 void SMS_WindMoveMario(const JGeometry::TVec3<float>& vec) {
@@ -103,7 +115,7 @@ u32 SMS_IsMarioOnYoshi() {
 }
 
 bool SMS_IsMarioOpeningDoor() {
-    if (gpMarioOriginal->_07C == 0x1320 || gpMarioOriginal->_07C == 0x1321) {
+    if (gpMarioOriginal->status == 0x1320 || gpMarioOriginal->status == 0x1321) {
         return true;
     } else {
         return false;
@@ -140,7 +152,7 @@ asm u8 SMS_IsMarioOnWire() {
 u8 SMS_IsMarioTouchGround4cm() {
     u8 ret;
 
-    if (gpMarioOriginal->position.y <= 4.0f + gpMarioOriginal->_0EC) {
+    if (gpMarioOriginal->position.y <= 4.0f + gpMarioOriginal->floorPosition.y) {
         ret = 1;
     } else {
         ret = 0;
@@ -153,4 +165,140 @@ u8 SMS_IsMarioTouchGround4cm() {
 
 void SMS_ThrowMario(const JGeometry::TVec3<float>& vec, float f) {
     gpMarioOriginal->throwMario(vec, f);
+}
+
+#pragma push
+asm void SMS_SendMessageToMario(THitActor *m, u32 mesg) {
+    nofralloc
+    /* 80152BB4 0014FAF4  7C 08 02 A6 */    mflr r0
+    /* 80152BB8 0014FAF8  7C 85 23 78 */    mr r5, r4
+    /* 80152BBC 0014FAFC  90 01 00 04 */    stw r0, 4(r1)
+    /* 80152BC0 0014FB00  7C 60 1B 78 */    mr r0, r3
+    /* 80152BC4 0014FB04  7C 04 03 78 */    mr r4, r0
+    /* 80152BC8 0014FB08  94 21 FF F8 */    stwu r1, -8(r1)
+    /* 80152BCC 0014FB0C  80 6D 98 B8 */    lwz r3, gpMarioOriginal(r13)
+    /* 80152BD0 0014FB10  81 83 00 00 */    lwz r12, 0(r3)
+    /* 80152BD4 0014FB14  81 8C 00 A0 */    lwz r12, 0xa0(r12)
+    /* 80152BD8 0014FB18  7D 88 03 A6 */    mtlr r12
+    /* 80152BDC 0014FB1C  4E 80 00 21 */    blrl 
+    /* 80152BE0 0014FB20  2C 03 00 00 */    cmpwi r3, 0
+    /* 80152BE4 0014FB24  41 82 00 0C */    beq lbl_80152BF0
+    /* 80152BE8 0014FB28  38 60 00 01 */    li r3, 1
+    /* 80152BEC 0014FB2C  48 00 00 08 */    b lbl_80152BF4
+    lbl_80152BF0:
+    /* 80152BF0 0014FB30  38 60 00 00 */    li r3, 0
+    lbl_80152BF4:
+    /* 80152BF4 0014FB34  80 01 00 0C */    lwz r0, 0xc(r1)
+    /* 80152BF8 0014FB38  38 21 00 08 */    addi r1, r1, 8
+    /* 80152BFC 0014FB3C  7C 08 03 A6 */    mtlr r0
+    /* 80152C00 0014FB40  4E 80 00 20 */    blr 
+}
+#pragma pop
+
+void *SMS_GetMarioWaterGun() { // TODO: returns TWaterGun
+    return gpMarioOriginal->waterGun;
+}
+
+f32 SMS_GetMarioGravity() {
+    return gpMarioOriginal->jumpParams.gravity.value;
+}
+
+f32 SMS_GetMarioGrLevel() {
+    return gpMarioOriginal->floorPosition.y;
+}
+
+f32 SMS_GetMarioDamageRadius() {
+    return gpMarioOriginal->damageRadius;
+}
+
+s16 SMS_GetMarioHP() {
+    return gpMarioOriginal->health;
+}
+
+bool SMS_IsMarioHeadSlideAttack() {
+    if (gpMarioOriginal->status == 0x00800456 || gpMarioOriginal->status == 0x0080088A) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+bool SMS_IsMarioStatusElecDamage() {
+    if (gpMarioOriginal->status == 0x00020338) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+bool SMS_IsMarioStatusThrownDown() {
+    if (gpMarioOriginal->status == 0x000208B8) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+bool SMS_IsMarioStatusHipDrop() {
+    if (gpMarioOriginal->status == 0x008008A9) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+bool SMS_IsMarioStatusTypeSwimming() {
+    if (gpMarioOriginal->status & (1 << 13)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+bool SMS_IsMarioStatusTypeJumping() {
+    if (gpMarioOriginal->status & (1 << 11)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+TLiveActor *SMS_GetMarioLiveActor() {
+    return (TLiveActor *) gpMarioAddress;
+}
+
+THitActor *SMS_GetMarioHitActor() {
+    return (THitActor *) gpMarioAddress;
+}
+
+u32 SMS_AskJumpIntoWaterEffectExist() {
+    gpMarioOriginal->askJumpIntoWaterEffectExist();
+}
+
+void *SMS_GetYoshi() { // TODO: returns TYoshi
+    return gpMarioOriginal->yoshi;
+}
+
+void SMS_SetMarioAccessParams() {
+    s16 *angle;
+    f32 *speed;
+
+    gpMarioAddress = gpMarioOriginal;
+    gpMarioPos = &gpMarioOriginal->position;
+
+    angle = &gpMarioOriginal->angle.x;
+    gpMarioAngleX = angle;
+    gpMarioAngleY = angle + 1;
+    gpMarioAngleZ = angle + 2;
+
+    speed = &gpMarioOriginal->speed.x;
+    gpMarioSpeedX = speed;
+    gpMarioSpeedY = speed + 1;
+    gpMarioSpeedZ = speed + 2;
+
+    gpMarioLightID = &gpMarioOriginal->lightID;
+    gpMarioFlag = &gpMarioOriginal->_118;
+
+    gpMarioThrowPower = &gpMarioOriginal->deParams.throwPower.value;
+    gpMarioGroundPlane = &gpMarioOriginal->floor;
 }
---
+++ src/Player/MarioInit.hpp
@@ -0,0 +1,125 @@
+#ifndef MARIOINIT_HPP
+#define MARIOINIT_HPP
+
+#include "System/ParamInst.hpp"
+
+// TODO: move to a better file
+#include "System/BaseParam.hpp"
+class TParams {
+    public:
+        char *filename;
+        TBaseParam *head;
+};
+
+
+// length 0x42C
+class TDeParams : public TParams {
+    public:
+        TParamT<s16> hpMax;
+        TParamT<f32> runningMax;
+        TParamT<f32> dashMax;
+        TParamT<f32> dashAcc;
+        TParamT<f32> dashBrake;
+        TParamT<s16> dashStartTime;
+        TParamT<s16> waitingRotSp;
+        TParamT<s16> runningRotSpMin;
+        TParamT<s16> runningRotSpMax;
+        TParamT<s16> rocketRotSp;
+        TParamT<s16> pumpingRotSpMin;
+        TParamT<s16> pumpingRotSpMax;
+        TParamT<s16> invincibleTime;
+        TParamT<s16> footPrintTimerMax;
+        TParamT<u8> waterTriggerRate;
+        TParamT<u8> graffitoNoDmgTime;
+        TParamT<u8> restMax;
+        TParamT<f32> shadowSize;
+        TParamT<f32> shadowErase;
+        TParamT<f32> holdRadius;
+        TParamT<f32> damageRadius;
+        TParamT<f32> damageHeight;
+        TParamT<f32> attackHeight;
+        TParamT<f32> trampleRadius;
+        TParamT<f32> pushupRadius;
+        TParamT<f32> pushupHeight;
+        TParamT<f32> hipdropRadius;
+        TParamT<f32> quakeRadius;
+        TParamT<f32> quakeHeight;
+        TParamT<f32> tramplePowStep1;
+        TParamT<f32> tramplePowStep2;
+        TParamT<f32> tramplePowStep3;
+        TParamT<f32> jumpWallHeight;
+        TParamT<f32> throwPower;
+        TParamT<f32> slipStart;
+        TParamT<f32> wasOnWaterSlip;
+        TParamT<f32> inWaterSlip;
+        TParamT<f32> toroccoRotSp;
+        TParamT<s16> recoverTimer;
+        TParamT<s16> hotTimer;
+        TParamT<f32> feelDeep;
+        TParamT<f32> damageFallHeight;
+        TParamT<f32> forceSlipAngle;
+        TParamT<f32> clashSpeed;
+        TParamT<f32> hangWallMovableAngle;
+        TParamT<f32> colMvMax;
+        TParamT<s16> noFreezeTime;
+        TParamT<s16> kickFreezeTime;
+        TParamT<s16> surfStartFreezeTime;
+        TParamT<f32> sleepingCheckDist;
+        TParamT<f32> sleepingCheckHeight;
+        TParamT<s16> illegalPlaneCtInc;
+        TParamT<s16> illegalPlaneTime;
+};
+
+
+struct TJumpParams : TParams{ /* PlaceHolder Class Structure */
+    TParamT<f32> gravity;
+    TParamT<f32> spinJumpGravity;
+    TParamT<f32> jumpingMax;
+    TParamT<f32> jumpSpeedBrake;
+    TParamT<f32> jumpAccelControl;
+    TParamT<f32> jumpSlideControl;
+    TParamT<f32> turnJumpForce;
+    TParamT<f32> fenceSpeed;
+    TParamT<f32> fireDownForce;
+    TParamT<f32> fireDownControl;
+    TParamT<f32> fireBackVelocity;
+    TParamT<f32> broadJumpForce;
+    TParamT<f32> broadJumpForceY;
+    TParamT<f32> rotateJumpForceY;
+    TParamT<f32> popUpSpeedY;
+    TParamT<f32> popUpSpeedYMult;
+    TParamT<f32> backJumpForce;
+    TParamT<f32> backJumpForceY;
+    TParamT<f32> hipAttackSpeedY;
+    TParamT<f32> superHipAttackSpeedY;
+    TParamT<s16> jumpCatchRotXSp;
+    TParamT<s16> jumpCatchRotXMax;
+    TParamT<f32> rotBroadEnableV;
+    TParamT<f32> rotBroadJumpForce;
+    TParamT<f32> rotBroadJumpForceY;
+    TParamT<f32> trampolineDec;
+    TParamT<f32> secJumpEnableSp;
+    TParamT<f32> secJumpForce;
+    TParamT<f32> secJumpSpeedMult;
+    TParamT<f32> secJumpXZMult;
+    TParamT<f32> triJumpEnableSp;
+    TParamT<f32> ultraJumpForce;
+    TParamT<f32> ultraJumpSpeedMult;
+    TParamT<f32> ultraJumpXZMult;
+    TParamT<f32> valleyDepth;
+    TParamT<f32> thrownAccel;
+    TParamT<f32> thrownSlide;
+    TParamT<f32> thrownBrake;
+    TParamT<f32> tremblePower;
+    TParamT<f32> trembleAccele;
+    TParamT<f32> trembleBrake;
+    TParamT<s16> trembleTime;
+    TParamT<s16> clashAngle;
+    TParamT<f32> jumpJumpCatchSp;
+    TParamT<f32> getOffYoshiY;
+    TParamT<s16> superHipAttackCt;
+};
+
+
+
+#endif
