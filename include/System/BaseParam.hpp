eb374598b58a5ffe2f83c372dcf3989ba879eac1
TheGag96 <thegag96@gmail.com>
2023-01-07 23:11:23 -0600

    `TBaseParam`, `TParamT`: Fix to be more accurate
    `TBaseParam` has a virtual function, which gives it an extra word at
    offset `0` - it's not part of `TParamT`.

--- include/System/BaseParam.hpp
+++ include/System/BaseParam.hpp
@@ -4,9 +4,12 @@
 #include "dolphin.h"
 
 #include "types.h"
+#include "JSystem/JSUMemoryStream.hpp"
 
 class TBaseParam {
 public:
+	virtual void load(JSUMemoryInputStream&);
+
 	u16 keyCode;
 	char *name;
 	TBaseParam *next;
