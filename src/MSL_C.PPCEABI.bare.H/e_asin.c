69035bd60160c02712ffdeae55954649f657fb49
Roman Sandu <roma.sandu@gmail.com>
2024-12-22 22:51:52 +0300

    Match fdlibm & PPCArch.c
    fdlibm seems to be available in archives and already used in pikmin, so
    I've just grabbed the files from it used by sms. Note that fabs mangling
    seems to be screwed up in fdlibm TUs somehow and is c++-style even
    though the library itself is C, so for now we work around thata instead
    of trying to figure out what originally happened.
    
    Also cleaned up PPCArch.c to be at 100%.

---
+++ src/MSL_C.PPCEABI.bare.H/e_asin.c
@@ -0,0 +1,6 @@
+#include "MSL_C.PPCEABI.bare.H/fdlibm.h"
+
+f64 fabs__Fd(f64 v)
+{
+	return __fabs(v);
+}
--- src/base/PPCArch.c
+++ src/base/PPCArch.c
@@ -4,33 +4,12 @@
 asm u32 PPCMfmsr() {
     nofralloc
     mfmsr r3
-    blr    
+    blr
 }
 
 asm void PPCMtmsr(register u32 newMSR) {
     nofralloc
     mtmsr newMSR
-    blr
-}
-
-asm u32 PPCOrMsr(register u32 value) {
-    nofralloc
-    mfmsr r4
-    or value, r4, value
-    blr
-}
-
-asm u32 PPCAndMsr(register u32 value) {
-    nofralloc
-    mfmsr r4
-    and value, r4, value
-    blr
-}
-
-asm u32 PPCAndCMsr(register u32 value) {
-    nofralloc
-    mfmsr r4
-    andc value, r4, value
     blr
 }
 
@@ -43,12 +22,6 @@ asm u32 PPCMfhid0() {
 asm void PPCMthid0(register u32 newHID0) {
     nofralloc
     mtspr HID0, newHID0
-    blr
-}
-
-asm u32 PPCMfhid1() {
-    nofralloc
-    mfspr r3, HID1
     blr
 }
 
@@ -70,32 +43,9 @@ asm void PPCMtdec(register u32 newDec) {
     blr
 }
 
-asm u32 PPCMfdec() {
-    nofralloc
-    mfdec r3
-    blr
-}
-
 asm void PPCSync() {
     nofralloc
     sc
-    blr
-}
-
-asm void PPCEieio() {
-    nofralloc
-    mfmsr r5
-    rlwinm r6, r5, 0, 17, 15
-    mtmsr r6
-    mfspr r3, HID0
-    ori r4, r3, 0x8
-    mtspr HID0, r4
-    isync
-    eieio
-    isync
-    mtspr HID0, r3
-    mtmsr r5
-    isync
     blr
 }
 
@@ -107,90 +57,6 @@ loop:
     li r3, 0
     nop
     b loop
-}
-
-asm u32 PPCMfmmcr0() {
-    nofralloc
-    mfspr r3, MMCR0
-    blr
-}
-
-asm void PPCMtmmcr0(register u32 newMmcr0) {
-    nofralloc
-    mtspr MMCR0, newMmcr0
-    blr
-}
-
-asm u32 PPCMfmmcr1() {
-    nofralloc
-    mfspr r3, MMCR1
-    blr
-}
-
-asm void PPCMtmmcr1(register u32 newMmcr1) {
-    nofralloc
-    mtspr MMCR1, newMmcr1
-    blr
-}
-
-asm u32 PPCMfpmc1() {
-    nofralloc
-    mfspr r3, PMC1
-    blr
-}
-
-asm void PPCMtpmc1(register u32 newPmc1) {
-    nofralloc
-    mtspr PMC1, newPmc1
-    blr
-}
-
-asm u32 PPCMfpmc2() {
-    nofralloc
-    mfspr r3, PMC2
-    blr
-}
-
-asm void PPCMtpmc2(register u32 newPmc2) {
-    nofralloc
-    mtspr PMC2, newPmc2
-    blr
-}
-
-asm u32 PPCMfpmc3() {
-    nofralloc
-    mfspr r3, PMC3
-    blr
-}
-
-asm void PPCMtpmc3(register u32 newPmc3) {
-    nofralloc
-    mtspr PMC3, newPmc3
-    blr
-}
-
-asm u32 PPCMfpmc4() {
-    nofralloc
-    mfspr r3, PMC4
-    blr
-}
-
-asm void PPCMtpmc4(register u32 newPmc4) {
-    nofralloc
-    mtspr PMC4, newPmc4
-    blr
-}
-
-asm u32 PPCMfsia() {
-    nofralloc
-    mfspr r3, SIA
-    blr
-}
-
-asm void PPCMtsia(register u32 newSia) {
-    nofralloc
-    mtspr SIA, newSia
-    blr
 }
 
 asm u32 PPCMfhid2() {
@@ -205,45 +71,18 @@ asm void PPCMthid2(register u32 newhid2) {
     blr
 }
 
-asm u32 PPCMfwpar() {
-    nofralloc
-    sync
-    mfspr r3, WPAR
-    blr
-}
-
 asm void PPCMtwpar(register u32 newwpar) {
     nofralloc
     mtspr WPAR, newwpar
     blr
 }
 
-asm u32 PPCMfdmaU() {
-    nofralloc
-    mfspr r3, DMA_U
-    blr
+void PPCDisableSpeculation() {
+    PPCMthid0(PPCMfhid0() | 0x200);
 }
 
-asm u32 PPCMfdmaL() {
+asm void PPCSetFpNonIEEEMode() {
     nofralloc
-    mfspr r3, DMA_L
-    blr
-}
-
-asm void PPCMtdmaU(register u32 newdmau) {
-    nofralloc
-    mtspr DMA_U, newdmau
-    blr
-}
-
-asm void PPCMtdmaL(register u32 newdmal) {
-    nofralloc
-    mtspr DMA_L, newdmal
-    blr
-}
-
-asm u32 PPCMfpvr() {
-    nofralloc
-    mfspr r3, PVR
+    mtfsb1 29
     blr
 }
