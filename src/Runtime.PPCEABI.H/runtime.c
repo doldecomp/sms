3b577d79c0b4a6f01a80881d460082f6064ea225
Roman Sandu <roma.sandu@gmail.com>
2024-12-25 02:31:16 +0300

    test

--- src/Runtime.PPCEABI.H/runtime.c
+++ src/Runtime.PPCEABI.H/runtime.c
@@ -21,31 +21,31 @@ static const u32 __constants[] = {
 asm u32 __cvt_fp2unsigned(register f64 d)
 {
 #ifdef __MWERKS__ // clang-format off
-		nofralloc
-		stwu    r1,-16(r1)
-		lis     r4, __constants@h
-		ori     r4, r4, __constants@l
-		li      r3,0
-		lfd     fp0,0(r4)
-		lfd     fp3,8(r4)
-		lfd     fp4,16(r4)
-		fcmpu   cr0,fp1,fp0
-		fcmpu   cr6,fp1,fp3
-		blt     cr0, @exit
-		addi    r3,r3,-1
-		bge     cr6,@exit
-		fcmpu   cr7,fp1,fp4
-		fmr     fp2,fp1
-		blt     cr7,@1
-		fsub    fp2,fp1,fp4
+	nofralloc
+	stwu    r1,-16(r1)
+	lis     r4, __constants@h
+	ori     r4, r4, __constants@l
+	li      r3,0
+	lfd     fp0,0(r4)
+	lfd     fp3,8(r4)
+	lfd     fp4,16(r4)
+	fcmpu   cr0,fp1,fp0
+	fcmpu   cr6,fp1,fp3
+	blt     cr0, @exit
+	addi    r3,r3,-1
+	bge     cr6,@exit
+	fcmpu   cr7,fp1,fp4
+	fmr     fp2,fp1
+	blt     cr7,@1
+	fsub    fp2,fp1,fp4
 @1		fctiwz  fp2,fp2
-		stfd    fp2,8(r1)
-		lwz     r3,12(r1)
-		blt     cr7,@exit
-		addis   r3,r3,-0x8000
+	stfd    fp2,8(r1)
+	lwz     r3,12(r1)
+	blt     cr7,@exit
+	addis   r3,r3,-0x8000
 @exit:
-		addi    r1,r1,16
-		blr
+	addi    r1,r1,16
+	blr
 #endif // clang-format on
 }
 
