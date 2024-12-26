1a98189a6ae61fb646c921f970cae26aa1b957b8
Roman Sandu <roma.sandu@gmail.com>
2024-12-26 05:16:52 +0300

    Fix build

--- include/JSystem/JKernel/JKRStdHeap.hpp
+++ include/JSystem/JKernel/JKRStdHeap.hpp
@@ -19,15 +19,15 @@ public:
 	virtual u32 getHeapType() { return 'STDH'; }                   // _30
 	virtual s32 getTotalFreeSize() { return getFreeSize(); }       // _2C
 	virtual bool check() { return OSCheckHeap(mHeapId) != -1; }    // _34
-	virtual bool dump_sort();                                      // _38
+	virtual bool dump_sort() { return false; }                     // _38
 	virtual bool dump()                                            // _3C
 	{
 		OSDumpHeap(mHeapId);
 		return OSCheckHeap(mHeapId) != -1;
 	}
-	virtual s32 changeGroupID(u8 groupId);           // _40
-	virtual u8 getCurrentGroupId() { return 0; }     // _44 (weak)
-	virtual void state_register(TState*, u32) const; // _48
+	virtual s32 changeGroupID(u8 groupId) { return 0; } // _40
+	virtual u8 getCurrentGroupId() { return 0; }        // _44 (weak)
+	virtual void state_register(TState*, u32) const;    // _48
 	virtual bool state_compare(const TState& fst,
 	                           const TState& snd) const; // _4C
 
