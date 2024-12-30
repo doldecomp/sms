9160dc7505bbf682f0058b767ded49ff21d37bc7
Roman Sandu <roma.sandu@gmail.com>
2024-12-30 05:11:56 +0300

    File structure for JGadget -- decomping it too much of a pain

---
+++ include/JSystem/JGadget/list.hpp
@@ -0,0 +1,10 @@
+#ifndef JGADGET_LIST_HPP
+#define JGADGET_LIST_HPP
+
+namespace JGadget {
+
+template <typename T, typename Allocator> struct TList { };
+
+}; // namespace JGadget
+
+#endif
---
+++ include/JSystem/JGadget/vector.hpp
@@ -0,0 +1,12 @@
+#ifndef JGADGET_VECTOR_HPP
+#define JGADGET_VECTOR_HPP
+
+namespace JGadget {
+
+template <typename T, typename Allocator> class TVector { };
+
+class TVector_pointer_void { };
+
+} // namespace JGadget
+
+#endif
