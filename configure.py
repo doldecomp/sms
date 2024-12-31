4ca1b2cff3da9af9430dc02b1817521fc151c298
Roman Sandu <roma.sandu@gmail.com>
2024-12-31 18:32:37 +0300

    Match OSThread

--- configure.py
+++ configure.py
@@ -640,7 +640,7 @@ config.libs = [
             Object(Matching, "dolphin/os/OSRtc.c"),
             Object(Matching, "dolphin/os/OSStopwatch.c"),
             Object(Matching, "dolphin/os/OSSync.c"),
-            Object(NonMatching, "dolphin/os/OSThread.c"),
+            Object(Matching, "dolphin/os/OSThread.c"),
             Object(Matching, "dolphin/os/OSTime.c"),
             Object(NonMatching, "dolphin/os/__start.c"),
             Object(Matching, "dolphin/os/__ppc_eabi_init.cpp"),
