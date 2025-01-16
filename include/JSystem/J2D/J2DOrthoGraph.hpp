46fcc1b8ec30dc661bfbf9a29e20326a798d1ca5
Roman Sandu <roma.sandu@gmail.com>
2025-01-16 03:43:47 +0300

    Almost matched J2DOrthoGraph & J2DGrafContext

--- include/JSystem/J2D/J2DOrthoGraph.hpp
+++ include/JSystem/J2D/J2DOrthoGraph.hpp
@@ -6,19 +6,23 @@
 class J2DOrthoGraph : public J2DGrafContext {
 public:
 	J2DOrthoGraph();
-	J2DOrthoGraph(f32 x, f32 y, f32 width, f32 height, f32 far, f32 near);
+	J2DOrthoGraph(const JUTRect&);
+	J2DOrthoGraph(int x, int y, int width, int height);
 	void setOrtho(JGeometry::TBox2<f32> const& bounds, f32 far, f32 near);
-	void scissorBounds(JGeometry::TBox2<f32>*, JGeometry::TBox2<f32> const*);
+	void scissorBounds(JUTRect*, JUTRect*);
 
 	virtual ~J2DOrthoGraph() { }
 	virtual void setPort();
 	virtual s32 getGrafType() const { return 1; }
 	virtual void setLookat();
 
-	f32 getWidthPower() const { return mBounds.getWidth() / mOrtho.getWidth(); }
+	f32 getWidthPower() const
+	{
+		return (float)mBounds.getWidth() / (float)mOrtho.getWidth();
+	}
 	f32 getHeightPower() const
 	{
-		return mBounds.getHeight() / mOrtho.getHeight();
+		return (float)mBounds.getHeight() / (float)mOrtho.getHeight();
 	}
 
 	void setOrtho(f32 x, f32 y, f32 width, f32 height, f32 far, f32 near)
@@ -28,18 +32,17 @@ public:
 	}
 
 private:
-	/* 0xBC */ JGeometry::TBox2<f32> mOrtho;
-	/* 0xCC */ f32 mNear;
-	/* 0xD0 */ f32 mFar;
+	/* 0xD8 */ JUTRect mOrtho;
+	/* 0xE8 */ f32 mNear;
+	/* 0xEC */ f32 mFar;
 };
 
-void J2DDrawLine(f32 x1, f32 y1, f32 x2, f32 y2, JUtility::TColor color,
+void J2DDrawLine(int x1, int y1, int x2, int y2, JUtility::TColor color,
                  int line_width);
-void J2DFillBox(f32 x, f32 y, f32 width, f32 height, JUtility::TColor color);
-void J2DFillBox(JGeometry::TBox2<f32> const& box, JUtility::TColor color);
-void J2DDrawFrame(f32 x, f32 y, f32 width, f32 height, JUtility::TColor color,
+void J2DFillBox(int x, int y, int width, int height, JUtility::TColor color);
+void J2DFillBox(JUTRect box, JUtility::TColor color);
+void J2DDrawFrame(int x, int y, int width, int height, JUtility::TColor color,
                   u8 line_width);
-void J2DDrawFrame(JGeometry::TBox2<f32> const& box, JUtility::TColor color,
-                  u8 line_width);
+void J2DDrawFrame(JUTRect box, JUtility::TColor color, u8 line_width);
 
 #endif
--- include/JSystem/JUtility/JUTRect.hpp
+++ include/JSystem/JUtility/JUTRect.hpp
@@ -3,6 +3,11 @@
 
 class JUTRect {
 public:
+	JUTRect() { set(0, 0, 0, 0); }
+	JUTRect(int x1, int y1, int x2, int y2) { set(x1, y1, x2, y2); }
+
+	JUTRect(const JUTRect& other) { copy(other); }
+
 	void set(int, int, int, int);
 	void copy(const JUTRect&);
 	void add(int, int);
@@ -12,6 +17,8 @@ public:
 	void reform(int, int, int, int);
 	void normalize();
 	bool isEmpty() const;
+	int getWidth() const { return x2 - x1; }
+	int getHeight() const { return y2 - y1; }
 
 	int x1; // _00
 	int y1; // _04
