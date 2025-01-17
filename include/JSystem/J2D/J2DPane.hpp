cc7a2aa77a96f6b6633ba75aa757b876250dbe65
Roman Sandu <roma.sandu@gmail.com>
2025-01-17 04:08:22 +0300

    Cleanup J2DPane TWW leftovers

--- include/JSystem/J2D/J2DPane.hpp
+++ include/JSystem/J2D/J2DPane.hpp
@@ -95,10 +95,6 @@ public:
 	void setUserInfo(u64 info) { mUserInfoTag = info; }
 	const Mtx* getMtx() const { return &mPositionMtx; }
 
-	static s16 J2DCast_F32_to_S16(f32 value, u8 arg2);
-
-	static JGeometry::TBox2<f32> static_mBounds;
-
 public:
 	/* 0x4 */ u16 mInfoTag;
 	/* 0x8 */ int mKind;
