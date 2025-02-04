dc044a9bbffa9af9d2b50d9f03a0c94274461fc0
Roman Sandu <roma.sandu@gmail.com>
2025-02-05 01:02:00 +0300

    Bring gd up to date with dolsdk2001-dec

--- include/dolphin/gd/GDLight.h
+++ include/dolphin/gd/GDLight.h
@@ -11,20 +11,81 @@
 extern "C" {
 #endif
 
+#define XF_LIGHT_ID          0x0600
+#define XF_LIGHT_COLOR_ID    0x0603
+#define XF_LIGHT_ATTN_ID     0x0604
+#define XF_LIGHT_DISTATTN_ID 0x0607
+#define XF_LIGHT_POS_ID      0x060A
+#define XF_LIGHT_SPEC_DIR_ID 0x060A
+#define XF_LIGHT_DIR_ID      0x060D
+
+#define XF_REG_AMBIENT0_ID    0x100A
+#define XF_REG_AMBIENT1_ID    0x100B
+#define XF_REG_MATERIAL0_ID   0x100C
+#define XF_REG_MATERIAL1_ID   0x100D
+#define XF_REG_COLOR0CNTRL_ID 0x100E
+#define XF_REG_COLOR1CNTRL_ID 0x100F
+#define XF_REG_ALPHA0CNTRL_ID 0x1010
+#define XF_REG_ALPHA1CNTRL_ID 0x1011
+
+// clang-format off
+#define XF_REG_CHAN_CTRL(matsrc, enable, lightmask0, ambsrc, diff_fn, p5, p6, lightmask4) \
+	( \
+		(u32)(matsrc) << 0 | \
+		(u32)(enable) << 1 | \
+		(u32)(lightmask0) << 2 | \
+		(u32)(ambsrc) << 6 | \
+		(u32)(diff_fn) << 7 | \
+		(u32)(p5) << 9 | \
+		(u32)(p6) << 10 | \
+		(u32)(lightmask4) << 11 \
+	)
+// clang-format on
+
 void GDSetLightAttn(GXLightID light, float a0, float a1, float a2, float k0,
                     float k1, float k2);
 
-void GDSetLightColor(GXLightID light, GXColor* color);
+void GDSetLightSpot(GXLightID light, float cutoff, GXSpotFn spot_func);
+
+void GDSetLightDistAttn(GXLightID light, float ref_dist, float ref_br,
+                        GXDistAttnFn dist_func);
+
+void GDSetLightColor(GXLightID light, GXColor color);
 
 void GDSetLightPos(GXLightID light, float x, float y, float z);
 
 void GDSetLightDir(GXLightID light, float nx, float ny, float nz);
 
-void GDSetChanMatColor(GXChannelID chan, GXColor* color);
+void GDSetSpecularDirHA(GXLightID light, float nx, float ny, float nz, float hx,
+                        float hy, float hz);
+
+void GDSetSpecularDir(GXLightID light, float nx, float ny, float nz);
+
+void GDLoadLightObjIndx(u32 lt_obj_indx, GXLightID light);
+
+void GDSetChanAmbColor(GXChannelID chan, GXColor color);
+
+void GDSetChanMatColor(GXChannelID chan, GXColor color);
 
 void GDSetChanCtrl(GXChannelID chan, u8 enable, GXColorSrc amb_src,
                    GXColorSrc mat_src, u32 light_mask, GXDiffuseFn diff_fn,
                    GXAttnFn attn_fn);
+
+inline static u16 __GDLightID2Index(GXLightID id)
+{
+	u16 idx;
+
+	idx = 0x1F - __cntlzw(id);
+	if (idx > 7) {
+		idx = 0;
+	}
+	return idx;
+}
+
+inline static u16 __GDLightID2Offset(GXLightID id)
+{
+	return __GDLightID2Index(id) * 16;
+}
 
 #ifdef __cplusplus
 }
--- src/dolphin/gd/GDPixel.c
+++ src/dolphin/gd/GDPixel.c
@@ -1,21 +1,13 @@
 #include <dolphin/gd/GDPixel.h>
-#include "__gd.h"
+#include <dolphin/gd/GDBase.h>
 
 void GDSetBlendMode(GXBlendMode type, GXBlendFactor src_factor,
                     GXBlendFactor dst_factor, GXLogicOp logic_op)
 {
 	GDWriteBPCmd(0xfe001fe3);
-	// clang-format off
-	GDWriteBPCmd(
-		((u32)(type == GX_BM_BLEND || type == GX_BM_SUBTRACT)) |
-		((u32)(type == GX_BM_LOGIC)) << 1 |
-		((u32)dst_factor) << 5 |
-		((u32)src_factor) << 8 |
-		((u32)(type == GX_BM_SUBTRACT)) << 11 |
-		((u32)logic_op) << 12 |
-		((u32)0x41) << 24
-	);
-	// clang-format on
+	GDWriteBPCmd(BP_BLEND_MODE(
+	    type == GX_BM_BLEND || type == GX_BM_SUBTRACT, type == GX_BM_LOGIC, 0,
+	    0, 0, dst_factor, src_factor, type == GX_BM_SUBTRACT, logic_op, 0x41));
 }
 
 void GDSetBlendModeEtc(GXBlendMode type, GXBlendFactor src_factor,
@@ -23,42 +15,18 @@ void GDSetBlendModeEtc(GXBlendMode type, GXBlendFactor src_factor,
                        u8 color_update_enable, u8 alpha_update_enable,
                        u8 dither_enable)
 {
-	// clang-format off
-	GDWriteBPCmd(
-		((u32)(type == GX_BM_BLEND || type == GX_BM_SUBTRACT)) |
-		((u32)(type == GX_BM_LOGIC)) << 1 |
-		((u32)dither_enable) << 2 |
-		((u32)color_update_enable) << 3 |
-		((u32)alpha_update_enable) << 4 |
-		((u32)dst_factor) << 5 |
-		((u32)src_factor) << 8 |
-		((u32)(type == GX_BM_SUBTRACT)) << 11 |
-		((u32)logic_op) << 12 |
-		((u32)0x41) << 24
-	);
-	// clang-format on
+	GDWriteBPCmd(BP_BLEND_MODE(
+	    type == GX_BM_BLEND || type == GX_BM_SUBTRACT, type == GX_BM_LOGIC,
+	    dither_enable, color_update_enable, alpha_update_enable, dst_factor,
+	    src_factor, type == GX_BM_SUBTRACT, logic_op, 0x41));
 }
 
 void GDSetZMode(u8 compare_enable, GXCompare func, u8 update_enable)
 {
-	// clang-format off
-	GDWriteBPCmd(
-		((u32)compare_enable) |
-		((u32)func) << 1 |
-		((u32)update_enable) << 4 |
-		((u32)1) << 30
-	);
-	// clang-format on
+	GDWriteBPCmd(BP_Z_MODE(compare_enable, func, update_enable, 0x40));
 }
 
 void GDSetDstAlpha(u8 enable, u8 alpha)
 {
-	// clang-format off
-	GDWriteBPCmd(
-		((u32)alpha) |
-		((u32)enable) << 8 |
-		((u32)1) << 25 |
-		((u32)1) << 30
-	);
-	// clang-format on
+	GDWriteBPCmd(BP_DST_ALPHA(alpha, enable, 0x42));
 }
