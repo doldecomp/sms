49df2c3876b44d9a4d194ababe5f441905d9689f
Roman Sandu <roma.sandu@gmail.com>
2025-02-20 23:44:00 +0300

    Match JDRViewConnecter

---
+++ include/JSystem/JDrama/JDRViewConnecter.hpp
@@ -0,0 +1,23 @@
+#ifndef JDR_VIEW_CONNECTER_HPP
+#define JDR_VIEW_CONNECTER_HPP
+
+#include <JSystem/JDrama/JDRViewObj.hpp>
+
+namespace JDrama {
+
+class TViewConnecter : public TViewObj {
+public:
+	TViewConnecter(TViewObj*, TViewObj*, TFlagT<u16>, const char*);
+
+	virtual ~TViewConnecter() { }
+	virtual void perform(u32, JDrama::TGraphics*);
+
+public:
+	/* 0x10 */ TViewObj* unk10;
+	/* 0x14 */ TViewObj* unk14;
+	/* 0x18 */ TFlagT<u16> unk18;
+};
+
+}; // namespace JDrama
+
+#endif
--- src/JSystem/JDrama/JDRViewConnecter.cpp
+++ src/JSystem/JDrama/JDRViewConnecter.cpp
@@ -1,1 +1,27 @@
+#include <JSystem/JDrama/JDRViewConnecter.hpp>
 
+using namespace JDrama;
+
+TViewConnecter::TViewConnecter(TViewObj* param_1, TViewObj* param_2,
+                               TFlagT<u16> param_3, const char* name)
+    : TViewObj(name)
+    , unk10(param_1)
+    , unk14(param_2)
+    , unk18(param_3)
+{
+}
+
+void TViewConnecter::perform(u32 param_1, TGraphics* param_2)
+{
+	if (!(param_1 & 8))
+		return;
+
+	if (unk10)
+		unk10->testPerform(0x80, param_2);
+
+	if (unk14)
+		unk14->testPerform(param_1, param_2);
+
+	if (unk10 && (unk18.mValue & 1))
+		unk10->testPerform(8, param_2);
+}
