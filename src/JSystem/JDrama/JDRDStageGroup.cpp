492b397cc6a0541a94bd346eda9a6f2c4218caad
Roman Sandu <roma.sandu@gmail.com>
2025-02-22 23:57:21 +0300

    JDRDStageGroup class

--- src/JSystem/JDrama/JDRDStageGroup.cpp
+++ src/JSystem/JDrama/JDRDStageGroup.cpp
@@ -1,1 +1,12 @@
+#include <JSystem/JDrama/JDRDStageGroup.hpp>
 
+using namespace JDrama;
+
+#pragma dont_inline on
+void TDStageGroup::perform(u32 param_1, TGraphics* param_2)
+{
+	if (!(param_1 & 8))
+		return;
+
+	TViewObjPtrListT::perform(param_1, param_2);
+}
