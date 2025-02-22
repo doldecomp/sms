d6bf19aef3a4740760a60d14d38c0daf0eed2fb0
Roman Sandu <roma.sandu@gmail.com>
2025-02-22 23:05:45 +0300

    Almost match JDRDrawBufObj

--- include/JSystem/JDrama/JDRDrawBufObj.hpp
+++ include/JSystem/JDrama/JDRDrawBufObj.hpp
@@ -3,6 +3,8 @@
 
 #include <JSystem/JDrama/JDRViewObj.hpp>
 
+class J3DDrawBuffer;
+
 namespace JDrama {
 
 class TDrawBufObj : public TViewObj {
@@ -10,9 +12,14 @@ public:
 	TDrawBufObj();
 	TDrawBufObj(u32, u32, const char*);
 
-	virtual ~TDrawBufObj();
+	virtual ~TDrawBufObj() { }
 	virtual void load(JSUMemoryInputStream&);
 	virtual void perform(u32, TGraphics*);
+
+public:
+	/* 0x10 */ J3DDrawBuffer* mDrawBuffer;
+	/* 0x14 */ u32 mDrawBufferSize;
+	/* 0x18 */ u32 unk18;
 };
 
 }; // namespace JDrama
