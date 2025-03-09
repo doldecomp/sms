3b53c80682596e9cbababf4e445314ff58bd0c6a
Roman Sandu <roma.sandu@gmail.com>
2025-03-09 16:13:34 +0300

    Yoink JASWSParser from TWW

--- include/JSystem/JAudio/JASystem/JASWSParser.hpp
+++ include/JSystem/JAudio/JASystem/JASWSParser.hpp
@@ -7,23 +7,65 @@ namespace JASystem {
 
 class TWaveBank;
 
-class WSParser {
-public:
-	struct TCtrlGroup;
-	struct TCtrlScene;
-	struct TCtrl;
-	struct TWaveArchiveBank;
-	struct TWaveArchive;
-	struct TCtrlWave;
-	struct TWave;
+namespace WSParser {
 
-	static u32 getGroupCount(void* data);
-	static TWaveBank* createBasicWaveBank(void* data);
-	static TWaveBank* createSimpleWaveBank(void* data);
+	struct TCtrl {
+		/* 0x00 */ u32 mMagic;
+		/* 0x04 */ u32 mWaveCount;
+		/* 0x08 */ u32 mCtrlWaveOffsets[1];
+	};
+	struct TCtrlGroup {
+		/* 0x00 */ u32 mMagic;
+		/* 0x04 */ u8 unk4[0x08 - 0x04];
+		/* 0x08 */ u32 mCtrlGroupCount;
+		/* 0x0C */ u32 mCtrlSceneOffsets[1];
+	};
+	struct TCtrlScene {
+		/* 0x00 */ u32 mMagic;
+		/* 0x04 */ u8 unk4[0x0C - 0x04];
+		/* 0x0C */ u32 mCtrlOffset;
+	};
+	struct TCtrlWave {
+		/* 0x00 */ u32 unk0;
+	};
+	struct THeader {
+		/* 0x00 */ u8 unk0[0x10];
+		/* 0x10 */ u32 mArchiveBankOffset;
+		/* 0x14 */ u32 mCtrlGroupOffset;
+	};
+	struct TWave {
+		/* 0x00 */ u8 unk0;
+		/* 0x01 */ u8 unk1;
+		/* 0x02 */ u8 unk2;
+		/* 0x04 */ f32 unk4;
+		/* 0x08 */ u32 mOffset;
+		/* 0x0C */ u32 unkC;
+		/* 0x10 */ u32 unk10;
+		/* 0x14 */ u32 unk14;
+		/* 0x18 */ u32 unk18;
+		/* 0x1C */ u32 unk1c;
+		/* 0x20 */ s16 unk20;
+		/* 0x22 */ s16 unk22;
+		/* 0x24 */ u8 unk24[0x28 - 0x24];
+		/* 0x28 */ int unk28;
+	};
+	struct TWaveArchive {
+		/* 0x00 */ char mFileName[0x74];
+		/* 0x74 */ u32 mWaveOffsets[1];
+	};
+	struct TWaveArchiveBank {
+		/* 0x00 */ u32 mMagic;
+		/* 0x04 */ u8 unk4[0x08 - 0x04];
+		/* 0x08 */ u32 mArchiveOffsets[1];
+	};
+
+	u32 getGroupCount(void* data);
+	TWaveBank* createBasicWaveBank(void* data);
+	TWaveBank* createSimpleWaveBank(void* data);
 	u32 getUsedHeapSize();
 
-	static u32 sUsedHeapSize;
-};
+	extern u32 sUsedHeapSize;
+}; // namespace WSParser
 
 } // namespace JASystem
 
