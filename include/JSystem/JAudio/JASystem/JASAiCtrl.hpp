2552070b8e314d42bf865c0de2e4d6df625d5341
Roman Sandu <roma.sandu@gmail.com>
2025-03-12 01:51:36 +0300

    Yoink JASAiCtrl from TWW

--- include/JSystem/JAudio/JASystem/JASAiCtrl.hpp
+++ include/JSystem/JAudio/JASystem/JASAiCtrl.hpp
@@ -6,30 +6,19 @@
 namespace JASystem {
 namespace Kernel {
 
-	void registerDacCallback(void (*callback)(s16*, s32));
-	s16* mixMonoTrack(s16* buffer, s32 sampleCount, s16* (*mixCallback)(s32));
-	s16* mixMonoTrackWide(s16* buffer, s32 sampleCount,
-	                      s16* (*mixCallback)(s32));
-	s16* mixExtraTrack(s16* buffer, s32 sampleCount, s16* (*mixCallback)(s32));
-	s16* mixInterleaveTrack(s16* buffer, s32 sampleCount,
-	                        s16* (*mixCallback)(s32));
-	s16* (*getMixCallback(u8* mode))(s32);
-	void registerMixCallback(s16* (*callback)(s32), u8 mode);
-	u32 getCurrentVCounter();
-
-	extern void (*dacCallbackFunc)(s16*, s32);
-	extern s16* (*extMixCallback)(s32);
-	extern u8 extMixMode;
-	extern u8 useRspMadep;
-	extern u8 lastRspMadep;
-	extern u8 vframeWorkRunning;
-	extern u32 JASVframeCounter;
-	extern u32 JASUniversalDacCounter;
+	typedef s16* (*MixCallback)(s32);
 
 	void init();
 	void initSystem();
 	void vframeWork();
 	void updateDac();
+	void registerDacCallback(void (*callback)(s16*, s32));
+
+	MixCallback getMixCallback(s32);
+	void registerMixCallback(MixCallback callback, u8 mode);
+	u32 getCurrentVCounter();
+
+	extern u32 JASUniversalDacCounter;
 
 } // namespace Kernel
 } // namespace JASystem
