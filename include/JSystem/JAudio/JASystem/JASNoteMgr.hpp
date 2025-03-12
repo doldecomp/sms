f9793f120d5b029b9fc10d7e2abee38a0bcc40d7
Roman Sandu <roma.sandu@gmail.com>
2025-03-12 12:30:29 +0300

    Yoink JASNoteMgr from TWW

--- include/JSystem/JAudio/JASystem/JASNoteMgr.hpp
+++ include/JSystem/JAudio/JASystem/JASNoteMgr.hpp
@@ -1,7 +1,11 @@
 #ifndef JASNOTEMGR_HPP
 #define JASNOTEMGR_HPP
 
+#include <dolphin/types.h>
+
 namespace JASystem {
+
+class TChannel;
 
 namespace TTrack {
 	class TNoteMgr {
@@ -9,6 +13,14 @@ namespace TTrack {
 		void init();
 		void endProcess();
 		void* getChannel(int);
+
+	public:
+		/* 0x00 */ TChannel* unk0[8];
+		/* 0x20 */ u16 unk20[8];
+		/* 0x30 */ int mBaseTime;
+		/* 0x34 */ u8 mConnectCase;
+		/* 0x35 */ u8 mLastNote;
+		/* 0x36 */ u8 mBeforeTieMode;
 	};
 } // namespace TTrack
 
