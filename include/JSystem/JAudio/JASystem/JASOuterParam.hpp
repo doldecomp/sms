5d6684dbafaa1f765e7974af926d9710b7686af4
Roman Sandu <roma.sandu@gmail.com>
2025-03-15 02:47:45 +0300

    Yoink JASOuterParam from TWW

--- include/JSystem/JAudio/JASystem/JASOuterParam.hpp
+++ include/JSystem/JAudio/JASystem/JASOuterParam.hpp
@@ -12,14 +12,25 @@ namespace TTrack {
 		void initExtBuffer();
 		void setParam(u8, f32);
 		void onSwitch(u16);
-		void checkOuterSwitch(u16);
-		u16 getIntFirFilter(u8);
+		bool checkOuterSwitch(u16);
+		s16 getIntFirFilter(u8);
 		u16 getOuterUpdate();
 		void setOuterUpdate(u16);
 		void setOuterSwitch(u16);
 		void setFirFilter(s16*);
 		u16 getSwitch();
 		void setIntFirFilter(s16, u8);
+
+	public:
+		/* 0x00 */ u16 unk0;
+		/* 0x02 */ u16 unk2;
+		/* 0x04 */ f32 unk4;
+		/* 0x08 */ f32 unk8;
+		/* 0x0C */ f32 unkC;
+		/* 0x10 */ f32 unk10;
+		/* 0x14 */ f32 unk14;
+		/* 0x18 */ f32 unk18;
+		/* 0x1C */ s16 unk1C[8];
 	};
 } // namespace TTrack
 
