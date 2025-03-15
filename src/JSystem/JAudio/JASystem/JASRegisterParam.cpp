7545426915b292248864df5d50b49285029e08c5
Roman Sandu <roma.sandu@gmail.com>
2025-03-15 03:45:01 +0300

    Formatting

--- src/JSystem/JAudio/JASystem/JASRegisterParam.cpp
+++ src/JSystem/JAudio/JASystem/JASRegisterParam.cpp
@@ -4,46 +4,46 @@ namespace JASystem {
 
 TRegisterParam::TRegisterParam()
 {
-	unk0[0] = 0;
-	unk0[1] = 0;
-	unk0[2] = 0;
-	unk0[3] = 0;
-	unk0[4] = 0;
-	unk0[5] = 0;
-	unkC    = 0;
-	unkE    = 0;
-	unk1A   = 0;
+	unk0[0]      = 0;
+	unk0[1]      = 0;
+	unk0[2]      = 0;
+	unk0[3]      = 0;
+	unk0[4]      = 0;
+	unk0[5]      = 0;
+	unkC         = 0;
+	unkE         = 0;
+	unk1A        = 0;
 	mPanPower[0] = 0;
 	mPanPower[1] = 0;
 	mPanPower[2] = 0;
 	mPanPower[3] = 0;
 	mPanPower[4] = 0;
-	unk20   = 0;
-	unk24   = 0;
-	unk28   = 0;
-	unk2C   = 0;
+	unk20        = 0;
+	unk24        = 0;
+	unk28        = 0;
+	unk2C        = 0;
 }
 
 void TRegisterParam::init()
 {
-	unk0[0] = 0;
-	unk0[1] = 0;
-	unk0[2] = 0;
-	unk0[3] = 0;
-	unk0[4] = 0;
-	unk0[5] = 0;
-	unkC    = 0xf0;
-	unkE    = 0x0c;
-	unk1A   = 0x40;
+	unk0[0]      = 0;
+	unk0[1]      = 0;
+	unk0[2]      = 0;
+	unk0[3]      = 0;
+	unk0[4]      = 0;
+	unk0[5]      = 0;
+	unkC         = 0xf0;
+	unkE         = 0x0c;
+	unk1A        = 0x40;
 	mPanPower[0] = 0;
 	mPanPower[1] = 1;
 	mPanPower[2] = 1;
 	mPanPower[3] = 0x7fff;
 	mPanPower[4] = 0x4000;
-	unk20   = 0;
-	unk24   = 0;
-	unk28   = 0;
-	unk2C   = 0;
+	unk20        = 0;
+	unk24        = 0;
+	unk28        = 0;
+	unk2C        = 0;
 }
 
 void TRegisterParam::inherit(const TRegisterParam& other)
