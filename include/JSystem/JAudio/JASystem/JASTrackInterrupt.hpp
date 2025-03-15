b59925905297851e948fe9e72d6e4a68170de6a3
Roman Sandu <roma.sandu@gmail.com>
2025-03-15 03:32:57 +0300

    Yoink JASTrackInterrupt from TWW

--- include/JSystem/JAudio/JASystem/JASTrackInterrupt.hpp
+++ include/JSystem/JAudio/JASystem/JASTrackInterrupt.hpp
@@ -8,11 +8,20 @@ namespace JASystem {
 class TIntrMgr {
 public:
 	void init();
-	u32 request(u32);
+	void request(u32);
 	void setIntr(u32, void*);
 	void resetInter(u32);
-	void checkIntr();
+	void* checkIntr();
 	void timerProcess();
+
+public:
+	/* 0x00 */ u8 unk0;
+	/* 0x01 */ u8 unk1;
+	/* 0x02 */ u8 unk2;
+	/* 0x03 */ u8 unk3;
+	/* 0x04 */ u32 unk4;
+	/* 0x08 */ u32 unk8;
+	/* 0x0C */ void* unkC[8];
 };
 
 } // namespace JASystem
