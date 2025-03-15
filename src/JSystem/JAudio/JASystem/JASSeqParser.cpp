651edae37065cf7126ce3afff035c595ca7c6491
Roman Sandu <roma.sandu@gmail.com>
2025-03-15 13:44:28 +0300

    Fixup JASSeqParser data

--- src/JSystem/JAudio/JASystem/JASSeqParser.cpp
+++ src/JSystem/JAudio/JASystem/JASSeqParser.cpp
@@ -1,125 +1,222 @@
-#include "JSystem/JAudio/JASystem/JASSeqParser.hpp"
+#include <JSystem/JAudio/JASystem/JASSeqParser.hpp>
+#include <JSystem/JAudio/JASystem/JASTrack.hpp>
 
 namespace JASystem {
 
-void TSeqParser::mainProc(TTrack* track, TSeqCtrl* ctrl) { }
+TSeqParser::CmdFunc TSeqParser::sCmdPList[] = {
+	nullptr,
+	&TSeqParser::cmdOpenTrack,
+	&TSeqParser::cmdOpenTrackBros,
+	nullptr,
+	&TSeqParser::cmdCall,
+	nullptr,
+	&TSeqParser::cmdRet,
+	nullptr,
+	&TSeqParser::cmdJmp,
+	&TSeqParser::cmdLoopS,
+	&TSeqParser::cmdLoopE,
+	&TSeqParser::cmdReadPort,
+	&TSeqParser::cmdWritePort,
+	&TSeqParser::cmdCheckPortImport,
+	&TSeqParser::cmdCheckPortExport,
+	&TSeqParser::cmdWait,
+	nullptr,
+	&TSeqParser::cmdConnectName,
+	&TSeqParser::cmdParentWritePort,
+	&TSeqParser::cmdChildWritePort,
+	nullptr,
+	&TSeqParser::cmdSetLastNote,
+	&TSeqParser::cmdTimeRelate,
+	&TSeqParser::cmdSimpleOsc,
+	&TSeqParser::cmdSimpleEnv,
+	&TSeqParser::cmdSimpleADSR,
+	&TSeqParser::cmdTranspose,
+	&TSeqParser::cmdCloseTrack,
+	&TSeqParser::cmdOutSwitch,
+	&TSeqParser::cmdUpdateSync,
+	&TSeqParser::cmdBusConnect,
+	&TSeqParser::cmdPauseStatus,
+	&TSeqParser::cmdSetInterrupt,
+	&TSeqParser::cmdDisInterrupt,
+	&TSeqParser::cmdClrI,
+	&TSeqParser::cmdSetI,
+	&TSeqParser::cmdRetI,
+	&TSeqParser::cmdIntTimer,
+	nullptr,
+	&TSeqParser::cmdConnectOpen,
+	&TSeqParser::cmdConnectClose,
+	nullptr,
+	&TSeqParser::cmdSyncCPU,
+	&TSeqParser::cmdFlushAll,
+	&TSeqParser::cmdFlushRelease,
+	&TSeqParser::cmdWait,
+	&TSeqParser::cmdPanPowSet,
+	&TSeqParser::cmdIIRSet,
+	&TSeqParser::cmdFIRSet,
+	&TSeqParser::cmdEXTSet,
+	&TSeqParser::cmdPanSwSet,
+	&TSeqParser::cmdOscRoute,
+	&TSeqParser::cmdIIRCutOff,
+	&TSeqParser::cmdOscFull,
+	&TSeqParser::cmdVolumeMode,
+	nullptr,
+	nullptr,
+	nullptr,
+	&TSeqParser::cmdCheckWave,
+	&TSeqParser::cmdPrintf,
+	&TSeqParser::cmdNop,
+	&TSeqParser::cmdTempo,
+	&TSeqParser::cmdTimeBase,
+	&TSeqParser::cmdFinish,
+};
 
-void TSeqParser::cmdNoteOn(TTrack* track, u8 param) { }
+// Fabricated
+struct Arg_s {
+	u16 m00;
+	u16 m02;
+};
 
-void TSeqParser::cmdNoteOff(TTrack* track, u8 param) { }
+static const Arg_s Arglist[0x40] = {
+	{ 0x0000, 0x0000 }, { 0x0002, 0x0008 }, { 0x0002, 0x0008 },
+	{ 0x0001, 0x0002 }, { 0x0000, 0x0000 }, { 0x0000, 0x0000 },
+	{ 0x0001, 0x0000 }, { 0x0001, 0x0002 }, { 0x0000, 0x0000 },
+	{ 0x0001, 0x0001 }, { 0x0000, 0x0000 }, { 0x0002, 0x0000 },
+	{ 0x0002, 0x000C }, { 0x0001, 0x0000 }, { 0x0001, 0x0000 },
+	{ 0x0001, 0x0003 }, { 0x0002, 0x0005 }, { 0x0002, 0x000C },
+	{ 0x0002, 0x000C }, { 0x0000, 0x0000 }, { 0x0001, 0x0000 },
+	{ 0x0001, 0x0000 }, { 0x0001, 0x0000 }, { 0x0002, 0x0008 },
+	{ 0x0005, 0x0155 }, { 0x0001, 0x0000 }, { 0x0001, 0x0000 },
+	{ 0x0001, 0x0000 }, { 0x0001, 0x0001 }, { 0x0002, 0x0004 },
+	{ 0x0001, 0x0000 }, { 0x0002, 0x0008 }, { 0x0001, 0x0000 },
+	{ 0x0000, 0x0000 }, { 0x0000, 0x0000 }, { 0x0000, 0x0000 },
+	{ 0x0002, 0x0004 }, { 0x0000, 0x0000 }, { 0x0000, 0x0000 },
+	{ 0x0001, 0x0001 }, { 0x0000, 0x0000 }, { 0x0000, 0x0000 },
+	{ 0x0001, 0x0002 }, { 0x0005, 0x0000 }, { 0x0004, 0x0055 },
+	{ 0x0001, 0x0002 }, { 0x0001, 0x0002 }, { 0x0003, 0x0000 },
+	{ 0x0001, 0x0000 }, { 0x0001, 0x0000 }, { 0x0003, 0x0028 },
+	{ 0x0001, 0x0000 }, { 0x0000, 0x0000 }, { 0x0000, 0x0000 },
+	{ 0x0000, 0x0000 }, { 0x0000, 0x0000 }, { 0x0000, 0x0000 },
+	{ 0x0000, 0x0000 }, { 0x0001, 0x0001 }, { 0x0000, 0x0000 },
+	{ 0x0000, 0x0000 }, { 0x0001, 0x0001 }, { 0x0001, 0x0001 },
+	{ 0x0000, 0x0000 },
+};
 
-void TSeqParser::RegCmd_Process(TTrack* track, int param1, int param2) { }
+int TSeqParser::cmdOpenTrack(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdOpenTrackBros(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdCall(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdRet(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdJmp(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdLoopS(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdLoopE(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdReadPort(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdWritePort(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdCheckPortImport(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdCheckPortExport(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdWait(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdConnectName(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdParentWritePort(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdChildWritePort(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdSetLastNote(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdTimeRelate(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdSimpleOsc(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdSimpleEnv(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdSimpleADSR(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdTranspose(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdCloseTrack(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdOutSwitch(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdUpdateSync(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdBusConnect(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdPauseStatus(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdVolumeMode(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdSetInterrupt(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdDisInterrupt(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdClrI(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdSetI(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdRetI(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdIntTimer(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdConnectOpen(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdConnectClose(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdSyncCPU(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdFlushAll(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdFlushRelease(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdTimeBase(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdTempo(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdFinish(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdNop(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdPanPowSet(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdIIRSet(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdFIRSet(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdEXTSet(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdPanSwSet(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdOscRoute(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdIIRCutOff(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdOscFull(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdCheckWave(TTrack* track, u32* args) { }
+
+int TSeqParser::cmdPrintf(TTrack* track, u32* args) { }
 
 void TSeqParser::Cmd_Process(TTrack* track, u8 param1, u16 param2) { }
 
+void TSeqParser::RegCmd_Process(TTrack* track, int param1, int param2) { }
+
+int TSeqParser::cmdWait(TTrack* track, u8 args) { }
+
+int TSeqParser::cmdNoteOff(TTrack* track, u8 args) { }
+
+int TSeqParser::cmdNoteOn(TTrack* track, u8 args) { }
+
 bool TSeqParser::conditionCheck(TTrack* track, u8 condition) { return false; }
 
-void TSeqParser::cmdWait(TTrack* track, u8 param) { }
-
-void TSeqParser::cmdOpenTrack(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdOpenTrackBros(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdCall(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdRet(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdJmp(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdLoopS(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdLoopE(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdReadPort(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdWritePort(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdCheckPortImport(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdCheckPortExport(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdWait(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdConnectName(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdParentWritePort(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdChildWritePort(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdSetLastNote(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdTimeRelate(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdSimpleOsc(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdSimpleEnv(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdSimpleADSR(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdTranspose(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdCloseTrack(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdOutSwitch(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdUpdateSync(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdBusConnect(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdPauseStatus(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdSetInterrupt(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdDisInterrupt(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdClrI(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdSetI(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdRetI(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdIntTimer(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdConnectOpen(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdConnectClose(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdSyncCPU(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdFlushAll(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdFlushRelease(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdPanPowSet(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdIIRSet(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdFIRSet(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdEXTSet(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdPanSwSet(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdOscRoute(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdIIRCutOff(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdOscFull(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdVolumeMode(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdCheckWave(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdPrintf(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdNop(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdTempo(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdTimeBase(TTrack* track, u32* param) { }
-
-void TSeqParser::cmdFinish(TTrack* track, u32* param) { }
-
-Arglist TSeqParser::sCmdPList;
+void TSeqParser::mainProc(TTrack* track, TSeqCtrl* ctrl) { }
 
 } // namespace JASystem
