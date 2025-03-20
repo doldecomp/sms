0de657d020972062b447d92a9facde60482fe092
TheAzack9 <TheAzack9@users.noreply.github.com>
2025-03-20 10:50:36 +0100

    Implement Params and ParamsInst (#52)
    * Decompile BaseParam + Params
    
    * Implement most of Params and ParamsInst
    
    * Update MarioInit params to use new TParams macro
    
    * Fix build error
    
    * Cleanup after review

--- include/System/ParamInst.hpp
+++ include/System/ParamInst.hpp
@@ -1,13 +1,37 @@
 #ifndef PARAM_INST_HPP
 #define PARAM_INST_HPP
 
-#include "System/BaseParam.hpp"
+#include <JSystem/JDrama/JDRNameRef.hpp>
+#include <System/BaseParam.hpp>
+
+#define PARAM_INIT(member, value)                                              \
+	member(this, JDrama::TNameRef::calcKeyCode("#member"), "#member", value)
 
 template <typename T> class TParamT : public TBaseParam {
 public:
-	void load(JSUMemoryInputStream& stream);
+	TParamT(TParams* params, unsigned short code, const char* paramName,
+	        T defaultValue)
+	    : TBaseParam(params, code, paramName)
+	    , value(defaultValue)
+	{
+	}
+
+	virtual void load(JSUMemoryInputStream& stream);
+
+	// fabricated
+	T get() const { return value; }
 
 	T value;
+};
+
+template <typename T> class TParamRT : public TParamT<T> {
+public:
+	TParamRT(TParams* parent, u16 keycode, const char* name, T defaultValue)
+	    : TParamT<T>(parent, keycode, name, defaultValue)
+	{
+	}
+
+	inline void set(T param) {};
 };
 
 #endif
---
+++ src/System/Params.cpp
@@ -0,0 +1,76 @@
+#include <System/Params.hpp>
+#include <JSystem/JSupport/JSUMemoryInputStream.hpp>
+
+static const char SSceneParamsDir[] = "/map/params";
+
+JKRFileLoader* TParams::mArc      = nullptr;
+JKRFileLoader* TParams::mSceneArc = nullptr;
+
+bool TParams::load(const char* filename)
+{
+	bool found = false;
+	if (filename[0] == '/') {
+		filename++;
+	}
+
+	void* resource = nullptr;
+	if (mSceneArc != nullptr && mSceneArc->becomeCurrent(SSceneParamsDir)) {
+		resource = mSceneArc->getResource(filename);
+	}
+
+	if (resource != nullptr) {
+		s32 size = mSceneArc->getResSize(resource);
+		JSUMemoryInputStream stream(resource, size);
+		load(stream);
+		found = true;
+	} else {
+		if (mArc != nullptr) {
+			mArc->becomeCurrent("/");
+			void* resource2 = mArc->getResource(filename);
+			if (resource2 != nullptr) {
+				s32 size = mArc->getResSize(resource2);
+				JSUMemoryInputStream stream(resource2, size);
+				load(stream);
+				found = true;
+			}
+		}
+	}
+
+	return found;
+}
+
+void TParams::finalize()
+{
+	mArc      = nullptr;
+	mSceneArc = nullptr;
+}
+
+void TParams::init()
+{
+	mArc      = JKRFileLoader::getVolume("params");
+	mSceneArc = JKRFileLoader::getVolume("scene");
+}
+
+void TParams::load(JSUMemoryInputStream& stream)
+{
+	if (this->mHead != nullptr) {
+		s32 length = stream.readS32();
+		for (int i = 0; i < length; i++) {
+			u16 keyCode = stream.read16b();
+			char buffer[0x50];
+			stream.readString(buffer, 0x50);
+
+			TBaseParam* param;
+			for (param = this->mHead; param != nullptr; param = param->next) {
+				if (keyCode == param->keyCode && !strcmp(buffer, param->name)) {
+					param->load(stream);
+					break;
+				}
+			}
+			if (param == nullptr) {
+				s32 end = stream.read32b();
+				stream.skip(end);
+			}
+		}
+	}
+}
