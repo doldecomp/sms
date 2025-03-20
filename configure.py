457419a5b28a5d65dd65606a694fa3e66b88d8ba
Roman Sandu <roma.sandu@gmail.com>
2025-03-21 00:28:06 +0300

    Mark JAIDebug as matched

--- configure.py
+++ configure.py
@@ -418,7 +418,7 @@ config.libs = [
             Object(NonMatching, "JSystem/JAudio/JAInterface/JAIAnimation.cpp"),
             Object(NonMatching, "JSystem/JAudio/JAInterface/JAIBasic.cpp"),
             Object(Matching, "JSystem/JAudio/JAInterface/JAIConst.cpp"),
-            Object(NonMatching, "JSystem/JAudio/JAInterface/JAIDebug.cpp"),
+            Object(Matching, "JSystem/JAudio/JAInterface/JAIDebug.cpp"),
             Object(NonMatching, "JSystem/JAudio/JAInterface/JAIData.cpp"),
             Object(NonMatching, "JSystem/JAudio/JAInterface/JAIEntry.cpp"),
             Object(NonMatching, "JSystem/JAudio/JAInterface/JAIEntrySe.cpp"),
