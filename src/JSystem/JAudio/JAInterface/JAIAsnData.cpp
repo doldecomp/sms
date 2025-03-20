0cff169202691bb1f96c6954214a1546a9898469
Roman Sandu <roma.sandu@gmail.com>
2025-03-21 00:18:30 +0300

    Match JAIAsnData

--- src/JSystem/JAudio/JAInterface/JAIAsnData.cpp
+++ src/JSystem/JAudio/JAInterface/JAIAsnData.cpp
@@ -3,7 +3,18 @@
 
 namespace JAInter {
 
-char* TAsnData::asnFileName = "JaiInfo.asn";
-void* TAsnData::asnData     = 0;
+const char* TAsnData::asnFileName = "JaiInfo.asn";
+void* TAsnData::asnData           = 0;
+
+// unused but we need the strings
+void TAsnData::readAsnFile()
+{
+	OSReport("JAIAsnData::readAsnFile : ASN FILE(%s) が見つかりません。\n",
+	         asnFileName);
+	OSPanic(__FILE__, 0,
+	        "JAIAsnData::readAsnFile : ASN "
+	        "FILE(%s)のサイズ(0x%x)が32バイトアライメントされていません！！",
+	        asnFileName, 0);
+}
 
 } // namespace JAInter
