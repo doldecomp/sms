d4c6697c63ae3ab13c22368863bf8d3d6ff6dff5
Roman Sandu <roma.sandu@gmail.com>
2025-04-03 02:24:54 +0300

    JointModelManager equivalent

--- src/Map/JointModelManager.cpp
+++ src/Map/JointModelManager.cpp
@@ -1,12 +1,40 @@
 #include <Map/JointModelManager.hpp>
+#include <Map/JointModel.hpp>
 
-void TJointModelManager::newJointModel(int) const { }
+// rouge include needed for matching sinit & bss
+#include <MSound/MSSetSound.hpp>
+
+TJointModel* TJointModelManager::newJointModel(int) const
+{
+	return new TJointModel;
+}
 
 TJointModelManager::TJointModelManager(const char* name)
     : JDrama::TViewObj(name)
+    , mJointModelNum(0)
+    , mJointModels(nullptr)
+    , unk18(0)
+    , mModelsFolder(nullptr)
 {
 }
 
-void TJointModelManager::initJointModel(const char*, const char**) { }
+void TJointModelManager::initJointModel(const char* param_1,
+                                        const char** param_2)
+{
+	if (mJointModelNum == 0)
+		return;
+	mModelsFolder = param_1;
+	mActorAnimationData.init(mModelsFolder, nullptr);
+	mJointModels = new TJointModel*[mJointModelNum];
+	for (int i = 0; i < mJointModelNum; ++i) {
+		mJointModels[i] = newJointModel(i);
+		mJointModels[i]->setIndexInParent(i);
+		mJointModels[i]->initJointModel(this, param_2[i], &mActorAnimationData);
+	}
+}
 
-void TJointModelManager::perform(unsigned long, JDrama::TGraphics*) { }
+void TJointModelManager::perform(u32 param_1, JDrama::TGraphics* param_2)
+{
+	for (int i = 0; i < mJointModelNum; ++i)
+		mJointModels[i]->perform(param_1, param_2);
+}
