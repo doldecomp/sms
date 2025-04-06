5b6fec0f08ad2777701d132f5d591e3055a3d7e1
Roman Sandu <roma.sandu@gmail.com>
2025-04-06 17:01:41 +0300

    Application class

--- include/System/Application.hpp
+++ include/System/Application.hpp
@@ -1,6 +1,34 @@
 #ifndef APPLICATION_HPP
 #define APPLICATION_HPP
 
-float SMSGetAnmFrameRate();
+#include <dolphin/types.h>
+
+class JKRMemArchive;
+class JKRHeap;
+class TARAMBlock;
+
+void SMSGetVSyncTimesPerSec();
+f32 SMSGetAnmFrameRate();
+void SMSLoadArchive(const char*, void*, u32, JKRHeap*);
+void SMSLoadArchiveARAM(TARAMBlock*, const char*);
+void SMSMountAramArchive(JKRMemArchive*, TARAMBlock&);
+void SMSSwitch2DArchive(const char*, TARAMBlock&);
+
+class TApplication {
+public:
+	TApplication();
+	void setupThreadFuncBoot();
+	void initialize();
+	void setupThreadFuncLogo();
+	void initialize_bootAfter();
+	void initialize_nlogoAfter();
+	void initialize_processMeter();
+	void finalize();
+	void checkAdditionalMovie();
+	void proc();
+	void gameLoop();
+	void drawDVDErr();
+	void mountStageArchive();
+};
 
 #endif
