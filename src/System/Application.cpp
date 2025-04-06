5ca351366f71e14dcb7eda7363f9794c46a25c1e
Roman Sandu <roma.sandu@gmail.com>
2025-04-06 17:01:41 +0300

    Application class

--- src/System/Application.cpp
+++ src/System/Application.cpp
@@ -1,1 +1,23 @@
 #include <System/Application.hpp>
+
+void TApplication::mountStageArchive() { }
+void TApplication::drawDVDErr() { }
+void TApplication::gameLoop() { }
+void TApplication::proc() { }
+void TApplication::checkAdditionalMovie() { }
+void TApplication::finalize() { }
+void TApplication::initialize_processMeter() { }
+void TApplication::initialize_nlogoAfter() { }
+void TApplication::initialize_bootAfter() { }
+void SetupThreadFuncLogo(void*) { }
+void TApplication::setupThreadFuncLogo() { }
+void TApplication::initialize() { }
+void SetupThreadFuncBoot(void*) { }
+void TApplication::setupThreadFuncBoot() { }
+void SMSSwitch2DArchive(const char*, TARAMBlock&) { }
+void SMSMountAramArchive(JKRMemArchive*, TARAMBlock&) { }
+void SMSLoadArchiveARAM(TARAMBlock*, const char*) { }
+void SMSLoadArchive(const char*, void*, u32, JKRHeap*) { }
+TApplication::TApplication() { }
+f32 SMSGetAnmFrameRate() { }
+void SMSGetVSyncTimesPerSec() { }
