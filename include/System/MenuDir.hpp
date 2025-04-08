12b9814e8d06823ac50b3aeb49f4e86d5e21e2bf
Roman Sandu <roma.sandu@gmail.com>
2025-04-08 22:46:09 +0300

    MenuDir class

---
+++ include/System/MenuDir.hpp
@@ -0,0 +1,20 @@
+#ifndef SYSTEM_MENU_DIR_HPP
+#define SYSTEM_MENU_DIR_HPP
+
+#include <JSystem/JDrama/JDRDisplay.hpp>
+#include <JSystem/JDrama/JDRDirector.hpp>
+
+class TMarioGamePad;
+
+class TMenuDirector : public JDrama::TDirector {
+public:
+	TMenuDirector();
+	~TMenuDirector();
+	void setupThreadFunc(void*);
+	void setup(JDrama::TDisplay*, TMarioGamePad*);
+	void rsetup();
+	void direct();
+	void setFixedStageValue();
+};
+
+#endif
