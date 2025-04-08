194ef0a139b6ccd4c5c357740846182ac6c8d42e
Roman Sandu <roma.sandu@gmail.com>
2025-04-09 00:04:49 +0300

    TimeRec almost ok (regswap...)

--- src/System/TimeRec.cpp
+++ src/System/TimeRec.cpp
@@ -1,52 +1,10 @@
 #include <System/TimeRec.hpp>
 #include <System/DrawSyncManager.hpp>
 #include <dolphin/gx/GXManage.h>
+#include <dolphin/os.h>
 #include <types.h>
 
 TTimeRec* TTimeRec::_instance;
-
-void TTimeRec::drawSyncCallback(u16) { }
-
-void TTimeRec::suppleGXTime() { }
-
-void TTimeRec::snapGXTime(u32 param_1)
-{
-	if (unk4[unk814][1].append(0, param_1)) {
-		if ((unk81C & 1) == 0) {
-			u32 uVar1 = unk81A;
-			u32 uVar2 = unk4[unk814][1].mSize;
-			if (TDrawSyncManager::smInstance)
-				TDrawSyncManager::smInstance->pushBreakPoint();
-			GXSetDrawSync(uVar1 + (uVar2 - 1));
-		}
-	}
-}
-
-void TTimeRec::flip()
-{
-	unk814 ^= 1;
-	unk4[unk814][0].mSize = 0;
-	unk4[unk814][1].mSize = 0;
-}
-
-TTimeRec::TTimeRec(u16 param_1)
-    : unk814(0)
-    , unk818(0)
-    , unk81A(param_1)
-    , unk81C(0)
-{
-}
-
-void TTimeRec::drawSyncCallbackSt(u16) { }
-
-void TTimeRec::end() { }
-
-TTimeRec* TTimeRec::start(u16 param_1)
-{
-	if (_instance == nullptr)
-		_instance = new TTimeRec(param_1);
-	return _instance;
-}
 
 bool TTimeArray::append(u32 start, u32 end)
 {
@@ -58,4 +16,64 @@ bool TTimeArray::append(u32 start, u32 end)
 		result       = true;
 	}
 	return result;
+}
+
+TTimeRec* TTimeRec::start(u16 param_1)
+{
+	if (_instance == nullptr)
+		_instance = new TTimeRec(param_1);
+	return _instance;
+}
+
+void TTimeRec::end() { }
+
+void TTimeRec::drawSyncCallbackSt(u16) { }
+
+TTimeRec::TTimeRec(u16 param_1)
+    : unk814(0)
+    , unk818(0)
+    , unk81A(param_1)
+    , unk81C(0)
+{
+}
+
+void TTimeRec::flip()
+{
+	TTimeArray& array = _instance->crTimeAry()[1];
+	int size          = array.mSize;
+	if (size >= 3) {
+		int i    = size - 1;
+		u32 curr = array.mEntries[i].start;
+		while (i > 0) {
+			--i;
+			if (array.mEntries[i].start == 0) {
+				array.mEntries[i].start = curr;
+			} else {
+				curr = array.mEntries[i].start;
+			}
+		}
+	}
+
+	unk814 ^= 1;
+	crTimeAry()[0].mSize = 0;
+	crTimeAry()[1].mSize = 0;
+}
+
+void TTimeRec::snapGXTime(u32 param_1)
+{
+	if (crTimeAry()[1].append(0, param_1)) {
+		if ((unk81C & 1) == 0) {
+			u16 token = unk81A + crTimeAry()[1].size() - 1;
+			if (TDrawSyncManager::smInstance)
+				TDrawSyncManager::smInstance->pushBreakPoint();
+			GXSetDrawSync(token);
+		}
+	}
+}
+
+void TTimeRec::suppleGXTime() { }
+
+void TTimeRec::drawSyncCallback(u16 param_1)
+{
+	unk4[unk814][1][param_1 - unk81A].start = OSGetTick();
 }
