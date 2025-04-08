e4eecdc25b6fccd934950250e2cea2f9b7f73dad
Roman Sandu <roma.sandu@gmail.com>
2025-04-08 22:46:09 +0300

    MenuDir class

---
+++ src/System/MenuDir.cpp
@@ -0,0 +1,15 @@
+#include <System/MenuDir.hpp>
+
+void TMenuDirector::setFixedStageValue() { }
+
+void TMenuDirector::direct() { }
+
+void TMenuDirector::rsetup() { }
+
+void TMenuDirector::setup(JDrama::TDisplay*, TMarioGamePad*) { }
+
+void TMenuDirector::setupThreadFunc(void*) { }
+
+TMenuDirector::~TMenuDirector() { }
+
+TMenuDirector::TMenuDirector() { }
