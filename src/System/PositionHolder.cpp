667fbb0c451e045fb847add79a46a0d4e070e696
Roman Sandu <roma.sandu@gmail.com>
2025-04-09 11:46:13 +0300

    PositionHolder equivalent

---
+++ src/System/PositionHolder.cpp
@@ -0,0 +1,19 @@
+#include <System/PositionHolder.hpp>
+
+TStagePositionInfo* gpPositionHolder;
+
+void TStagePositionInfo::load(JSUMemoryInputStream& stream)
+{
+	JDrama::TNameRef::load(stream);
+	stream.read(&unkC.x, sizeof(f32));
+	stream.read(&unkC.y, sizeof(f32));
+	stream.read(&unkC.z, sizeof(f32));
+
+	// wtf is happening with inlines here...
+	stream.readF32();
+	stream.readF32();
+	stream.readF32();
+	stream.readF32();
+	stream.readF32();
+	stream.readF32();
+}
