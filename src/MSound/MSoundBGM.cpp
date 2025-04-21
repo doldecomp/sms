b265aef42c2365a2d13f815c271a408d3ab4d650
ThePlayerRolo <thesolis78243@gmail.com>
2025-04-21 13:47:05 -0400

    Implement getSceneNo

--- src/MSound/MSoundBGM.cpp
+++ src/MSound/MSoundBGM.cpp
@@ -39,7 +39,104 @@ void MSBgm::setStageBgmYoshiPercussion(bool param) { }
 
 bool MSBgm::checkPlaying(u32 param) { return false; }
 
-u32 MSBgm::getSceneNo(u32 param) { return 0; }
+u32 MSBgm::getSceneNo(u32 param) {  
+	switch(param) {
+	case 0x80010001:
+	  return 0x201;
+	case 0x80010002:
+	  return 0x202;
+	case 0x80010003:
+	  return 0x203;
+	case 0x80010004:
+	  return 0x204;
+	case 0x80010005:
+	  return 0x204;
+	case 0x80010006:
+	  return 0x205;
+	case 0x80010007:
+	  return 0x206;
+	case 0x80010008:
+	  return 0x207;
+	case 0x80010009:
+	  return 0x208;
+	case 0x8001000a:
+	  return 0x210;
+	case 0x8001000b:
+	  return 0x210;
+	case 0x8001000c:
+	  return 0x210;
+	case 0x8001000d:
+	  return 0x210;
+	case 0x8001000e:
+	  return 0x20a;
+	case 0x8001000f:
+	  return 0x209;
+	case 0x80010010:
+	  return 0x20a;
+	case 0x80010011:
+	  return 0xffffffff;
+	case 0x80010012:
+	  return 0x20c;
+	case 0x80010013:
+	  return 0x20d;
+	case 0x80010014:
+	  return 0x20e;
+	case 0x80010015:
+	  return 0x20f;
+	case 0x80010016:
+	  return 0x210;
+	case 0x80010017:
+	  return 0x210;
+	case 0x80010018:
+	  return 0x206;
+	case 0x80010019:
+	  return 0x211;
+	case 0x8001001a:
+	  return 0x212;
+	case 0x8001001b:
+	  return 0x210;
+	case 0x8001001c:
+	  return 0x20a;
+	case 0x8001001d:
+	  return 0x206;
+	case 0x8001001e:
+	  return 0x20d;
+	case 0x8001001f:
+	  return 0x210;
+	case 0x80010020:
+	  return 0x210;
+	case 0x80010021:
+	  return 0x214;
+	case 0x80010022:
+	  return 0x206;
+	case 0x80010023:
+	  return 0x204;
+	case 0x80010024:
+	  return 0x210;
+	case 0x80010025:
+	  return 0x20d;
+	case 0x80010026:
+	  return 0x210;
+	case 0x80010027:
+	  return 0x210;
+	case 0x80010028:
+	  return 0x210;
+	case 0x80010029:
+	  return 0x210;
+	case 0x8001002a:
+	  return 0x210;
+	case 0x8001002b:
+	  return 0x210;
+	case 0x8001002c:
+	  return 0x206;
+	default:
+	  return 0xffffffff;
+	case 0x8001002e:
+	  return 0x215;
+	case 0x8001002f:
+	  return 0x210;
+	 } 
+	}
 
 JAISound* MSBgm::getHandle(u8 param) 
 { 
