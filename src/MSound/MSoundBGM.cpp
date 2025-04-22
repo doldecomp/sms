11b00e5fbec694a99e2182842a3124f5412c1e90
ThePlayerRolo <thesolis78243@gmail.com>
2025-04-22 15:47:49 -0400

    Changing it to JAISound still doesn't work?

--- src/MSound/MSoundBGM.cpp
+++ src/MSound/MSoundBGM.cpp
@@ -26,7 +26,7 @@ void MSBgm::setTrackVolume(u8 param1, f32 param2, u32 param3, u8 param4) {
 	if ((track) &&
 	   (sound = *(JAISound **)((&smBgmInTrack)[param1] + 0x14), sound != (JAISound *)0x0)) {
         //Using JAISound::setVolume  instead causes an  Static Error. Even though that is in the Decompiled Ghidra Code. 
-		setVolume(param1,param2,param3,param4);
+		JAISound::setVolume(param1,param2,param3,param4);
 	}
 	return;
  }
