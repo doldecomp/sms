0e0cfb6364166203c6c1c8374bcd74613f3c97ec
Roman Sandu <roma.sandu@gmail.com>
2025-05-10 02:42:12 +0300

    MarineSnow equivalent

--- configure.py
+++ configure.py
@@ -1077,7 +1077,7 @@ config.libs = [
             Object(NonMatching, "Map/PollutionLayer.cpp"),
             Object(NonMatching, "Map/PollutionEvent.cpp"),
             Object(NonMatching, "Map/MapCollisionPlane.cpp"),
-            Object(NonMatching, "Map/MarineSnow.cpp"),
+            Object(NonMatching, "Map/MarineSnow.cpp", extra_cflags=["-inline deferred"]),
             Object(Matching, "Map/MapData.cpp"),
             Object(NonMatching, "Map/MapEventDolpic.cpp"),
             Object(NonMatching, "Map/MapEventMare.cpp"),
