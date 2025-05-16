14894cb64908c4154d0174fe07859ba6a14dfb75
ThePlayerRolo <thesolis78243@gmail.com>
2025-05-16 17:13:27 -0400

    Format

--- src/MSound/MSoundBGM.cpp
+++ src/MSound/MSoundBGM.cpp
@@ -19,21 +19,23 @@ void MSBgm::setVolume(u32 param1, f32 param2, u32 param3, u8 param4)
 	MSBgm* bgm = JALListS<MSBgm, u32>::search(param1 & 0x3FF);
 }
 
-void MSBgm::setTrackVolume(u8 param1, f32 param2, u32 param3, u8 param4) {
+void MSBgm::setTrackVolume(u8 param1, f32 param2, u32 param3, u8 param4)
+{
 	JAISound* sound = nullptr;
-    
-    	MSBgm* track = smBgmInTrack[param1];
-	if ((track) &&
-	   (sound = *(JAISound **)(track + 0x14), sound != (JAISound*) 0x0)) {
-        //Using JAISound::setVolume  instead causes an  Static Error. Even though that is in the Decompiled Ghidra Code. 
-		//Function should be something like JAISound::setVolume(param2,param3,param4); 
-		//But due to a "a nonstatic member reference must be relative to a specific object" error with JAISound I can't
-		setVolume(param2,param3,param4, param1);
+
+	MSBgm* track = smBgmInTrack[param1];
+	if ((track)
+	    && (sound = *(JAISound**)(track + 0x14), sound != (JAISound*)0x0)) {
+		// Using JAISound::setVolume  instead causes an  Static Error. Even
+		// though that is in the Decompiled Ghidra Code. Function should be
+		// something like JAISound::setVolume(param2,param3,param4); But due to
+		// a "a nonstatic member reference must be relative to a specific
+		// object" error with JAISound I can't
+		setVolume(param2, param3, param4, param1);
 	}
 	return;
- }
+}
 
- 
 void MSBgm::setSeqTrackVolume(u8 param1, u8 param2, f32 param3, u32 param4) { }
 
 void MSBgm::setPan(u8 param1, f32 param2, u32 param3, u8 param4) { }
@@ -52,112 +54,113 @@ void MSBgm::setStageBgmYoshiPercussion(bool param) { }
 
 bool MSBgm::checkPlaying(u32 param) { return false; }
 
-u32 MSBgm::getSceneNo(u32 param) {  
-	switch(param) {
+u32 MSBgm::getSceneNo(u32 param)
+{
+	switch (param) {
 	case 0x80010001:
-	  return 0x201;
+		return 0x201;
 	case 0x80010002:
-	  return 0x202;
+		return 0x202;
 	case 0x80010003:
-	  return 0x203;
+		return 0x203;
 	case 0x80010004:
-	  return 0x204;
+		return 0x204;
 	case 0x80010005:
-	  return 0x204;
+		return 0x204;
 	case 0x80010006:
-	  return 0x205;
+		return 0x205;
 	case 0x80010007:
-	  return 0x206;
+		return 0x206;
 	case 0x80010008:
-	  return 0x207;
+		return 0x207;
 	case 0x80010009:
-	  return 0x208;
+		return 0x208;
 	case 0x8001000a:
-	  return 0x210;
+		return 0x210;
 	case 0x8001000b:
-	  return 0x210;
+		return 0x210;
 	case 0x8001000c:
-	  return 0x210;
+		return 0x210;
 	case 0x8001000d:
-	  return 0x210;
+		return 0x210;
 	case 0x8001000e:
-	  return 0x20a;
+		return 0x20a;
 	case 0x8001000f:
-	  return 0x209;
+		return 0x209;
 	case 0x80010010:
-	  return 0x20a;
+		return 0x20a;
 	case 0x80010011:
-	  return 0xffffffff;
+		return 0xffffffff;
 	case 0x80010012:
-	  return 0x20c;
+		return 0x20c;
 	case 0x80010013:
-	  return 0x20d;
+		return 0x20d;
 	case 0x80010014:
-	  return 0x20e;
+		return 0x20e;
 	case 0x80010015:
-	  return 0x20f;
+		return 0x20f;
 	case 0x80010016:
-	  return 0x210;
+		return 0x210;
 	case 0x80010017:
-	  return 0x210;
+		return 0x210;
 	case 0x80010018:
-	  return 0x206;
+		return 0x206;
 	case 0x80010019:
-	  return 0x211;
+		return 0x211;
 	case 0x8001001a:
-	  return 0x212;
+		return 0x212;
 	case 0x8001001b:
-	  return 0x210;
+		return 0x210;
 	case 0x8001001c:
-	  return 0x20a;
+		return 0x20a;
 	case 0x8001001d:
-	  return 0x206;
+		return 0x206;
 	case 0x8001001e:
-	  return 0x20d;
+		return 0x20d;
 	case 0x8001001f:
-	  return 0x210;
+		return 0x210;
 	case 0x80010020:
-	  return 0x210;
+		return 0x210;
 	case 0x80010021:
-	  return 0x214;
+		return 0x214;
 	case 0x80010022:
-	  return 0x206;
+		return 0x206;
 	case 0x80010023:
-	  return 0x204;
+		return 0x204;
 	case 0x80010024:
-	  return 0x210;
+		return 0x210;
 	case 0x80010025:
-	  return 0x20d;
+		return 0x20d;
 	case 0x80010026:
-	  return 0x210;
+		return 0x210;
 	case 0x80010027:
-	  return 0x210;
+		return 0x210;
 	case 0x80010028:
-	  return 0x210;
+		return 0x210;
 	case 0x80010029:
-	  return 0x210;
+		return 0x210;
 	case 0x8001002a:
-	  return 0x210;
+		return 0x210;
 	case 0x8001002b:
-	  return 0x210;
+		return 0x210;
 	case 0x8001002c:
-	  return 0x206;
+		return 0x206;
 	case 0x8001002e:
-	  return 0x215;
+		return 0x215;
 	case 0x8001002f:
-	  return 0x210;
-	  default:
-	  return 0xffffffff;
-	 } 
+		return 0x210;
+	default:
+		return 0xffffffff;
 	}
+}
 
-JAISound* MSBgm::getHandle(u8 param) 
-{ 
-		MSBgm* track = smBgmInTrack[param];
-		if (track) {
-			return (JAISound*)(&track[0x14]);
-		}
-		return nullptr;
+JAISound* MSBgm::getHandle(u8 param)
+{
+	MSBgm* track = smBgmInTrack[param];
+	if (track) {
+		return (JAISound*)(&track[0x14]);
 	}
+	return nullptr;
+}
 
 JAISound* MSBgm::getJASTrack(JAISound* param1, u8 param2) { return nullptr; }
