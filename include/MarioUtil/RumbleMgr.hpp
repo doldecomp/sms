68d1d76abb61f092be76af053f8ed2da6814e1d6
Hyblocker <hyblocker@protonmail.com>
2025-10-22 20:40:21 +0200

    how were u not clang formatted

--- include/MarioUtil/RumbleMgr.hpp
+++ include/MarioUtil/RumbleMgr.hpp
@@ -5,7 +5,7 @@
 #include <dolphin/pad.h>
 #include <JSystem/JGeometry.hpp>
 
-#define MAX_RUMBLE_ID (25)
+#define MAX_RUMBLE_ID       (25)
 #define MAX_RUMBLE_CHANNELS (32)
 
 struct RumbleChannelData {
@@ -25,14 +25,14 @@ public:
 	f32 update();
 
 public:
-	/* 0x00 */ f32 mElapsedTime;                            /* inferred */
-	/* 0x04 */ f32 mCurrentIntensity;                       /* inferred */
-	/* 0x08 */ s32 mChannelID;                              /* inferred */
-	/* 0x0C */ s32 mLoopCount;                              /* inferred */
-	/* 0x10 */ f32* mExternalDampenPtr;                     /* inferred */
-	/* 0x14 */ Vec* mPositionalSourcePtr; /* inferred */
-	/* 0x18 */ const RumbleChannelData* rumbleData;                           /* inferred */
-	/* 0x1C */ void* unk20;                                 /* inferred */
+	/* 0x00 */ f32 mElapsedTime;                    /* inferred */
+	/* 0x04 */ f32 mCurrentIntensity;               /* inferred */
+	/* 0x08 */ s32 mChannelID;                      /* inferred */
+	/* 0x0C */ s32 mLoopCount;                      /* inferred */
+	/* 0x10 */ f32* mExternalDampenPtr;             /* inferred */
+	/* 0x14 */ Vec* mPositionalSourcePtr;           /* inferred */
+	/* 0x18 */ const RumbleChannelData* rumbleData; /* inferred */
+	/* 0x1C */ void* unk20;                         /* inferred */
 };
 
 class RumbleControllerMgr {
@@ -46,13 +46,13 @@ public:
 	void update();
 
 public:
-	f32 currentPower;          // 0x00
-    RumbleChannelMgr *channels;  // 0x04
-    RumbleChannelMgr *unk8;      // 0x08
-    u32 unkC;                    // 0x0C
-    u16 motorTime;               // 0x10
-    bool unk12;                    // 0x12
-    u8 padding_13;               // 0x13
+	f32 currentPower;           // 0x00
+	RumbleChannelMgr* channels; // 0x04
+	RumbleChannelMgr* unk8;     // 0x08
+	u32 unkC;                   // 0x0C
+	u16 motorTime;              // 0x10
+	bool unk12;                 // 0x12
+	u8 padding_13;              // 0x13
 };
 
 class RumbleMgr;
@@ -91,11 +91,11 @@ public:
 	f32 m_masterIntensityL;                       // 0x00
 	f32 m_masterIntensityR;                       // 0x04
 	bool m_isInitialized;                         // 0x08
-	bool m_flags;                                  // 0x09
-	bool unkA;                                  // 0x0A
+	bool m_flags;                                 // 0x09
+	bool unkA;                                    // 0x0A
 	RumbleControllerState* m_controllerStates[4]; // 0x0C
 	RumbleControllerMgr* m_controllerManagers[4]; // 0x1C
-	const int *** m_rumbleOutput;                          // 0x2C
+	const int*** m_rumbleOutput;                  // 0x2C
 
 	static u32 mMotorCountLimit;
 	static u16 mMotorTimerPeriod;
