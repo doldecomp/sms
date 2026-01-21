f7f1c12a304d0ac6ba604562b3fbcb766041090c
Lingo <lingomaniac88@gmail.com>
2026-01-21 00:32:41 -0800

    CLBCalcScaleTranslateMatrix

--- src/Camera/cameralib.cpp
+++ src/Camera/cameralib.cpp
@@ -4,6 +4,32 @@
 #define ABS(x) ((x) >= 0 ? (x) : -(x))
 
 /**
+ * @brief Creates a scale-and-translate matrix.
+ *
+ * @param mtx the destination matrix
+ * @param scale scale factors
+ * @param translate translation values
+ */
+void CLBCalcScaleTranslateMatrix(MtxPtr mtx, const Vec& scale,
+                                 const Vec& translate)
+{
+	mtx[0][0] = scale.x;
+	mtx[0][1] = 0.0f;
+	mtx[0][2] = 0.0f;
+	mtx[0][3] = translate.x;
+
+	mtx[1][0] = 0.0f;
+	mtx[1][1] = scale.y;
+	mtx[1][2] = 0.0f;
+	mtx[1][3] = translate.y;
+
+	mtx[2][0] = 0.0f;
+	mtx[2][1] = 0.0f;
+	mtx[2][2] = scale.z;
+	mtx[2][3] = translate.z;
+}
+
+/**
  * @brief Moves dstValue toward targetValue by a fraction defined by ratio.
  *
  * @param dstValue the value to be modified
