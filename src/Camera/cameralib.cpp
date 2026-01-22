8b721eab0870c0ead276599825bbb2e274ab91ef
Lingo <lingomaniac88@gmail.com>
2026-01-22 01:42:01 -0800

    CLBCalcRotateZXYTranslateMatrix
    registers are still slightly wonky

--- src/Camera/cameralib.cpp
+++ src/Camera/cameralib.cpp
@@ -1,7 +1,48 @@
 #include <Camera/cameralib.hpp>
+#include <JSystem/JMath.hpp>
 
 // TODO: This macro should probably be consolidated elsewhere
 #define ABS(x) ((x) >= 0 ? (x) : -(x))
+
+/**
+ * @brief Creates a rotate-and-translate matrix.
+ *
+ * @details
+ * Rotations are applied in ZXY order.
+ *
+ * @param mtx the destination matrix
+ * @param rotate rotation angles in degrees
+ * @param translate translation values
+ */
+void CLBCalcRotateZXYTranslateMatrix(MtxPtr mtx, const Vec& rotate,
+                                     const Vec& translate)
+{
+	s16 sAngleX = CLBRoundf<s16>(DEG2SHORTANGLE(rotate.x));
+	s16 sAngleY = CLBRoundf<s16>(DEG2SHORTANGLE(rotate.y));
+	s16 sAngleZ = CLBRoundf<s16>(DEG2SHORTANGLE(rotate.z));
+
+	f32 sinX = JMASSin(sAngleX);
+	f32 cosX = JMASCos(sAngleX);
+	f32 sinY = JMASSin(sAngleY);
+	f32 cosY = JMASCos(sAngleY);
+	f32 sinZ = JMASSin(sAngleZ);
+	f32 cosZ = JMASCos(sAngleZ);
+
+	mtx[0][0] = cosY * cosZ + sinY * sinX * sinZ;
+	mtx[0][1] = -sinZ * cosY + cosZ * sinY * sinX;
+	mtx[0][2] = sinY * cosX;
+	mtx[0][3] = translate.x;
+
+	mtx[1][0] = cosX * sinZ;
+	mtx[1][1] = cosX * cosZ;
+	mtx[1][2] = -sinX;
+	mtx[1][3] = translate.y;
+
+	mtx[2][0] = -sinY * cosZ + sinZ * cosY * sinX;
+	mtx[2][1] = sinY * sinZ + cosZ * cosY * sinX;
+	mtx[2][2] = cosY * cosX;
+	mtx[2][3] = translate.z;
+}
 
 /**
  * @brief Creates a scale-and-translate matrix.
