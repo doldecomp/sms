46a72b02c1c5eb866b4bfa7aad9b8241ff0eb6d6
Roman Sandu <roma.sandu@gmail.com>
2026-02-13 01:27:14 +0300

    StageEventInfo OK

--- src/System/StageEventInfo.cpp
+++ src/System/StageEventInfo.cpp
@@ -1,1 +1,14 @@
+#include <System/StageEventInfo.hpp>
 
+void TStageEventInfo::load(JSUMemoryInputStream& stream)
+{
+	JDrama::TNameRef::load(stream);
+
+	unkC  = stream.readU32();
+	unk10 = stream.readString();
+	unk14 = stream.readString();
+	unk18 = stream.readString();
+	unk1C = stream.readString();
+	unk20 = stream.readString();
+	unk24 = stream.readString();
+}
