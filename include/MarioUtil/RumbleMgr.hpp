ebcc6bf166fd774cc3b150718d908289c9504657
Hyblocker <hyblocker@protonmail.com>
2026-02-21 22:14:53 +0100

    chore: lint

--- include/MarioUtil/RumbleMgr.hpp
+++ include/MarioUtil/RumbleMgr.hpp
@@ -1,130 +1,123 @@
 #ifndef MARIO_UTIL_RUMBLE_MGR_HPP
 #define MARIO_UTIL_RUMBLE_MGR_HPP
 
-#include <types.h>
 #include <dolphin/mtx.h>
 #include <dolphin/pad.h>
+#include <types.h>
 
 #define RUMBLE_CHANNELS_PER_CONTROLLER 32
 
 struct RumbleChannelDataTbl {
-	int* mPointData;
-	float* mFrameData;
-	float* mPowerData;
+    int *mPointData;
+    float *mFrameData;
+    float *mPowerData;
 };
 
 extern RumbleChannelDataTbl channelDataTbl[];
 
 class RumbleBase {
-public:
-	RumbleBase(int chan)
-	    : mPower(0.0f)
-	    , mAccumulator(0.0f)
-	    , mChan(chan)
-	{
-	}
-	void init();
-	void stop();
-	void update(f32, bool);
+  public:
+    RumbleBase( int chan )
+        : mPower( 0.0f ), mAccumulator( 0.0f ), mChan( chan ) {}
+    void init();
+    void stop();
+    void update( f32, bool );
 
-	f32 mPower;
-	f32 mAccumulator;
-	int mChan;
+    f32 mPower;
+    f32 mAccumulator;
+    int mChan;
 };
 
 class RumbleChannelDataMgr {
-public:
-	RumbleChannelDataMgr()
-	    : mChannelDataTbl(nullptr)
-	{
-	}
-	void init();
-	RumbleChannelDataTbl* getChannelDataTbl(int);
+  public:
+    RumbleChannelDataMgr() : mChannelDataTbl( nullptr ) {}
+    void init();
+    RumbleChannelDataTbl *getChannelDataTbl( int );
 
-	RumbleChannelDataTbl* mChannelDataTbl;
+    RumbleChannelDataTbl *mChannelDataTbl;
 };
 
 class RumbleChannelMgr {
-public:
-	RumbleChannelMgr();
-	void init(RumbleChannelDataMgr*);
-	void reset();
-	void repeat();
-	void start(int, int, f32*);
-	void start(int, int, Vec*);
-	f32 update();
+  public:
+    RumbleChannelMgr();
+    void init( RumbleChannelDataMgr * );
+    void reset();
+    void repeat();
+    void start( int, int, f32 * );
+    void start( int, int, Vec * );
+    f32 update();
 
-	f32 mElapsedTime;
-	f32 mPower;
-	int mChannelDataIdx;
-	int mRepeatCount;
-	f32* mMultiplierF;
-	Vec* mWorldPos;
-	RumbleChannelDataTbl* mChannelDataTbl;
-	RumbleChannelDataMgr* mChannelDataMgr;
+    f32 mElapsedTime;
+    f32 mPower;
+    int mChannelDataIdx;
+    int mRepeatCount;
+    f32 *mMultiplierF;
+    Vec *mWorldPos;
+    RumbleChannelDataTbl *mChannelDataTbl;
+    RumbleChannelDataMgr *mChannelDataMgr;
 };
 
 class RumbleControllerMgr {
-public:
-	RumbleControllerMgr();
-	void init();
-	void reset();
-	void start(int, int, f32*);
-	void start(int, int, Vec*);
-	void stop();
-	void stop(int);
-	bool channelMgrIsAllFree();
-	void updateMotorCount();
-	f32 update();
+  public:
+    RumbleControllerMgr();
+    void init();
+    void reset();
+    void start( int, int, f32 * );
+    void start( int, int, Vec * );
+    void stop();
+    void stop( int );
+    bool channelMgrIsAllFree();
+    void updateMotorCount();
+    f32 update();
 
-	f32 mPower;
-	RumbleChannelMgr* mChannelMgrTbl;
-	RumbleChannelDataMgr* mChannelDataMgr;
-	u32 mMotorCounter;
-	u16 mMotorTimer;
-	s8 unk12;
+    f32 mPower;
+    RumbleChannelMgr *mChannelMgrTbl;
+    RumbleChannelDataMgr *mChannelDataMgr;
+    u32 mMotorCounter;
+    u16 mMotorTimer;
+    s8 unk12;
 };
 
 class RumbleMgr;
 
-extern RumbleMgr* SMSRumbleMgr;
+extern RumbleMgr *SMSRumbleMgr;
 
 class RumbleMgr {
-public:
-	RumbleMgr(bool, bool, bool, bool);
-	void init();
-	void reset();
-	void start(int, f32*);
-	void start(int, Vec*);
-	void start(int, MtxPtr);
-	void start(int, int, f32*);
-	void start(int, int, Vec*);
-	void start(int, int, MtxPtr);
-	void start(int, int, int, f32*);
-	void start(int, int, int, Vec*);
-	void start(int, int, int, MtxPtr);
-	void stop();
-	void stop(int);
-	void stop(int, int);
-	void update();
-	void setActive(bool);
-	void startPause();
-	void finishPause();
-	void changePause();
-	void changeMode();
+  public:
+    RumbleMgr( bool, bool, bool, bool );
+    void init();
+    void reset();
+    void start( int, f32 * );
+    void start( int, Vec * );
+    void start( int, MtxPtr );
+    void start( int, int, f32 * );
+    void start( int, int, Vec * );
+    void start( int, int, MtxPtr );
+    void start( int, int, int, f32 * );
+    void start( int, int, int, Vec * );
+    void start( int, int, int, MtxPtr );
+    void stop();
+    void stop( int );
+    void stop( int, int );
+    void update();
+    void setActive( bool );
+    void startPause();
+    void finishPause();
+    void changePause();
+    void changeMode();
 
-	f32 mPower;
-	f32 mTimer;
-	bool mActive;
-	bool mPaused;
-	bool mStopHard;
-	RumbleBase* mRumbleBaseTbl[4];
-	RumbleControllerMgr* mControllerMgrTbl[4];
-	RumbleChannelDataMgr* mChannelDataMgr;
+    f32 mPower;
+    f32 mTimer;
+    bool mActive;
+    bool mPaused;
+    bool mStopHard;
+    RumbleBase *mRumbleBaseTbl[ 4 ];
+    RumbleControllerMgr *mControllerMgrTbl[ 4 ];
+    RumbleChannelDataMgr *mChannelDataMgr;
 
-	static int mMotorCountLimit;
-	static u16 mMotorTimerPeriod;
-	static f32 mPowerThreshold;
+    static int mMotorCountLimit;
+    static u16 mMotorTimerPeriod;
+    static f32 mPowerThreshold;
 };
 
 #endif
--- src/MarioUtil/RumbleType.cpp
+++ src/MarioUtil/RumbleType.cpp
@@ -4,57 +4,55 @@
 extern int channelNum;
 
 struct RumbleTypeInfo {
-	unsigned int unk0;
-	char* str;
+    unsigned int unk0;
+    char *str;
 };
 
 // Enum To Str?
 // @BUG - channelNum (number of rumble types) is 25,
 // but _info is only 23 elements long.
-struct RumbleTypeInfo _info[23] = {
-	{ 0, "MARIO_HIPDROP" },
-	{ 1, "MARIO_DAMAGE" },
-	{ 2, "MARIO_LANDING" },
-	{ 3, "MARIO_0_1SEC_FULL" },
-	{ 4, "MARIO_HIP_ATTACK2" },
-	{ 5, "MARIO_DAMAGE2" },
-	{ 6, "MARIO_RUMBLE_06" },
-	{ 7, "MARIO_RUMBLE_07" },
-	{ 8, "MARIO_1SEC_SIN" },
-	{ 9, "MARIO_1SEC_SIN_HALF" },
-	{ 10, "MARIO_RUMBLE_10" },
-	{ 11, "MARIO_RUMBLE_11" },
-	{ 12, "MARIO_RUMBLE_12" },
-	{ 13, "MARIO_RUMBLE_13" },
-	{ 14, "MARIO_RUMBLE_14" },
-	{ 15, "MARIO_RUMBLE_15" },
-	{ 16, "MARIO_RUMBLE_16" },
-	{ 17, "MARIO_RUMBLE_17" },
-	{ 18, "MARIO_RUMBLE_18" },
-	{ 19, "MAPOBJ_APPEAR" },
-	{ 20, "MARIO_WEAK" },
-	{ 21, "MARIO_HARD" },
-	{ 22, "MARIO_ELECTRIC_DAMAGE" },
+struct RumbleTypeInfo _info[ 23 ] = {
+    { 0, "MARIO_HIPDROP" },
+    { 1, "MARIO_DAMAGE" },
+    { 2, "MARIO_LANDING" },
+    { 3, "MARIO_0_1SEC_FULL" },
+    { 4, "MARIO_HIP_ATTACK2" },
+    { 5, "MARIO_DAMAGE2" },
+    { 6, "MARIO_RUMBLE_06" },
+    { 7, "MARIO_RUMBLE_07" },
+    { 8, "MARIO_1SEC_SIN" },
+    { 9, "MARIO_1SEC_SIN_HALF" },
+    { 10, "MARIO_RUMBLE_10" },
+    { 11, "MARIO_RUMBLE_11" },
+    { 12, "MARIO_RUMBLE_12" },
+    { 13, "MARIO_RUMBLE_13" },
+    { 14, "MARIO_RUMBLE_14" },
+    { 15, "MARIO_RUMBLE_15" },
+    { 16, "MARIO_RUMBLE_16" },
+    { 17, "MARIO_RUMBLE_17" },
+    { 18, "MARIO_RUMBLE_18" },
+    { 19, "MAPOBJ_APPEAR" },
+    { 20, "MARIO_WEAK" },
+    { 21, "MARIO_HARD" },
+    { 22, "MARIO_ELECTRIC_DAMAGE" },
 };
 
-const char* invalidStr = "Invalid RumbleType";
+const char *invalidStr = "Invalid RumbleType";
 
-int RumbleType::getIndex(char* strIn)
-{
-	for (int i = 0; i < channelNum; i++) {
-		if (!strcmp(strIn, _info[i].str)) {
-			return _info[i].unk0;
-		}
-	}
-	return -1;
+int RumbleType::getIndex( char *strIn ) {
+    for ( int i = 0; i < channelNum; i++ ) {
+        if ( !strcmp( strIn, _info[ i ].str ) ) {
+            return _info[ i ].unk0;
+        }
+    }
+    return -1;
 }
 
 // Size needed: 0x54, current: 0x34
-const char* RumbleType::getName(int index)
-{
-	if (index < 0 || index >= channelNum) {
-		return invalidStr;
-	}
+const char *RumbleType::getName( int index ) {
+    if ( index < 0 || index >= channelNum ) {
+        return invalidStr;
+    }
 
-	return _info[index].str;
+    return _info[ index ].str;
 }
