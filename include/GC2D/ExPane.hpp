1c610ac9ca4187219993c70ee1873017df5de31f
Roman Sandu <roma.sandu@gmail.com>
2026-02-21 16:00:35 +0300

    (Fake)match & document ExPane & Coord2D

--- include/GC2D/ExPane.hpp
+++ include/GC2D/ExPane.hpp
@@ -19,52 +19,81 @@ inline static s16 clapAlpha(s16 alpha)
 	return a;
 }
 
+/**
+ * @brief Wrapper around J2DPane that supports animating the pane's position,
+ * size, and alpha over a specified number of frames.
+ */
 class TExPane {
 public:
 	TExPane(JUTTexture*, GXCullMode);
 	TExPane(J2DScreen*, u32);
 
+	/// Performs one frame of the animations.
 	bool update();
 
-	void setPaneOffset(s32 param_1, s32 param_2, s32 param_3, s32 param_4,
-	                   s32 param_5)
+	/// Initiates a pane's position animation.
+	void setPaneOffset(s32 time, s32 target_x, s32 target_y, s32 initial_x,
+	                   s32 initial_y)
 	{
-		unk14.setValue(param_1, param_2, param_3, param_4, param_5);
-		unk0->move(unk4.x1 + param_4, unk4.y1 + param_5);
-		unk4E = 1;
+		mOffsetInterpolator.setValue(time, target_x, target_y, initial_x,
+		                             initial_y);
+		mPane->move(mInitialBounds.x1 + initial_x,
+		            mInitialBounds.y1 + initial_y);
+		mOffsetAnimPending = true;
 	}
-	void setPaneSize(s32 param_1, s32 param_2, s32 param_3, s32 param_4,
-	                 s32 param_5)
-	{
-		unk14.setValue(param_1, param_2, param_3, param_4, param_5);
-		unk0->resize(unk4.x1, unk4.x1);
-		unk4F = 1;
-	}
-	void setPaneAlpha(s32 param_1, s16 param_2, s16 param_3)
-	{
-		unk0->setAlpha(clapAlpha(param_3));
 
-		unk44 = param_3;
-		unk48 = f32(param_2 - param_3) / param_1;
-		unk4C = param_2;
-		unk50 = true;
+	/// Initiates a pane's size animation.
+	void setPaneSize(s32 time, s32 target_w, s32 target_h, s32 initial_w,
+	                 s32 initial_h)
+	{
+		mSizeInterpolator.setValue(time, target_w, target_h, initial_w,
+		                           initial_h);
+		mPane->resize(initial_w, initial_h);
+		mSizeAnimPending = true;
+	}
+
+	/// Initiates a pane's alpha animation.
+	void setPaneAlpha(s32 time, s16 target_alpha, s16 initial_alpha)
+	{
+		mPane->setAlpha(clapAlpha(initial_alpha));
+
+		mCurrentAlpha     = initial_alpha;
+		mAlphaStep        = f32(target_alpha - initial_alpha) / time;
+		mTargetAlpha      = target_alpha;
+		mAlphaAnimPending = true;
 	}
 
 	// fabricated
-	J2DPane* getPane() const { return unk0; }
-	const JUTRect& getUnk4() const { return unk4; }
+	J2DPane* getPane() const { return mPane; }
+	const JUTRect& getInitialBounds() const { return mInitialBounds; }
+
+	bool isBoundsAnimationCompleted() const
+	{
+		bool result = false;
+		if (!mOffsetAnimPending && !mSizeAnimPending)
+			result = true;
+		return result;
+	}
+
+	bool isThing2() const
+	{
+		bool result = false;
+		if (isBoundsAnimationCompleted() && !mAlphaAnimPending)
+			result = true;
+		return result;
+	}
 
 public:
-	/* 0x0 */ J2DPane* unk0;
-	/* 0x4 */ JUTRect unk4;
-	/* 0x14 */ TCoord2D unk14;
-	/* 0x2C */ TCoord2D unk2C;
-	/* 0x44 */ f32 unk44;
-	/* 0x48 */ f32 unk48;
-	/* 0x4C */ s16 unk4C;
-	/* 0x4E */ bool unk4E;
-	/* 0x4F */ bool unk4F;
-	/* 0x50 */ bool unk50;
+	/* 0x0 */ J2DPane* mPane;
+	/* 0x4 */ JUTRect mInitialBounds;
+	/* 0x14 */ TCoord2D mOffsetInterpolator;
+	/* 0x2C */ TCoord2D mSizeInterpolator;
+	/* 0x44 */ f32 mCurrentAlpha;
+	/* 0x48 */ f32 mAlphaStep;
+	/* 0x4C */ s16 mTargetAlpha;
+	/* 0x4E */ bool mOffsetAnimPending;
+	/* 0x4F */ bool mSizeAnimPending;
+	/* 0x50 */ bool mAlphaAnimPending;
 };
 
 #endif
