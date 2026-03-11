8c00a2373736097f6b2fdc2f84ff6722fc343f61
Roman Sandu <roma.sandu@gmail.com>
2026-03-11 23:43:21 +0300

    BathtubKiller scaffolding

--- include/Enemy/BathtubKiller.hpp
+++ include/Enemy/BathtubKiller.hpp
@@ -1,23 +1,157 @@
-#ifndef BATHTUB_KILLER
-#define BATHTUB_KILLER
+#ifndef ENEMY_BATHTUB_KILLER_HPP
+#define ENEMY_BATHTUB_KILLER_HPP
 
-#include <Enemy/Enemy.hpp>
 #include <Enemy/SmallEnemy.hpp>
-#include <Enemy/Walker.hpp>
-#include <Enemy/Graph.hpp>
-#include <Enemy/Spider.hpp>
-#include <Enemy/Conductor.hpp>
+#include <Strategic/Nerve.hpp>
+
+class TBathtubKillerParams;
+class TBathtub;
+
+class TBathtubKillerPersonality {
+public:
+	TBathtubKillerPersonality();
+
+	void makeFast(const TBathtubKillerParams*);
+	void makeShine(const TBathtubKillerParams*);
+	void makeNormal(const TBathtubKillerParams*);
+};
+
+class TBathtubKillerParams : public TSmallEnemyParams {
+public:
+	TBathtubKillerParams(const char* name = "バスタブキラー");
+
+	/* 0x2d4 */ TParamRT<f32> fastAccelerationQuatRate;
+	/* 0x2e8 */ TParamRT<f32> fastChaseAcceleration;
+	/* 0x2fc */ TParamRT<f32> fastChaseSpeed;
+	/* 0x310 */ TParamRT<f32> fastInitialSpeed;
+	/* 0x324 */ TParamRT<s32> fastDeadPeriod;
+	/* 0x338 */ TParamRT<f32> shineAccelerationQuatRate;
+	/* 0x34c */ TParamRT<f32> shineChaseAcceleration;
+	/* 0x360 */ TParamRT<f32> shineChaseSpeed;
+	/* 0x374 */ TParamRT<f32> shineInitialSpeed;
+	/* 0x388 */ TParamRT<s32> shineDeadPeriod;
+	/* 0x39c */ TParamRT<f32> mushroomProbability;
+	/* 0x3b0 */ TParamRT<f32> mSLColorChangeRateDelta;
+	/* 0x3c4 */ TParamRT<f32> mSLAccelerationQuatRate;
+	/* 0x3d8 */ TParamRT<f32> mSLChaseAcceleration;
+	/* 0x3ec */ TParamRT<f32> mSLChaseSpeed;
+	/* 0x400 */ TParamRT<f32> mSLInitialSpeed;
+	/* 0x414 */ TParamRT<s32> mSLDeadPeriod;
+	/* 0x428 */ TParamRT<f32> mSLStraightDistance;
+	/* 0x43c */ TParamRT<f32> mSLChaseMinY;
+	/* 0x450 */ TParamRT<f32> mSLChaseMaxY;
+	/* 0x464 */ TParamRT<f32> mSLAboidDistanceY;
+	/* 0x478 */ TParamRT<f32> mSLAboidDistance;
+	/* 0x48c */ TParamRT<f32> mSLChaseDistanceY;
+	/* 0x4a0 */ TParamRT<f32> mSLChaseDistance;
+	/* 0x4b4 */ TParamRT<f32> mSLTrampleVelocity;
+	/* 0x4c8 */ TParamRT<f32> mSLFlyingSpeedMax;
+	/* 0x4dc */ TParamRT<f32> mSLFlyingGravityY;
+	/* 0x4f0 */ TParamRT<f32> mSLBombRange;
+	/* 0x504 */ TParamRT<f32> aboidAngle;
+	/* 0x518 */ TParamRT<s32> mSLChaseStraightPeriod;
+	/* 0x52c */ TParamRT<s32> mSLSmokeInterval;
+	/* 0x540 */ TParamRT<s32> mSLLaunchingPeriod;
+	/* 0x554 */ TParamRT<s32> noCollisionAmongKillers;
+};
 
 class TBathtubKiller : public TSmallEnemy {
 public:
 	TBathtubKiller(const char* name = "バスタブキラー");
 
-	void makeQuat(JGeometry::TVec3<f32> axis, float moveAmountY,
-	              float moveAmountX);
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+	virtual void init(TLiveManager*);
+	virtual void calcRootMatrix();
+	virtual void bind();
+	virtual f32 getGravityY() const;
+	virtual const char** getBasNameTable() const;
+	virtual void reset();
+	virtual void behaveToWater(THitActor*);
+	virtual void attackToMario();
+	virtual void setMActorAndKeeper();
+	virtual bool isCollidMove(THitActor*);
 
-	/* 0x194 */ u8 unk194[0x1AC - 0x194];
+	void resetBathtubKiller();
+	void generateItemBathtubKiller();
+	void killBathtubKiller();
+	void breakBathtubKiller();
+	void explodeBathtubKiller();
+	void makeNoseColor();
+	f32 getBathtubY();
+	void makeInitialVelocity(JGeometry::TVec3<f32>);
+	void moveParabolic();
+	void moveChasing();
+	void moveStraight();
+	void makeVelocityQuat();
+	void makeAccelerationQuat();
+	void makeQuat(JGeometry::TVec3<f32>, f32, f32);
+	void makeScrewQuat(JGeometry::TVec3<f32>, f32, f32);
+	void setNormalBathtubKillerAnm();
+	void setChaseBathtubKillerAnm();
+	void setStraightBathtubKillerAnm();
+	void setDeadBathtubKillerAnm();
+	void updateTimers();
+	bool isAttackable();
+	bool isAboided();
+	bool canChase();
+	void generateExplosion();
+
+	// fabricated
+	TBathtubKillerParams* getSaveParam2() const
+	{
+		return (TBathtubKillerParams*)getSaveParam();
+	}
+
+public:
+	/* 0x194 */ u8 unk194;
+	/* 0x198 */ f32 unk198;
+	/* 0x19C */ f32 unk19C;
+	/* 0x1A0 */ f32 unk1A0;
+	/* 0x1A4 */ f32 unk1A4;
+	/* 0x1A8 */ s32 unk1A8;
 	/* 0x1AC */ JGeometry::TQuat4<f32> mQuat;
-	/* 0x1BC */ u8 unk1BC[0x250 - 0x1BC];
+	/* 0x1BC */ JGeometry::TVec3<f32> unk1BC;
+	/* 0x1C8 */ char unk1C8[4];
+	/* 0x1CC */ TBathtub* unk1CC;
+	/* 0x1D0 */ char unk1D0[4];
+	/* 0x1D4 */ int unk1D4;
+	/* 0x1D8 */ GXColorS10 unk1D8;
+	/* 0x1E0 */ GXColorS10 unk1E0;
+	/* 0x1E8 */ GXColorS10 unk1E8;
+	/* 0x1F0 */ GXColorS10 unk1F0;
+	/* 0x1F8 */ f32 unk1F8;
+	/* 0x1FC */ f32 unk1FC;
+	/* 0x200 */ f32 unk200;
+	/* 0x204 */ f32 unk204;
+	/* 0x208 */ int unk208;
+	/* 0x20C */ int unk20C;
+	/* 0x210 */ int unk210;
+	/* 0x214 */ int unk214;
+	/* 0x218 */ int unk218;
+	/* 0x21C */ u32 unk21C;
+	/* 0x220 */ TMtx34f unk220;
+};
+
+DECLARE_NERVE(TNerveBathtubKillerWander, TLiveActor);
+DECLARE_NERVE(TNerveBathtubKillerChase, TLiveActor);
+DECLARE_NERVE(TNerveBathtubKillerChaseStraight, TLiveActor);
+DECLARE_NERVE(TNerveBathtubKillerStraight, TLiveActor);
+DECLARE_NERVE(TNerveBathtubKillerBreak, TLiveActor);
+DECLARE_NERVE(TNerveBathtubKillerExplosion, TLiveActor);
+
+class TBathtubKillerManager : public TSmallEnemyManager {
+public:
+	TBathtubKillerManager(const char* name = "バスタブキラーマネージャー");
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual void loadAfter();
+	virtual void createModelData();
+	virtual TSpineEnemy* createEnemyInstance();
+
+	void generateMushroom(JGeometry::TVec3<f32>);
+	int countActiveKillers();
+	int countActiveShineKillers();
 };
 
 #endif
--- src/Enemy/BathtubKiller.cpp
+++ src/Enemy/BathtubKiller.cpp
@@ -1,7 +1,144 @@
 #include <Enemy/BathtubKiller.hpp>
+#include <Strategic/ObjModel.hpp>
 
-void TBathtubKiller::makeQuat(JGeometry::TVec3<f32> axis, float moveAmountY,
-                              float moveAmountX)
+// rogue includes needed for matching sinit & bss
+#include <MSound/MSSetSound.hpp>
+#include <MSound/MSoundBGM.hpp>
+#include <M3DUtil/InfectiousStrings.hpp>
+
+static const char* bathtubkiller_bastable[] = {
+	"/scene/bathtubkiller/bas/bathtubdownkiller_down1.bas",
+	nullptr,
+	nullptr,
+};
+
+TBathtubKillerPersonality::TBathtubKillerPersonality() { }
+
+void TBathtubKillerPersonality::makeFast(const TBathtubKillerParams*) { }
+
+void TBathtubKillerPersonality::makeShine(const TBathtubKillerParams*) { }
+
+void TBathtubKillerPersonality::makeNormal(const TBathtubKillerParams*) { }
+
+TBathtubKillerParams::TBathtubKillerParams(const char* prm)
+    : TSmallEnemyParams(prm)
+    , PARAM_INIT(fastAccelerationQuatRate, 0.0f)
+    , PARAM_INIT(fastChaseAcceleration, 5.0f)
+    , PARAM_INIT(fastChaseSpeed, 40.0f)
+    , PARAM_INIT(fastInitialSpeed, 15.0f)
+    , PARAM_INIT(fastDeadPeriod, 1800)
+    , PARAM_INIT(shineAccelerationQuatRate, 0.0f)
+    , PARAM_INIT(shineChaseAcceleration, 5.0f)
+    , PARAM_INIT(shineChaseSpeed, 40.0f)
+    , PARAM_INIT(shineInitialSpeed, 15.0f)
+    , PARAM_INIT(shineDeadPeriod, 1800)
+    , PARAM_INIT(mushroomProbability, 0.0f)
+    , PARAM_INIT(mSLColorChangeRateDelta, 0.0f)
+    , PARAM_INIT(mSLAccelerationQuatRate, 0.0f)
+    , PARAM_INIT(mSLChaseAcceleration, 5.0f)
+    , PARAM_INIT(mSLChaseSpeed, 40.0f)
+    , PARAM_INIT(mSLInitialSpeed, 15.0f)
+    , PARAM_INIT(mSLDeadPeriod, 1800)
+    , PARAM_INIT(mSLStraightDistance, 0.0f)
+    , PARAM_INIT(mSLChaseMinY, 0.0f)
+    , PARAM_INIT(mSLChaseMaxY, 0.0f)
+    , PARAM_INIT(mSLAboidDistanceY, 0.0f)
+    , PARAM_INIT(mSLAboidDistance, 0.0f)
+    , PARAM_INIT(mSLChaseDistanceY, 5.0f)
+    , PARAM_INIT(mSLChaseDistance, 5.0f)
+    , PARAM_INIT(mSLTrampleVelocity, 40.0f)
+    , PARAM_INIT(mSLFlyingSpeedMax, 0.0f)
+    , PARAM_INIT(mSLFlyingGravityY, 5.0f)
+    , PARAM_INIT(mSLBombRange, 1.0f)
+    , PARAM_INIT(aboidAngle, 10.0f)
+    , PARAM_INIT(mSLChaseStraightPeriod, 360)
+    , PARAM_INIT(mSLSmokeInterval, 3)
+    , PARAM_INIT(mSLLaunchingPeriod, 360)
+    , PARAM_INIT(noCollisionAmongKillers, 360)
+{
+	TParams::load(mPrmPath);
+
+	fastAccelerationQuatRate.set(0.03f);
+	fastChaseAcceleration.set(0.1f);
+	fastChaseSpeed.set(10.0f);
+	fastInitialSpeed.set(14.0f);
+	fastDeadPeriod.set(720);
+	shineAccelerationQuatRate.set(0.03f);
+	shineChaseAcceleration.set(0.03f);
+	shineChaseSpeed.set(7.0f);
+	shineInitialSpeed.set(12.0f);
+	shineDeadPeriod.set(2400);
+	mSLAccelerationQuatRate.set(0.03f);
+	mSLChaseAcceleration.set(0.1f);
+	mSLChaseSpeed.set(8.0f);
+	mSLInitialSpeed.set(12.0f);
+	mSLDeadPeriod.set(1440);
+	mSLColorChangeRateDelta.set(0.16f);
+	mSLStraightDistance.set(400.0f);
+	mSLChaseMinY.set(50.0f);
+	mSLChaseMaxY.set(100.0f);
+	mSLAboidDistanceY.set(100.0f);
+	mSLAboidDistance.set(100.0f);
+	mSLChaseDistanceY.set(400.0f);
+	mSLDamageRadius.set(120);
+	mSLDamageHeight.set(100);
+	mSLAttackRadius.set(100);
+	mSLAttackHeight.set(90);
+	mSLTrampleVelocity.set(500.0f);
+	mSLFlyingSpeedMax.set(200.0f);
+	mSLFlyingGravityY.set(0.06f);
+	mSLBombRange.set(500.0f);
+	aboidAngle.set(5.0f);
+	mSLChaseStraightPeriod.set(420);
+	mSLSmokeInterval.set(4);
+	mSLLaunchingPeriod.set(240);
+	noCollisionAmongKillers.set(480);
+	mushroomProbability.set(0.3f);
+}
+
+TBathtubKiller::TBathtubKiller(const char* name)
+    : TSmallEnemy(name)
+{
+}
+
+void TBathtubKiller::init(TLiveManager*) { }
+
+void TBathtubKiller::setMActorAndKeeper() { }
+
+void TBathtubKiller::reset() { }
+
+void TBathtubKiller::resetBathtubKiller() { }
+
+void TBathtubKiller::generateItemBathtubKiller() { }
+
+void TBathtubKiller::killBathtubKiller() { }
+
+void TBathtubKiller::breakBathtubKiller() { }
+
+void TBathtubKiller::explodeBathtubKiller() { }
+
+void TBathtubKiller::bind() { }
+
+void TBathtubKiller::perform(u32, JDrama::TGraphics*) { }
+
+void TBathtubKiller::makeNoseColor() { }
+
+f32 TBathtubKiller::getBathtubY() { return 0.0f; }
+
+void TBathtubKiller::makeInitialVelocity(JGeometry::TVec3<f32>) { }
+
+void TBathtubKiller::moveParabolic() { }
+
+void TBathtubKiller::moveChasing() { }
+
+void TBathtubKiller::moveStraight() { }
+
+void TBathtubKiller::makeVelocityQuat() { }
+
+void TBathtubKiller::makeAccelerationQuat() { }
+
+void TBathtubKiller::makeQuat(JGeometry::TVec3<f32> axis, f32 moveAmountY,
+                              f32 moveAmountX)
 {
 	JGeometry::TVec3<f32> normAxis = axis;
 	normAxis.normalize();
@@ -33,4 +170,83 @@ void TBathtubKiller::makeQuat(JGeometry::TVec3<f32> axis, float moveAmountY,
 	}
 
 	static_cast<JGeometry::TVec4<f32>&>(mQuat).normalize();
+}
+
+void TBathtubKiller::makeScrewQuat(JGeometry::TVec3<f32>, f32, f32) { }
+
+f32 TBathtubKiller::getGravityY() const { return 0.0f; }
+
+void TBathtubKiller::calcRootMatrix() { }
+
+BOOL TBathtubKiller::receiveMessage(THitActor*, u32) { return false; }
+
+void TBathtubKiller::attackToMario() { }
+
+bool TBathtubKiller::isCollidMove(THitActor*) { return false; }
+
+void TBathtubKiller::behaveToWater(THitActor*) { }
+
+const char** TBathtubKiller::getBasNameTable() const
+{
+	return bathtubkiller_bastable;
+}
+
+void TBathtubKiller::setNormalBathtubKillerAnm() { }
+
+void TBathtubKiller::setChaseBathtubKillerAnm() { }
+
+void TBathtubKiller::setStraightBathtubKillerAnm() { }
+
+void TBathtubKiller::setDeadBathtubKillerAnm() { }
+
+void TBathtubKiller::updateTimers() { }
+
+bool TBathtubKiller::isAttackable() { return false; }
+
+bool TBathtubKiller::isAboided() { return false; }
+
+bool TBathtubKiller::canChase() { return false; }
+
+void TBathtubKiller::generateExplosion() { }
+
+DEFINE_NERVE(TNerveBathtubKillerWander, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveBathtubKillerChase, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveBathtubKillerChaseStraight, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveBathtubKillerStraight, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveBathtubKillerBreak, TLiveActor) { return FALSE; }
+
+DEFINE_NERVE(TNerveBathtubKillerExplosion, TLiveActor) { return FALSE; }
+
+TBathtubKillerManager::TBathtubKillerManager(const char* name)
+    : TSmallEnemyManager(name)
+{
+}
+
+void TBathtubKillerManager::load(JSUMemoryInputStream&) { }
+
+void TBathtubKillerManager::loadAfter() { }
+
+void TBathtubKillerManager::generateMushroom(JGeometry::TVec3<f32>) { }
+
+int TBathtubKillerManager::countActiveKillers() { return 0; }
+
+int TBathtubKillerManager::countActiveShineKillers() { return 0; }
+
+void TBathtubKillerManager::createModelData()
+{
+	static TModelDataLoadEntry entry[] = {
+		{ "bathtubkiller_model1.bmd", 0x50230000, 0 },
+		{ "bathtubdownkiller_model1.bmd", 0x50210000, 0 },
+		{ nullptr, 0, 0 },
+	};
+	createModelDataArray(entry);
+}
+
+TSpineEnemy* TBathtubKillerManager::createEnemyInstance()
+{
+	return new TBathtubKiller;
 }
