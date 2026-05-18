98eea2ca1740ddbc08cef909f32f05961ee9f99a
theAzack9 <robin@cavachi.be>
2026-05-18 18:33:14 +0200

    MapObjBlock mostly matching

--- include/MoveBG/MapObjBlock.hpp
+++ include/MoveBG/MapObjBlock.hpp
@@ -4,6 +4,9 @@
 #include <MoveBG/MapObjHide.hpp>
 #include <MoveBG/MapObjGeneral.hpp>
 
+#include <MarioUtil/MathUtil.hpp>
+#include <MSound/MSound.hpp>
+
 // TODO: mark virtual methods as such
 class TSmallEnemy;
 
@@ -18,10 +21,25 @@ public:
 
 class TSandBlock : public TMapObjBase {
 public:
+	// Fabricated
+	typedef enum ESandBlockStates {
+		Waiting   = 1,
+		Restoring = 2,
+		Touched   = 3,
+		Falling   = 4,
+		Gone      = 5
+	};
+
 	TSandBlock(const char* name = "砂ブロック");
 	void initMapObj();
 	void control();
 	void touchPlayer(THitActor*);
+
+	static u32 mWaitTimeToFall;
+	static f32 mSandScaleUp;
+	static f32 mSandScaleDown;
+	static f32 mSandScaleMin;
+	static u32 mSandWaitTime;
 };
 
 class TLeanBlock : public TMapObjBase {
@@ -35,6 +53,9 @@ public:
 
 	void calcLeanMtx(MtxPtr);
 
+	// Fabricated
+	JGeometry::SMatrix34C<f32>& getDefaultMtx() { return mDefaultMtx; }
+
 public:
 	/* 0x138 */ f32 unk138;
 	/* 0x13C */ f32 unk13C;
@@ -43,7 +64,7 @@ public:
 	/* 0x148 */ f32 unk148;
 	/* 0x14C */ JGeometry::TVec3<f32> unk14C;
 	/* 0x158 */ JGeometry::TVec3<f32> unk158;
-	/* 0x164 */ Mtx unk164;
+	/* 0x164 */ JGeometry::SMatrix34C<f32> mDefaultMtx;
 };
 
 class TIceBlock : public TMapObjBase {
@@ -55,6 +76,10 @@ public:
 
 	u32 touchWater(THitActor*);
 	u32 getSDLModelFlag() const;
+
+	static f32 mMeltSpeedWater;
+	static f32 mMeltSpeedAuto;
+	static f32 mAutoMeltScale;
 };
 
 class TBrickBlock : public THideObjBase {
@@ -81,7 +106,7 @@ public:
 	JGeometry::TVec3<f32>& getUnk140() { return unk140; }
 
 public:
-	/* 0x138 */ S16Vec unk138;
+	/* 0x138 */ GXColorS10 unk138;
 	/* 0x140 */ JGeometry::TVec3<f32> unk140;
 	/* 0x14C */ TSmallEnemy* unk14C;
 };
@@ -103,6 +128,9 @@ public:
 	TSuperHipDropBlock(const char* name = "スーパーヒップドロップブロック");
 	void loadAfter();
 	BOOL receiveMessage(THitActor* sender, u32 message);
+
+public:
+	/* 0x150 */ bool mMonteBlockBroken;
 };
 
 #endif
