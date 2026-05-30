c1ea2d13503697e4194819505331bac3cecb4a73
Roman Sandu <roma.sandu@gmail.com>
2026-05-30 03:01:50 +0300

    Atom.cpp free OK

--- src/Player/Atom.cpp
+++ src/Player/Atom.cpp
@@ -1,1 +1,9 @@
 
+#include <Map/MapCollisionEntry.hpp>
+#include <JSystem/JGeometry/JGVec3.hpp>
+
+// NOTE: file with debug stuff?
+
+static JGeometry::TVec3<f32> cDeformedTerrainCenter(0.0f, 5000.0f, 0.0f);
+
+void dummy(TMapCollisionBase* col) { col->setUp(); }
