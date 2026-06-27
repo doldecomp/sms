191d33306f0fb953abd97847e0b3ee20d203ca22
Roman Sandu <roma.sandu@gmail.com>
2026-06-27 18:07:10 +0300

    Wire stuff closer

--- include/Enemy/WireBinder.hpp
+++ include/Enemy/WireBinder.hpp
@@ -5,22 +5,29 @@
 #include <Strategic/Binder.hpp>
 #include <Strategic/LiveActor.hpp>
 
+class TMapWire;
+
 class TWireBinder : TBinder {
 public:
 	bool init(const JGeometry::TVec3<f32>&);
+	bool reset(const JGeometry::TVec3<f32>&);
+
 	void bind(TLiveActor*);
 	JGeometry::TVec3<f32> getDir() const { return mDir; }
 	JGeometry::TVec3<f32> getDirAtPos(const JGeometry::TVec3<f32>&, f32) const;
 	void getPoint(JGeometry::TVec3<f32>*, f32) const;
 	void getPoint(JGeometry::TVec3<f32>*, const JGeometry::TVec3<f32>&) const;
-	bool isEndWire(const JGeometry::TVec3<float>&, float) const;
 
 	static bool isOnWire(const JGeometry::TVec3<f32>&);
-
-	~TWireBinder();
+	f32 getRangePos(const JGeometry::TVec3<f32>&) const;
+	TMapWire* getWire() const;
+	void isStartWire(const JGeometry::TVec3<f32>&, f32) const;
+	bool isEndWire(const JGeometry::TVec3<f32>&, f32) const;
+	void getStartRangePos(f32);
+	void getEndRangePos(f32);
 
 private:
-	/* 0x04 */ u32 mWireNumber;
+	/* 0x04 */ s32 mWireNumber;
 	/* 0x08 */ JGeometry::TVec3<f32> mDir;
 };
 
--- include/Map/MapWireManager.hpp
+++ include/Map/MapWireManager.hpp
@@ -2,39 +2,42 @@
 #define MAP_MAP_WIRE_MANAGER_HPP
 
 #include <JSystem/JDrama/JDRViewObj.hpp>
+#include <Map/MapWire.hpp>
 #include <Strategic/TakeActor.hpp>
 
 class TMapWireActorManager;
-class TMapWire;
 
 class TMapWireActor : public TTakeActor {
 public:
-	MtxPtr getTakingMtx() { return nullptr; }
+	TMapWireActor(const char*);
+
+	virtual BOOL receiveMessage(THitActor* sender, u32 message);
+	virtual MtxPtr getTakingMtx() { return nullptr; }
+
 	void checkTakingActor();
 	f32 getPosInWire() const;
 	void getTipPoints(JGeometry::TVec3<f32>*, JGeometry::TVec3<f32>*) const;
-	BOOL receiveMessage(THitActor* sender, u32 message);
 	void init(TMapWireActorManager*);
-	TMapWireActor(const char*);
 
 	static f32 mCommonAttackRadius;
 	static f32 mCommonAttackHeight;
 
 public:
 	/* 0x70 */ u8 unk70;
-	/* 0x74 */ void* unk74;
+	/* 0x74 */ TMapWireActorManager* unk74;
 };
 
 class TMapWireActorManager {
 public:
+	TMapWireActorManager(TTakeActor*);
+
 	void doActorToWire();
 	void doWireToActor();
-	TMapWireActorManager(TTakeActor*);
 
 public:
 	/* 0x0 */ TTakeActor* unk0;
 	/* 0x4 */ TMapWireActor unk4;
-	/* 0x7C */ u32 unk7C;
+	/* 0x7C */ TMapWire* unk7C;
 };
 
 class TMapWireManager;
@@ -43,32 +46,29 @@ TMapWireManager* gpMapWireManager;
 
 class TMapWireManager : public JDrama::TViewObj {
 public:
-	TMapWire* getWire(int index) const;
+	TMapWireManager(const char* name = "ワイヤー管理");
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual void loadAfter();
+	virtual void perform(u32, JDrama::TGraphics*);
+
 	u32 getWireNo(const JGeometry::TVec3<f32>&) const;
 	void getPointPosInNthWire(int, const JGeometry::TVec3<f32>&,
 	                          JGeometry::TVec3<f32>*) const;
 	void getPointPosInWire(const JGeometry::TVec3<f32>&,
 	                       JGeometry::TVec3<f32>*) const;
-	void perform(u32, JDrama::TGraphics*);
 	void entry(TTakeActor*);
-	void loadAfter();
-	void load(JSUMemoryInputStream&);
-	TMapWireManager(const char* name = "ワイヤー管理");
+
+	TMapWire* getWire(int index) const { return unk18[index]; }
 
 	static JUtility::TColor mUpperSurface;
 	static JUtility::TColor mLowerSurface;
-
-	// fabricated
-	static TMapWire* getGlobalWire(int index)
-	{
-		return gpMapWireManager->unk18[index];
-	}
 
 public:
 	/* 0x10 */ int unk10;
 	/* 0x14 */ u32 unk14;
 	/* 0x18 */ TMapWire** unk18;
-	/* 0x1C */ u32 unk1C;
+	/* 0x1C */ int unk1C;
 	/* 0x20 */ u32 unk20;
 	/* 0x24 */ TMapWireActorManager** unk24;
 	/* 0x28 */ u16 unk28;
