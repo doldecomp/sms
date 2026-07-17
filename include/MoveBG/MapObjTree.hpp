e9d3916885a6bb13c5dd3e57ac05895863414479
theAzack9 <robin@cavachi.be>
2026-07-17 11:28:14 +0200

    MapObjTree mostly matching

--- include/MoveBG/MapObjTree.hpp
+++ include/MoveBG/MapObjTree.hpp
@@ -8,20 +8,26 @@ public:
 	TMapObjLeaf();
 
 public:
-	/* 0x0 */ f32 unk0;
-	/* 0x4 */ f32 unk4;
-	/* 0x8 */ TMapCollisionMove* unk8;
-	/* 0xC */ JGeometry::SMatrix34C<f32> unkC;
+	/* 0x0 */ f32 unk0;                // Leaf angle
+	/* 0x4 */ f32 unk4;                // Leaf angular velocity
+	/* 0x8 */ TMapCollisionMove* unk8; // Leaf collision
+	/* 0xC */ JGeometry::TMatrix34<JGeometry::SMatrix34C<f32> >
+	    unkC; // Base matrix
 };
 
 class TMapObjTree : public TMapObjGeneral {
 public:
 	virtual void perform(u32 cue, JDrama::TGraphics* graphics);
-	virtual f32 getRadiusAtY(f32) const;
+	virtual f32 getRadiusAtY(f32 param_1) const
+	{
+		return unk148
+		       + (unk14C - unk148) * (mPosition.y + mDamageHeight - param_1)
+		             / mDamageHeight;
+	}
 	virtual void initMapObj();
 	virtual void touchPlayer(THitActor*);
 
-	void controlLeaf(int);
+	int controlLeaf(int);
 	void initEach();
 	TMapObjTree(const char* name = "木");
 
@@ -30,25 +36,39 @@ public:
 	static f32 mBananaTreeJumpPower;
 
 public:
-	/* 0x148 */ f32 unk148;
-	/* 0x14C */ f32 unk14C;
-	/* 0x150 */ s32 unk150;
-	/* 0x154 */ TMapObjLeaf* unk154;
-	/* 0x158 */ bool unk158;
-	/* 0x15C */ f32 unk15C;
-	/* 0x160 */ f32 unk160;
-	/* 0x164 */ f32 unk164;
-	/* 0x168 */ f32 unk168;
+	/* 0x148 */ f32 unk148;          // Upper canopy radius
+	/* 0x14C */ f32 unk14C;          // Lower canopy radius
+	/* 0x150 */ s32 unk150;          // Leaf count
+	/* 0x154 */ TMapObjLeaf* unk154; // Leaf data
+	/* 0x158 */ bool unk158; // Probably true when all leaf motion has settled
+	/* 0x15C */ f32 unk15C;  // Probably standard leaf movement speed
+	/* 0x160 */ f32 unk160;  // Hip-drop leaf movement speed
+	/* 0x164 */ f32 unk164;  // Leaf return strength
+	/* 0x168 */ f32 unk168;  // Leaf damping
 	/* 0x16C */ u32 unk16C;
 };
 
-class TMapObjTreeScale : public TMapObjGeneral {
+class TMapEventSink;
+
+class TMapObjTreeScale : public TMapObjTree {
 public:
+	enum {
+		STATE_UNKB = 0xB, // Fully shrunken
+		STATE_UNKC = 0xC, // Growing vertically
+		STATE_UNKD = 0xD, // Growing horizontally
+	};
+
 	virtual void loadAfter();
 	virtual void control();
 	virtual u32 touchWater(THitActor*);
 
-	void startScaleUp();
+	void startScaleUp()
+	{
+		awake();
+		mActorType = 0x40000039;
+		removeMapCollision();
+		mState = STATE_UNKC;
+	}
 	void beSmall();
 	TMapObjTreeScale(const char* name = "スケールの木");
 
@@ -56,6 +76,12 @@ public:
 	static f32 mStatusChangeScaleY;
 	static f32 mScaleSpeedXZ;
 	static f32 mScaleMin;
+
+public:
+	/* 0x170 */ JGeometry::TVec3<f32> unk170[30]; // Particle positions
+	/* 0x2D8 */ s32 unk2D8;                       // Index into unk170
+	/* 0x2DC */ s32 unk2DC;                       // Particle timer
+	/* 0x2E0 */ TMapEventSink* unk2E0; // Bianco terrain-sinking event
 };
 
 #endif
--- src/MoveBG/MapObjTree.cpp
+++ src/MoveBG/MapObjTree.cpp
@@ -1,39 +1,107 @@
 #include <MoveBG/MapObjTree.hpp>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+#include <Camera/CameraShake.hpp>
 #include <Map/MapCollisionManager.hpp>
 #include <Map/MapCollisionEntry.hpp>
 #include <Map/MapData.hpp>
+#include <Map/MapEventSink.hpp>
+#include <Map/PollutionManager.hpp>
+#include <MarioUtil/PacketUtil.hpp>
+#include <MarioUtil/RumbleMgr.hpp>
+#include <MSound/MSound.hpp>
 #include <Player/MarioAccess.hpp>
+#include <System/EmitterViewObj.hpp>
+#include <System/MarDirector.hpp>
 
 #include <JSystem/J3D/J3DGraphAnimator/J3DModel.hpp>
+#include <JSystem/JDrama/JDRNameRefGen.hpp>
+
+static int sWaitTime                      = 1;
+f32 TMapObjTreeScale::mScaleMin           = 0.1f;
+f32 TMapObjTreeScale::mScaleSpeedXZ       = 0.007f;
+f32 TMapObjTreeScale::mStatusChangeScaleY = 0.3f;
+f32 TMapObjTreeScale::mScaleSpeedY        = 0.005f;
+f32 TMapObjTree::mBananaTreeJumpPower     = 1000.0f;
 
 TMapObjLeaf::TMapObjLeaf()
     : unk0(0.0f)
     , unk4(0.0f)
     , unk8(nullptr)
 {
-	unkC.set(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-	         1.0f);
+	unkC.identity();
 	unk8 = new TMapCollisionMove();
 }
 
 void TMapObjTree::touchPlayer(THitActor* player)
 {
-	unk158   = 0;
-	s16 data = gpMarioGroundPlane[0]->getData();
-	if (!(gpMarioGroundPlane[0]->getActor() == this && data >= 0
+	unk158   = false;
+	s16 data = SMS_GetMarioGroundPlane()->getData();
+	if (!(SMS_GetMarioGroundPlane()->getActor() == this && data >= 0
 	      && data < unk150)) {
 		return;
 	}
 	if (marioHipAttack()) {
-		unk154[data].unk4 = unk154[data].unk4 + unk160;
-	} else {
-		if (marioIsOn()) {
-			unk154[data].unk4 = unk154[data].unk4 + unk15C;
-		}
+		unk154[data].unk4 += unk160;
+	} else if (marioIsOn()) {
+		unk154[data].unk4 += unk15C;
 	}
+}
+
+int TMapObjTree::controlLeaf(int index)
+{
+	TMapObjLeaf& leaf = unk154[index];
+
+	if (leaf.unk4 == 0.0f) {
+		// This feels wrong, probably wrapper function?
+		// Was likely not SMS_GetMarioSpeedY since that takes a THitActor
+		if (*gpMarioSpeedY <= 0.0f) {
+			JGeometry::TMatrix34<JGeometry::SMatrix34C<f32> > mtx;
+			mtx.set(leaf.unkC);
+			leaf.unk8->moveMtx(mtx);
+		}
+		return 1;
+	}
+
+	leaf.unk0 += leaf.unk4;
+	leaf.unk4 -= leaf.unk0 * unk164;
+	leaf.unk4 *= unk168;
+
+	JGeometry::TMatrix34<JGeometry::SMatrix34C<f32> > rotation;
+	rotation.identity();
+	JGeometry::TMatrix34<JGeometry::SMatrix34C<f32> > mtx;
+	JGeometry::TVec3<f32> axis(1.0f, 0.0f, 0.0f);
+	PSMTXRotAxisRad(rotation, axis, leaf.unk0);
+
+	mtx.set(leaf.unkC);
+	MTXConcat(mtx, rotation, mtx);
+	MTXCopy(mtx, getModel()->getAnmMtx(unk150 - index));
+
+	if (*gpMarioSpeedY <= 0.0f)
+		leaf.unk8->moveMtx(mtx);
+
+	// TODO: There is a weird bge thing here
+	// I could not get a unary matching, probably skill issue
+	if (fabs(leaf.unk0) < unk15C)
+		return 1;
+	return 0;
+}
+
+void TMapObjTree::perform(u32 cue, JDrama::TGraphics* graphics)
+{
+	if (!unk158 && (cue & CUE_MOVE)) {
+		int count = 0;
+		for (int i = 0; i < unk150; ++i)
+			count += controlLeaf(i);
+
+		if (mColCount == 0 && count == unk150)
+			unk158 = true;
+	}
+
+	TMapObjGeneral::perform(cue, graphics);
 }
 
 void TMapObjTree::initEach()
@@ -97,7 +165,7 @@ void TMapObjTree::initMapObj()
 	for (s32 i = 0; i < unk150; ++i) {
 		TMapObjLeaf& leaf = unk154[i];
 		leaf.unk8         = new TMapCollisionMove();
-		char buffer[72];
+		char buffer[64];
 		if (isActorType(0x40000038)) {
 			snprintf(buffer, 0x100, "/mapObj/palmLeaf%02d", i + 1);
 		} else {
@@ -108,13 +176,154 @@ void TMapObjTree::initMapObj()
 		leaf.unk8->remove();
 
 		leaf.unkC.set(getModel()->getAnmMtx(unk150 - i));
-		TMapCollisionMove* move = leaf.unk8;
-		// Possibly inline?
-		PSMTXCopy(leaf.unkC, (MtxPtr)move->unk20);
-		move->setUp();
+		TMapCollisionMove* collision = leaf.unk8;
+		collision->setMtx(leaf.unkC);
+		collision->setUp();
 	}
 
 	if (getMapCollisionManager() != nullptr) {
 		getMapCollisionManager()->unk10 = nullptr;
 	}
+}
+
+TMapObjTree::TMapObjTree(const char* name)
+    : TMapObjGeneral(name)
+    , unk148(0.0f)
+    , unk14C(0.0f)
+    , unk150(0)
+    , unk154(nullptr)
+    , unk158(true)
+    , unk15C(0.0f)
+    , unk160(0.0f)
+    , unk164(0.0f)
+    , unk168(0.0f)
+    , unk16C(0)
+{
+}
+
+u32 TMapObjTreeScale::touchWater(THitActor* water)
+{
+	if (mScaling.x == 1.0f)
+		return TMapObjGeneral::touchWater(water);
+
+	if (isState(STATE_UNKB))
+		startScaleUp();
+
+	return 1;
+}
+
+void TMapObjTreeScale::control()
+{
+	switch (mState) {
+	case STATE_UNKB:
+		if (gpMarDirector->getCurrentMap() == 4)
+			break;
+		if (gpPollution->isPolluted(mPosition.x, mPosition.y, mPosition.z))
+			break;
+		startScaleUp();
+		break;
+
+	case STATE_UNKC:
+		gpMSound->startSoundActor(MSD_SE_OBJ_TREE_APPEAR, &mPosition, 0,
+		                          nullptr, 0, 4);
+		mScaling.y += mScaleSpeedY;
+		if (mScaling.y > mStatusChangeScaleY)
+			mState = STATE_UNKD;
+		break;
+
+	case STATE_UNKD:
+		gpMSound->startSoundActor(MSD_SE_OBJ_TREE_APPEAR, &mPosition, 0,
+		                          nullptr, 0, 4);
+		if (mScaling.y < 1.0f)
+			mScaling.y += mScaleSpeedY;
+		else
+			mScaling.y = 1.0f;
+
+		if (mScaling.x < 1.0f) {
+			mScaling.x += mScaleSpeedXZ;
+			mScaling.z += mScaleSpeedXZ;
+		} else {
+			mScaling.x = 1.0f;
+			mScaling.z = 1.0f;
+			onMapObjFlag(MAP_OBJ_FLAG_UNK100);
+			getModel()->calc();
+			offHitFlag(HIT_FLAG_CANNOT_ATTACK);
+			setUpCurrentMapCollision();
+			mState = STATE_NORMAL;
+		}
+		break;
+
+	default:
+		TMapObjGeneral::control();
+		break;
+	}
+
+	if (!isState(STATE_UNKC) && !isState(STATE_UNKD))
+		return;
+
+	setObjHitData(0);
+	if (gpMarDirector->mMap != 2
+	    || (!gpMarDirector->isDemoModeNow()
+	        && (unk2E0 == nullptr || unk2E0->isBuried(1)))) {
+		SMSRumbleMgr->start(0x13, &mPosition);
+		gpCameraShake->keepShake(CAM_SHAKE_MODE_UNK5, 1.0f);
+	}
+
+	if (unk2DC > sWaitTime) {
+		// circular buffer of particle positions?
+		unk170[unk2D8].set(
+		    mPosition.x + 400.0f * (rand() * (1.0f / 32768.0f)) - 200.0f,
+		    mPosition.y,
+		    mPosition.z + 400.0f * (rand() * (1.0f / 32768.0f)) - 200.0f);
+
+		gpMarioParticleManager->emit(0x1db, &unk170[unk2D8], 2, this);
+		++unk2D8;
+		if (unk2D8 >= 30)
+			unk2D8 = 0;
+		unk2DC = 0;
+	}
+	++unk2DC;
+}
+
+inline void TMapObjTreeScale::beSmall()
+{
+	mScaling.set(mScaleMin, mScaleMin, mScaleMin);
+	sleep();
+	offHitFlag(HIT_FLAG_NO_COLLISION);
+	onHitFlag(HIT_FLAG_CANNOT_ATTACK);
+	setObjHitData(0);
+	mDamageRadius = mAttackRadius;
+	calcEntryRadius();
+	mDamageHeight = 30.0f;
+	calcEntryRadius();
+	removeMapCollision();
+	offMapObjFlag(MAP_OBJ_FLAG_UNK100);
+	mActorType = 0x4000003b;
+	mState     = STATE_UNKB;
+	SMS_HideAllShapePacket(getModel());
+}
+
+void TMapObjTreeScale::loadAfter()
+{
+	TMapObjGeneral::loadAfter();
+
+	if (gpMarDirector->getCurrentMap() == 4
+	    || gpPollution->isPolluted(mPosition.x, mPosition.y, mPosition.z)) {
+		beSmall();
+	}
+
+	// Translated: "Event (Bianco terrain sinking)"
+	unk2E0 = (TMapEventSink*)JDrama::TNameRefGen::getInstance()
+	             ->getRootNameRef()
+	             ->search("イベント（地形沈むビアンコ）");
+}
+
+TMapObjTreeScale::TMapObjTreeScale(const char* name)
+    : TMapObjTree(name)
+    , unk2D8(0)
+    , unk2DC(0)
+    , unk2E0(nullptr)
+{
+	for (int i = 0; i < 30; ++i)
+		unk170[i].zero();
 }
