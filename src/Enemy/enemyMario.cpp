f076450f9afca698884b490109368dbbadbc0693
KakarotCake <61133727+KakarottoCake@users.noreply.github.com>
2026-07-20 22:35:13 -0400

    Decompile EnemyMario return handling

--- src/Enemy/enemyMario.cpp
+++ src/Enemy/enemyMario.cpp
@@ -2,6 +2,12 @@
 // This file uses reverse MAP definition order because Enemy is built with
 // -inline deferred.
 #include <Enemy/EnemyMario.hpp>
+#include <Enemy/Emario.hpp>
+#include <Enemy/Graph.hpp>
+#include <Map/MapData.hpp>
+#include <System/EmitterViewObj.hpp>
+#include <System/MarDirector.hpp>
+#include <System/MSoundMainSide.hpp>
 
 void TEnemyMario::changeEMDoing(u16 doing)
 {
@@ -9,10 +15,29 @@ void TEnemyMario::changeEMDoing(u16 doing)
 	mEMDoing      = doing;
 }
 
-void TEnemyMario::startRunAway()
+void TEnemyMario::startDisappear(u16 doing)
 {
-	changeEMDoing(EM_DOING_RUN_AWAY);
+	mDisappearPosition = mPosition;
+
+	u8 currentMap      = gpMarDirector->getCurrentMap();
+	u8 currentStage    = gpMarDirector->getCurrentStage();
+	bool keepBossLives = false;
+	if (currentMap == 1 && currentStage == 1) {
+		keepBossLives = true;
+	} else if (currentMap == 1 && currentStage == 9) {
+		keepBossLives = true;
+	}
+
+	if (!keepBossLives) {
+		MSMainProc::setBossLivesFlag(false);
+	}
+
+	gpMarioParticleManager->emitAndBindToPosPtr(0xED, &mDisappearPosition, 0,
+	                                            nullptr);
+	changeEMDoing(doing);
 }
+
+void TEnemyMario::startRunAway() { changeEMDoing(EM_DOING_RUN_AWAY); }
 
 u8 TEnemyMario::thinkTrample()
 {
@@ -38,6 +63,26 @@ void TEnemyMario::reachGoal()
 	changeEMDoing(EM_DOING_GOAL);
 }
 
-void TEnemyMario::damageExec(THitActor*, int, int, int, f32, int, f32, s16)
+void TEnemyMario::checkReturn()
 {
+	if (!mGroundPlane->checkFlag(BG_CHECK_FLAG_ILLEGAL)) {
+		return;
+	}
+
+	TGraphWeb* graph = mEMario->unk124->getGraph();
+	int nodeIndex    = graph->findNearestNodeIndex(mPosition, -1);
+	bool searching   = true;
+	while (searching) {
+		Vec point;
+		graph->getGraphNode(nodeIndex).getPoint(&point);
+
+		if (JGeometry::TVec3<f32>(point - *gpMarioPos).length() > 1000.0f) {
+			searching = false;
+			mPosition = point;
+		}
+
+		nodeIndex = (nodeIndex + 1) % graph->getNodeNum();
+	}
 }
+
+void TEnemyMario::damageExec(THitActor*, int, int, int, f32, int, f32, s16) { }
