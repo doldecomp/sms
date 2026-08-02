3a1adcd7eb0334dcecb2f999ec75433ac0efcbba
Private Contributor <redacted@example.invalid>
2026-08-02 11:24:17 -0400

    GC2D/BlendPane: decompile matching TU (#133)
    * GC2D/BlendPane: decompile matching TU
    
    * GC2D/BlendPane: model fields in derived class

--- include/GC2D/BlendPane.hpp
+++ include/GC2D/BlendPane.hpp
@@ -2,13 +2,31 @@
 #define GC2D_BLEND_PANE_HPP
 
 #include <GC2D/BoundPane.hpp>
+#include <JSystem/J2D/J2DPicture.hpp>
+#include <JSystem/JUtility/JUTTexture.hpp>
 
 class TBlendPane : public TBoundPane {
+	/* 0x68 */ f32 mBlendStep;
+	/* 0x6C */ f32 mBlendProgress;
+	/* 0x70 */ bool mBlendActive;
+
 public:
 	TBlendPane(J2DScreen*, u32);
 
-	virtual void update();
-	void setPaneBlend(s32, JUTTexture*, JUTTexture*);
+	virtual bool update();
+	void setPaneBlend(s32 frames, JUTTexture* texture1, JUTTexture* texture2)
+	{
+		if (texture2 == nullptr) {
+			((J2DPicture*)getPane())->changeTexture(texture1->getTexInfo(), 0);
+		} else {
+			((J2DPicture*)getPane())->changeTexture(texture1->getTexInfo(), 0);
+			((J2DPicture*)getPane())->changeTexture(texture2->getTexInfo(), 1);
+		}
+
+		mBlendActive   = true;
+		mBlendStep     = 1.0f / frames;
+		mBlendProgress = 0.0f;
+	}
 };
 
 #endif
--- src/GC2D/BlendPane.cpp
+++ src/GC2D/BlendPane.cpp
@@ -1,1 +1,31 @@
+#include <GC2D/BlendPane.hpp>
+#include <JSystem/J2D/J2DScreen.hpp>
 
+TBlendPane::TBlendPane(J2DScreen* screen, u32 tag)
+    : TBoundPane(screen, tag)
+{
+	mBlendActive   = false;
+	mBlendStep     = 0.0f;
+	mBlendProgress = 0.0f;
+}
+
+bool TBlendPane::update()
+{
+	bool result = TBoundPane::update();
+	if (mBlendActive) {
+		if (mBlendProgress >= 1.0f) {
+			mBlendProgress = 1.0f;
+			mBlendActive   = false;
+		}
+
+		f32 inverseProgress;
+		f32 progress;
+		inverseProgress     = 1.0f - (progress = mBlendProgress);
+		J2DPicture* picture = (J2DPicture*)getPane();
+		picture->setBlendKonstColor(progress, inverseProgress, 1.0f, 1.0f);
+		picture->setBlendKonstAlpha(progress, inverseProgress, 1.0f, 1.0f);
+		mBlendProgress += mBlendStep;
+	}
+
+	return (result && !mBlendActive) ? true : false;
+}
