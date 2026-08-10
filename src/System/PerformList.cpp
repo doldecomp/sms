f238822385e1344e0faa032f52c56f23dea94678
Roman Sandu <roma.sandu@gmail.com>
2026-08-10 03:27:14 +0300

    PerformList closer

--- src/System/PerformList.cpp
+++ src/System/PerformList.cpp
@@ -8,7 +8,8 @@ void TPerformList::forEachPerform(
 {
 	for (JGadget::TSingleLinkList<TPerformLink, 0>::iterator it = b; it != e;
 	     ++it) {
-		it->unk4->testPerform(param_4 & it->unk8, param_3);
+		TPerformLink& link = *it;
+		link.unk4->testPerform(link.unk8 & param_4, param_3);
 	}
 }
 
@@ -27,11 +28,13 @@ void TPerformList::load(JSUMemoryInputStream& stream)
 
 		JDrama::TViewObj* obj
 		    = JDrama::TNameRefGen::search<JDrama::TViewObj>(acStack_6c);
+
+		// TODO: feels fake and stack is missing, needs more tinkering
 		u32 value;
 		stream >> value;
 		u32 uVar5 = value;
 		if (value & CUE_MOVE)
-			uVar5 = value | (CUE_MOVEMENT_GATE_A | CUE_MOVEMENT_GATE_B);
+			uVar5 |= (CUE_MOVEMENT_GATE_A | CUE_MOVEMENT_GATE_B);
 		if (obj)
 			push_back(obj, uVar5);
 	}
@@ -39,13 +42,19 @@ void TPerformList::load(JSUMemoryInputStream& stream)
 
 void TPerformList::push_back(const char* param_1, u32 param_2)
 {
+	// TODO: every instruction matches, but the target holds one more compiler
+	// temporary and no `obj` local, so its stack slots all sit 4 bytes higher.
+	// Folding the search into the `new TPerformLink(...)` argument removes the
+	// local, but it also moves the allocation ahead of the search, which the
+	// target does not do. Delegating to the TViewObj* overload costs an inline
+	// pass and turns end() into a call. Both are worse.
 	JDrama::TViewObj* obj
 	    = JDrama::TNameRefGen::search<JDrama::TViewObj>(param_1);
 
-	getChildren().Push_back(new TPerformLink(obj, param_2));
+	Push_back(new TPerformLink(obj, param_2));
 }
 
 void TPerformList::push_back(JDrama::TViewObj* param_1, u32 param_2)
 {
-	getChildren().Push_back(new TPerformLink(param_1, param_2));
+	Push_back(new TPerformLink(param_1, param_2));
 }
