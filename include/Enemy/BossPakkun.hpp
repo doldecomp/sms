e374cba7f91d4c2938cf77a925205d00b663fb97
KakarotCake <61133727+KakarottoCake@users.noreply.github.com>
2026-08-31 01:11:43 -0400

    Decompile Boss Pakkun class structure

--- include/Enemy/BossPakkun.hpp
+++ include/Enemy/BossPakkun.hpp
@@ -1,36 +1,251 @@
 #ifndef ENEMY_BOSS_PAKKUN_HPP
 #define ENEMY_BOSS_PAKKUN_HPP
 
+#include <Enemy/Enemy.hpp>
+#include <Enemy/EnemyManager.hpp>
+#include <JSystem/JDrama/JDRViewObj.hpp>
+#include <M3DUtil/M3UJoint.hpp>
 #include <Strategic/Nerve.hpp>
 
-class TLiveActor;
+class MActor;
+class TAreaCylinderManager;
+class TWaterEmitInfo;
 
-DECLARE_NERVE(TNerveBPWait, TLiveActor);
-DECLARE_NERVE(TNerveBPCannon, TLiveActor);
-DECLARE_NERVE(TNerveBPVomit, TLiveActor);
-DECLARE_NERVE(TNerveBPTornado, TLiveActor);
-DECLARE_NERVE(TNerveBPPivot, TLiveActor);
-DECLARE_NERVE(TNerveBPSwallow, TLiveActor);
-DECLARE_NERVE(TNerveBPTumbleIn, TLiveActor);
-DECLARE_NERVE(TNerveBPTumble, TLiveActor);
-DECLARE_NERVE(TNerveBPTumbleOut, TLiveActor);
-DECLARE_NERVE(TNerveBPGetUp, TLiveActor);
-DECLARE_NERVE(TNerveBPSwing, TLiveActor);
-DECLARE_NERVE(TNerveBPStompReact, TLiveActor);
-DECLARE_NERVE(TNerveBPJumpReact, TLiveActor);
-DECLARE_NERVE(TNerveBPPreDie, TLiveActor);
-DECLARE_NERVE(TNerveBPDie, TLiveActor);
-DECLARE_NERVE(TNerveBPTakeOff, TLiveActor);
-DECLARE_NERVE(TNerveBPFly, TLiveActor);
-DECLARE_NERVE(TNerveBPTouchDown, TLiveActor);
-DECLARE_NERVE(TNerveBPFlyCannon, TLiveActor);
-DECLARE_NERVE(TNerveBPFlyPivot, TLiveActor);
-DECLARE_NERVE(TNerveBPHover, TLiveActor);
-DECLARE_NERVE(TNerveBPFall, TLiveActor);
-DECLARE_NERVE(TNerveBPSleep, TLiveActor);
-DECLARE_NERVE(TNerveBPBreakSleep, TLiveActor);
-DECLARE_NERVE(TNerveBPWaitL, TLiveActor);
+class TBossPakkun;
+class TBossPakkunMtxCalc;
+class TBPHeadHit;
+class TBPNavel;
+class TBPPolDrop;
+class TBPTornado;
+class TBPVomit;
+
+class TBossPakkunParams : public TSpineEnemyParams {
+public:
+	TBossPakkunParams(const char*);
+
+public:
+	/* 0x0A8 */ TParamRT<s32> mSLWaitFrameStg0;
+	/* 0x0BC */ TParamRT<s32> mSLWaterMarkLimit;
+	/* 0x0D0 */ TParamRT<f32> mSLSwingLength;
+	/* 0x0E4 */ TParamRT<f32> mSLPollBallStampScale;
+	/* 0x0F8 */ TParamRT<s32> mSLTumbleTime;
+	/* 0x10C */ TParamRT<s32> mSLAnmBlendTime0;
+	/* 0x120 */ TParamRT<f32> mSLFlySpeed;
+	/* 0x134 */ TParamRT<f32> mSLPivotSpeed;
+	/* 0x148 */ TParamRT<f32> mSLPivotSpeedAware;
+	/* 0x15C */ TParamRT<f32> mSLVomitAnmRate;
+	/* 0x170 */ TParamRT<f32> mSLHeadHomingLimit;
+	/* 0x184 */ TParamRT<f32> mSLDamageAngle;
+	/* 0x198 */ TParamRT<f32> mSLTornadoProp;
+	/* 0x1AC */ TParamRT<f32> mSLTornadoSpeed;
+	/* 0x1C0 */ TParamRT<f32> mSLTornadoRollSpeed;
+	/* 0x1D4 */ TParamRT<f32> mSLTornadoMoveInit;
+	/* 0x1E8 */ TParamRT<f32> mSLTornadoMoveInc;
+	/* 0x1FC */ TParamRT<f32> mSLTornadoMoveLimit;
+	/* 0x210 */ TParamRT<s32> mSLWaterHitTimer;
+	/* 0x224 */ TParamRT<s32> mSLHoverTimer;
+	/* 0x238 */ TParamRT<f32> mSLPollBallRange;
+	/* 0x24C */ TParamRT<f32> mSLPollBallSpeed;
+	/* 0x260 */ TParamRT<f32> mSLPollBallFront;
+};
+
+class TBossPakkunMtxCalc : public M3UMtxCalcSIAnmBlendQuat {
+public:
+	TBossPakkunMtxCalc(TBossPakkun*);
+
+	virtual void calc(u16);
+
+	void setAnm(int);
+	void joinAnm(int);
+	void calcHeadDir(u16);
+	void calcBellyScale(u16);
+
+public:
+	/* 0x64 */ TBossPakkun* mOwner;
+};
+
+class TBossPakkunManager : public TEnemyManager {
+public:
+	TBossPakkunManager(const char*, int);
+
+	virtual void load(JSUMemoryInputStream&);
+	virtual void createModelData();
+
+	void initJParticle();
+
+public:
+	/* 0x54 */ int unk54;
+};
+
+class TBossPakkun : public TSpineEnemy {
+public:
+	TBossPakkun(const char* name = "ボスパックン");
+
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+	virtual void init(TLiveManager*);
+	virtual void setGroundCollision();
+	virtual void kill();
+	virtual const char** getBasNameTable() const;
+
+	void flyToCurPathNode(f32, f32);
+	void changeBck(int);
+	void killSmallEnemies();
+	void launchTornado();
+	void launchPolDrop();
+	void gotTrampleDamage();
+	void gotHipDropDamage();
+	void gotWaterDamage();
+	void gotFlyingDamage();
+	BOOL inArea(const JGeometry::TVec3<f32>&);
+	void resetWaterMark();
+	void startTornadoBlur();
+	void ignoreWaterCheck();
+	BOOL is2ndFightNow() const;
+	void showMessage(u32);
+	void rumblePad(int, const JGeometry::TVec3<f32>&);
+	void startBGM();
+	void checkMarioRiding();
+
+	TBossPakkunParams* getBossPakkunParams() const
+	{
+		return static_cast<TBossPakkunParams*>(getSaveParam());
+	}
+
+public:
+	/* 0x150 */ TBossPakkunMtxCalc* mMtxCalc;
+	/* 0x154 */ f32 unk154;
+	/* 0x158 */ TBPPolDrop* mPolDrop;
+	/* 0x15C */ TBPVomit* mVomit;
+	/* 0x160 */ TBPTornado* mTornado;
+	/* 0x164 */ TBPHeadHit* mHeadHit;
+	/* 0x168 */ TBPNavel* mNavel;
+	/* 0x16C */ s8 unk16C;
+	/* 0x170 */ s32 unk170;
+	/* 0x174 */ s32 unk174;
+	/* 0x178 */ s32 unk178;
+	/* 0x17C */ u8 unk17C;
+	/* 0x180 */ MActor* unk180;
+	/* 0x184 */ f32 unk184;
+	/* 0x188 */ TAreaCylinderManager* unk188;
+	/* 0x18C */ TWaterEmitInfo* unk18C;
+	/* 0x190 */ u8 unk190;
+	/* 0x194 */ JGeometry::TVec3<f32> unk194;
+	/* 0x1A0 */ JGeometry::TVec3<f32> unk1A0;
+	/* 0x1AC */ JGeometry::TVec3<f32> unk1AC;
+	/* 0x1B8 */ s32 unk1B8;
+	/* 0x1BC */ u8 unk1BC;
+	/* 0x1C0 */ s32 unk1C0;
+	/* 0x1C4 */ u8 unk1C4;
+	/* 0x1C8 */ f32 unk1C8;
+	/* 0x1CC */ u8 unk1CC;
+};
+
+class TBPNavel : public THitActor {
+public:
+	TBPNavel(TBossPakkun*, const char*);
+
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+
+public:
+	/* 0x68 */ TBossPakkun* mOwner;
+};
+
+class TBPHeadHit : public THitActor {
+public:
+	TBPHeadHit(TBossPakkun*, const char*);
+
+	virtual void perform(u32, JDrama::TGraphics*);
+	virtual BOOL receiveMessage(THitActor*, u32);
+
+	void throwActor(THitActor*);
+
+public:
+	/* 0x68 */ TBossPakkun* mOwner;
+};
+
+class TBPTornado : public THitActor {
+public:
+	TBPTornado(TBossPakkun*, const char* name = "<TBPTornado>");
+
+	virtual void perform(u32, JDrama::TGraphics*);
+
+	void launch(const JGeometry::TVec3<f32>&);
+	void vanish();
+
+public:
+	/* 0x68 */ TBossPakkun* mOwner;
+	/* 0x6C */ MActor* mActor;
+	/* 0x70 */ JGeometry::TVec3<f32> unk70;
+	/* 0x7C */ JGeometry::TVec3<f32> unk7C;
+	/* 0x88 */ JGeometry::TVec3<f32> unk88;
+	/* 0x94 */ f32 unk94;
+	/* 0x98 */ s32 unk98;
+};
+
+class TBPVomit : public JDrama::TViewObj {
+public:
+	TBPVomit(TBossPakkun*, const char* name = "<TBPVomit>");
+
+	virtual void perform(u32, JDrama::TGraphics*);
+
+	void vomitFinished();
+	void vomit();
+
+public:
+	/* 0x10 */ TBossPakkun* mOwner;
+	/* 0x14 */ MActor* unk14;
+	/* 0x18 */ MActor* unk18;
+};
+
+class TBPPolDrop : public THitActor {
+public:
+	TBPPolDrop(TBossPakkun*, const char* name = "<TBPPolDrop>");
+
+	virtual void perform(u32, JDrama::TGraphics*);
+
+	void launch(const JGeometry::TVec3<f32>&, const JGeometry::TVec3<f32>&);
+	void move();
+	void drop();
+
+public:
+	/* 0x68 */ TBossPakkun* mOwner;
+	/* 0x6C */ JGeometry::TVec3<f32> unk6C;
+	/* 0x78 */ MActor* unk78;
+	/* 0x7C */ MActor* unk7C;
+	/* 0x80 */ s32 unk80;
+	/* 0x84 */ s32 unk84;
+	/* 0x88 */ f32 unk88;
+};
+
 DECLARE_NERVE(TNerveBPCannonL, TLiveActor);
+DECLARE_NERVE(TNerveBPWaitL, TLiveActor);
+DECLARE_NERVE(TNerveBPBreakSleep, TLiveActor);
+DECLARE_NERVE(TNerveBPSleep, TLiveActor);
+DECLARE_NERVE(TNerveBPFall, TLiveActor);
+DECLARE_NERVE(TNerveBPHover, TLiveActor);
+DECLARE_NERVE(TNerveBPFlyPivot, TLiveActor);
+DECLARE_NERVE(TNerveBPFlyCannon, TLiveActor);
+DECLARE_NERVE(TNerveBPTouchDown, TLiveActor);
+DECLARE_NERVE(TNerveBPFly, TLiveActor);
+DECLARE_NERVE(TNerveBPTakeOff, TLiveActor);
+DECLARE_NERVE(TNerveBPDie, TLiveActor);
+DECLARE_NERVE(TNerveBPPreDie, TLiveActor);
+DECLARE_NERVE(TNerveBPJumpReact, TLiveActor);
+DECLARE_NERVE(TNerveBPStompReact, TLiveActor);
+DECLARE_NERVE(TNerveBPSwing, TLiveActor);
+DECLARE_NERVE(TNerveBPGetUp, TLiveActor);
+DECLARE_NERVE(TNerveBPTumbleOut, TLiveActor);
+DECLARE_NERVE(TNerveBPTumble, TLiveActor);
+DECLARE_NERVE(TNerveBPTumbleIn, TLiveActor);
+DECLARE_NERVE(TNerveBPSwallow, TLiveActor);
+DECLARE_NERVE(TNerveBPPivot, TLiveActor);
+DECLARE_NERVE(TNerveBPTornado, TLiveActor);
+DECLARE_NERVE(TNerveBPVomit, TLiveActor);
+DECLARE_NERVE(TNerveBPCannon, TLiveActor);
+DECLARE_NERVE(TNerveBPWait, TLiveActor);
+
 DECLARE_NERVE(TNervePakkunGenerate, TLiveActor);
 DECLARE_NERVE(TNervePakkunStay, TLiveActor);
 DECLARE_NERVE(TNervePakkunAppear, TLiveActor);
--- src/Enemy/bosspakkun.cpp
+++ src/Enemy/bosspakkun.cpp
@@ -1,1 +1,295 @@
+#include <Enemy/BossPakkun.hpp>
 
+static const char* bosspakkun_bastable[] = {
+	nullptr,
+	nullptr,
+	"/scene/bosspakkun/bas/bosspaku_ball_end.bas",
+	"/scene/bosspakkun/bas/bosspaku_down.bas",
+	"/scene/bosspakkun/bas/bosspaku_down_end.bas",
+	nullptr,
+	"/scene/bosspakkun/bas/bosspaku_down_loop.bas",
+	nullptr,
+	"/scene/bosspakkun/bas/bosspaku_fall_end.bas",
+	nullptr,
+	"/scene/bosspakkun/bas/bosspaku_fall_start.bas",
+	"/scene/bosspakkun/bas/bosspaku_fly.bas",
+	"/scene/bosspakkun/bas/bosspaku_fly_pollut.bas",
+	"/scene/bosspakkun/bas/bosspaku_fly_start.bas",
+	"/scene/bosspakkun/bas/bosspaku_getup.bas",
+	"/scene/bosspakkun/bas/bosspaku_head.bas",
+	"/scene/bosspakkun/bas/bosspaku_hovering.bas",
+	"/scene/bosspakkun/bas/bosspaku_jump_reaction.bas",
+	"/scene/bosspakkun/bas/bosspaku_land.bas",
+	"/scene/bosspakkun/bas/bosspaku_panpan.bas",
+	"/scene/bosspakkun/bas/bosspaku_pollut_end.bas",
+	"/scene/bosspakkun/bas/bosspaku_pollut_start.bas",
+	"/scene/bosspakkun/bas/bosspaku_return.bas",
+	"/scene/bosspakkun/bas/bosspaku_sleep.bas",
+	"/scene/bosspakkun/bas/bosspaku_tornado.bas",
+	nullptr,
+	"/scene/bosspakkun/bas/bosspaku_water_hit.bas",
+	nullptr,
+	nullptr,
+	nullptr,
+};
+
+TBossPakkunParams::TBossPakkunParams(const char* path)
+    : TSpineEnemyParams(path)
+    , PARAM_INIT(mSLWaitFrameStg0, 400)
+    , PARAM_INIT(mSLWaterMarkLimit, 600)
+    , PARAM_INIT(mSLSwingLength, 600.0f)
+    , PARAM_INIT(mSLPollBallStampScale, 1.0f)
+    , PARAM_INIT(mSLTumbleTime, 2400)
+    , PARAM_INIT(mSLAnmBlendTime0, 60)
+    , PARAM_INIT(mSLFlySpeed, 5.0f)
+    , PARAM_INIT(mSLPivotSpeed, 0.7f)
+    , PARAM_INIT(mSLPivotSpeedAware, 1.5f)
+    , PARAM_INIT(mSLVomitAnmRate, 0.6f)
+    , PARAM_INIT(mSLHeadHomingLimit, 30.0f)
+    , PARAM_INIT(mSLDamageAngle, 180.0f)
+    , PARAM_INIT(mSLTornadoProp, 0.4f)
+    , PARAM_INIT(mSLTornadoSpeed, 2.0f)
+    , PARAM_INIT(mSLTornadoRollSpeed, 0.06f)
+    , PARAM_INIT(mSLTornadoMoveInit, 10000.0f)
+    , PARAM_INIT(mSLTornadoMoveInc, 1.0f)
+    , PARAM_INIT(mSLTornadoMoveLimit, 10720.0f)
+    , PARAM_INIT(mSLWaterHitTimer, 20)
+    , PARAM_INIT(mSLHoverTimer, 1200)
+    , PARAM_INIT(mSLPollBallRange, 10000.0f)
+    , PARAM_INIT(mSLPollBallSpeed, 20.0f)
+    , PARAM_INIT(mSLPollBallFront, 1000.0f)
+{
+	TParams::load(mPrmPath);
+}
+
+TBPPolDrop::TBPPolDrop(TBossPakkun* owner, const char* name)
+    : THitActor(name)
+    , mOwner(owner)
+    , unk78(nullptr)
+    , unk7C(nullptr)
+    , unk80(0)
+    , unk84(0)
+    , unk88(0.0f)
+{
+}
+
+void TBPPolDrop::drop() { }
+
+void TBPPolDrop::move() { }
+
+void TBPPolDrop::launch(const JGeometry::TVec3<f32>&,
+                        const JGeometry::TVec3<f32>&)
+{
+}
+
+void TBPPolDrop::perform(u32, JDrama::TGraphics*) { }
+
+TBPVomit::TBPVomit(TBossPakkun* owner, const char* name)
+    : JDrama::TViewObj(name)
+    , mOwner(owner)
+    , unk14(nullptr)
+    , unk18(nullptr)
+{
+}
+
+void TBPVomit::vomit() { }
+
+void TBPVomit::vomitFinished() { }
+
+void TBPVomit::perform(u32, JDrama::TGraphics*) { }
+
+TBPTornado::TBPTornado(TBossPakkun* owner, const char* name)
+    : THitActor(name)
+    , mOwner(owner)
+    , mActor(nullptr)
+    , unk94(0.0f)
+    , unk98(0)
+{
+}
+
+void TBPTornado::vanish() { }
+
+void TBPTornado::perform(u32, JDrama::TGraphics*) { }
+
+void TBPTornado::launch(const JGeometry::TVec3<f32>&) { }
+
+TBPHeadHit::TBPHeadHit(TBossPakkun* owner, const char* name)
+    : THitActor(name)
+    , mOwner(owner)
+{
+}
+
+BOOL TBPHeadHit::receiveMessage(THitActor*, u32) { return false; }
+
+void TBPHeadHit::throwActor(THitActor*) { }
+
+void TBPHeadHit::perform(u32, JDrama::TGraphics*) { }
+
+TBPNavel::TBPNavel(TBossPakkun* owner, const char* name)
+    : THitActor(name)
+    , mOwner(owner)
+{
+}
+
+BOOL TBPNavel::receiveMessage(THitActor*, u32) { return false; }
+
+void TBPNavel::perform(u32, JDrama::TGraphics*) { }
+
+TBossPakkunMtxCalc::TBossPakkunMtxCalc(TBossPakkun* owner)
+    : M3UMtxCalcSIAnmBlendQuat(false)
+    , mOwner(owner)
+{
+}
+
+void TBossPakkunMtxCalc::calcBellyScale(u16) { }
+
+void TBossPakkunMtxCalc::calcHeadDir(u16) { }
+
+void TBossPakkunMtxCalc::joinAnm(int) { }
+
+void TBossPakkunMtxCalc::setAnm(int) { }
+
+void TBossPakkunMtxCalc::calc(u16) { }
+
+TBossPakkun::TBossPakkun(const char* name)
+    : TSpineEnemy(name)
+    , mMtxCalc(nullptr)
+    , unk154(0.0f)
+    , mPolDrop(nullptr)
+    , mVomit(nullptr)
+    , mTornado(nullptr)
+    , mHeadHit(nullptr)
+    , mNavel(nullptr)
+    , unk16C(0)
+    , unk170(0)
+    , unk174(0)
+    , unk178(0)
+    , unk17C(0)
+    , unk180(nullptr)
+    , unk184(0.0f)
+    , unk188(nullptr)
+    , unk18C(nullptr)
+    , unk190(0)
+    , unk1B8(0)
+    , unk1BC(0)
+    , unk1C0(0)
+    , unk1C4(0)
+    , unk1C8(0.0f)
+    , unk1CC(0)
+{
+}
+
+void TBossPakkun::init(TLiveManager*) { }
+
+void TBossPakkun::checkMarioRiding() { }
+
+void TBossPakkun::startBGM() { }
+
+void TBossPakkun::rumblePad(int, const JGeometry::TVec3<f32>&) { }
+
+void TBossPakkun::showMessage(u32) { }
+
+BOOL TBossPakkun::is2ndFightNow() const { return false; }
+
+void TBossPakkun::ignoreWaterCheck() { }
+
+void TBossPakkun::startTornadoBlur() { }
+
+void TBossPakkun::resetWaterMark() { }
+
+BOOL TBossPakkun::inArea(const JGeometry::TVec3<f32>&) { return false; }
+
+void TBossPakkun::gotFlyingDamage() { }
+
+void TBossPakkun::gotWaterDamage() { }
+
+void TBossPakkun::gotHipDropDamage() { }
+
+void TBossPakkun::gotTrampleDamage() { }
+
+void TBossPakkun::launchPolDrop() { }
+
+void TBossPakkun::launchTornado() { }
+
+void TBossPakkun::killSmallEnemies() { }
+
+void TBossPakkun::changeBck(int) { }
+
+void TBossPakkun::flyToCurPathNode(f32, f32) { }
+
+const char** TBossPakkun::getBasNameTable() const
+{
+	return bosspakkun_bastable;
+}
+
+void TBossPakkun::setGroundCollision() { }
+
+void TBossPakkun::kill() { }
+
+BOOL TBossPakkun::receiveMessage(THitActor*, u32) { return false; }
+
+void TBossPakkun::perform(u32, JDrama::TGraphics*) { }
+
+TBossPakkunManager::TBossPakkunManager(const char* name, int value)
+    : TEnemyManager(name)
+    , unk54(value)
+{
+}
+
+void TBossPakkunManager::initJParticle() { }
+
+void TBossPakkunManager::createModelData() { }
+
+void TBossPakkunManager::load(JSUMemoryInputStream&) { }
+
+DEFINE_NERVE(TNerveBPWait, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPCannon, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPVomit, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPTornado, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPPivot, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPSwallow, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPTumbleIn, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPTumble, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPTumbleOut, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPGetUp, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPSwing, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPStompReact, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPJumpReact, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPPreDie, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPDie, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPTakeOff, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPFly, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPTouchDown, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPFlyCannon, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPFlyPivot, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPHover, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPFall, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPSleep, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPBreakSleep, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPWaitL, TLiveActor) { return false; }
+
+DEFINE_NERVE(TNerveBPCannonL, TLiveActor) { return false; }
