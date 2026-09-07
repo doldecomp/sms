cc8befb694180a71f580fe5f34ac2a490d487e35
Magi <paoloimbrmagi@gmail.com>
2026-09-08 01:33:02 +0200

    feat: TBW started, shakeCamera done

--- include/Enemy/BossWanwan.hpp
+++ include/Enemy/BossWanwan.hpp
@@ -2,8 +2,62 @@
 #define ENEMY_BOSS_WANWAN_HPP
 
 #include <Strategic/Nerve.hpp>
+#include <Strategic/Spine.hpp>
+#include <Strategic/LiveManager.hpp>
+#include <Enemy/EnemyManager.hpp>
+#include <Enemy/Enemy.hpp>
 
 class TLiveActor;
+class TBossWanwan;
+
+class TBWParams : public TSpineEnemyParams {
+public:
+	/* * TSpineEnemyParams ends at 0xA8. *
+	 * Every TParamRT<T> occupies 0x14 bytes:
+	 * * +0x00 : TParam / base data
+	 * * +0x04 : ...
+	 * * +0x0C : ...
+	 * * +0x10 : actual parameter value
+	 * * * Therefore: * parameter start = X * parameter value = X + 0x10 */
+	TBWParams(const char*);
+
+	/* 0x0A8 */ TParamRT<f32> mSLMarchSpeed;
+	/* 0x0BC */ TParamRT<f32> mSLTurnSpeed;
+	/* 0x0D0 */ TParamRT<f32> mSLLeashNodeLen;
+	/* 0x0E4 */ TParamRT<f32> mSLPicketHeight;
+	/* 0x0F8 */ TParamRT<f32> mSLPicketRadius;
+	/* 0x10C */ TParamRT<f32> mSLChainHitHeight;
+	/* 0x120 */ TParamRT<f32> mSLChainHitRadius;
+	/* 0x134 */ TParamRT<f32> mSLChainGroundRadius;
+	/* 0x148 */ TParamRT<f32> mSLPullLimit;
+	/* 0x15C */ TParamRT<f32> mSLAttackSpeed;
+	/* 0x170 */ TParamRT<s32> mSLStunTimer;
+	/* 0x184 */ TParamRT<f32> mSLSearchLength;
+	/* 0x198 */ TParamRT<f32> mSLSearchAngle;
+	/* 0x1AC */ TParamRT<u8> mSLBWHitPointMax;
+	/* 0x1C0 */ TParamRT<f32> mSLHeadGap;
+	/* 0x1D4 */ TParamRT<f32> mSLShakeLengthMax;
+	/* 0x1E8 */ TParamRT<f32> mSLShakeLengthMaxHP0;
+};
+
+class TBossWanwan : public TSpineEnemy {
+public:
+	TBossWanwan(const char* name = "ボスワンワン");
+
+	virtual void kill();
+	virtual void init(TLiveManager*);
+	virtual void shakeCamera(int shakeType);
+};
+
+class TBossWanwanManager : public TEnemyManager {
+public:
+	TBossWanwanManager(const char* name = "ボスワンワンマネージャ");
+
+	virtual void load(JSUMemoryInputStream&);
+
+	TSpineEnemy* createEnemyInstance();
+	void createModelData();
+};
 
 DECLARE_NERVE(TNerveBWGraphWander, TLiveActor);
 DECLARE_NERVE(TNerveBWRoll, TLiveActor);
