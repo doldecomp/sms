e2b7de2772f8d22840ceb8103b77f53bbb9ce2aa
Roman Sandu <roma.sandu@gmail.com>
2026-09-08 16:48:50 +0300

    JAudio small thing

--- include/JSystem/JAudio/JASystem/JASDSPChannel.hpp
+++ include/JSystem/JAudio/JASystem/JASDSPChannel.hpp
@@ -22,16 +22,6 @@ public:
 	}
 	~TDSPChannel() { }
 
-	static void initAll();
-	static void updateAll();
-	static TDSPChannel* alloc(u32 param1, u32 param2);
-	static int free(TDSPChannel* channel, u32 param);
-	static TDSPChannel* getHandle(u32 handle);
-	u32 getNumUse();
-	u32 getNumFree();
-	void setLimitDSP(f32 limit);
-	f32* getHistory();
-
 	void init(u8 param);
 	BOOL allocate(u32 param);
 	void free();
@@ -41,10 +31,10 @@ public:
 	void restart();
 	bool forceStop();
 	void forceDelete();
-	static JASystem::TDSPChannel* getLower();
-	static JASystem::TDSPChannel* getLowerActive();
-	static BOOL breakLower(u8 param);
-	static BOOL breakLowerActive(u8 param);
+	u32 getNumUse();
+	u32 getNumFree();
+	void setLimitDSP(f32 limit);
+	f32* getHistory();
 
 	BOOL isFree() const { return mStatus == 1 ? TRUE : FALSE; }
 
@@ -61,11 +51,6 @@ public:
 	void setCBInterval(u16 interval) { mCBInterval = interval; }
 	void decCBInterval() { mCBInterval--; }
 
-	static TDSPChannel* DSPCH;
-	static u32 smnUse;
-	static u32 smnFree;
-
-	// fake, stolen from tww
 	TChannel* getLogicalChannel()
 	{
 		if (mCallback != nullptr) {
@@ -74,6 +59,25 @@ public:
 			return nullptr;
 		}
 	}
+
+	static void initAll();
+	static void updateAll();
+	static TDSPChannel* alloc(u32 param1, u32 param2);
+	static int free(TDSPChannel* channel, u32 param);
+	static TDSPChannel* getHandle(u32 handle);
+	static JASystem::TDSPChannel* getLower();
+	static JASystem::TDSPChannel* getLowerActive();
+	static BOOL breakLower(u8 param);
+	static BOOL breakLowerActive(u8 param);
+	static void forceStop(TDSPChannel* channel)
+	{
+		if (channel != nullptr)
+			channel->forceStop();
+	}
+
+	static TDSPChannel* DSPCH;
+	static u32 smnUse;
+	static u32 smnFree;
 
 public:
 	/* 0x0 */ u8 mNumber;
