c3109ace0cd9e5ea82bd38e0fcb29b73d6286414
gitRasheed <rasheed.khoshnaw@gmail.com>
2026-09-12 16:42:37 +0100

    MoveBG/MapObjTrap: match TLampTrapSpike loadAfter

--- src/MoveBG/MapObjTrap.cpp
+++ src/MoveBG/MapObjTrap.cpp
@@ -28,9 +28,9 @@ TLampTrapSpikeHit::TLampTrapSpikeHit(TLampTrapSpike* trap, const char* name)
     , unk68(trap)
 {
 	initHitActor(0x4000001E, 3, -0x80000000, 500.0f, 300.0f, 500.0f, 300.0f);
-	JDrama::TNameRefGen::search<TIdxGroupObj>("アイテムグループ")
-	    ->getChildren()
-	    .push_back(this);
+	TIdxGroupObj* group
+	    = JDrama::TNameRefGen::search<TIdxGroupObj>("アイテムグループ");
+	group->getChildren().push_back(this);
 	offHitFlag(HIT_FLAG_NO_COLLISION);
 }
 
