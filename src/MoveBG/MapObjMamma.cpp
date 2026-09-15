7bf3b117c885d298726dc29caad5d8b2f12f37da
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 15:26:25 -0500

    worker-integration(job-2938): mario/MoveBG/MapObjMamma::.sdata [checkpoint 88f50e0f]

--- src/MoveBG/MapObjMamma.cpp
+++ src/MoveBG/MapObjMamma.cpp
@@ -1,1 +1,24 @@
+#include <MoveBG/MapObjMamma.hpp>
+
+int mWitherTime__9TSandBase = 800;
+float mScaleMin__9TSandBase = 0.00001f;
+
+float mFiringFrameSpeed__13TSandBombBase = 3.0f;
+float mFiringFrameDownSpeed__13TSandBombBase = 0.2f;
+float mExplodeFrameSpeed__13TSandBombBase = 1.0f;
+float mMarioJumpRate__13TSandBombBase = 0.12f;
+int mExlodingRumbleTime__13TSandBombBase = 20;
+
+float mCollisionRate__11TSandCastle = 1.7f;
+
+int mGoTargetTime__11TLeanMirror = 600;
+int mDemoWaitTime__11TLeanMirror = -1;
+int mDemoLightTime__11TLeanMirror = 360;
+
+float mRotSpeed__17TMammaBlockRotate = 0.1f;
+float mRotReturnSpeed__17TMammaBlockRotate = 0.01f;
+float mRotEnd__17TMammaBlockRotate = 130.0f;
+float mMapGoSpeed__17TMammaBlockRotate = 1.0f;
+float mMapBackSpeed__17TMammaBlockRotate = 0.1f;
+int mWaitTime__17TMammaBlockRotate = 600;
 
