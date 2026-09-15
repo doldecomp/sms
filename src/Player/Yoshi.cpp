b562de90ff22215556e4e972b6c12bfce6d763fd
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 03:21:01 -0500

    worker-integration(job-9036): mario/Player/Yoshi::appearFromEgg__6TYoshiFRCQ29JGeometry8TVec3<f>fP9TEggYoshi [checkpoint 51a32a83]

--- src/Player/Yoshi.cpp
+++ src/Player/Yoshi.cpp
@@ -392,15 +392,17 @@ bool TYoshi::appearFromEgg(const JGeometry::TVec3<f32>& pos, f32 yrot,
 	mLastTranslation = pos;
 	mTranslation     = pos;
 	mTranslation.y += 1.0f;
-	mEggRotSpeed = DEG2SHORTANGLE(yrot);
-	mState       = STATE_UNK2;
+	s16 eggRotSpeed = DEG2SHORTANGLE(yrot);
+	mEggRotSpeed    = eggRotSpeed;
+	mState          = STATE_UNK2;
 
 	changeAnimation(0);
 
-	TTakeActor* fruit = (TTakeActor*)egg->getFruit();
+	THitActor* fruit       = egg->getFruit();
+	TTakeActor* takeActor  = (TTakeActor*)fruit;
 	if (mMario->getHeldObject() == fruit) {
-		fruit->receiveMessage(mMario->getFloorHitActor(), HIT_MESSAGE_UNK8);
-		fruit->mHolder      = nullptr;
+		takeActor->receiveMessage(mMario->getFloorHitActor(), HIT_MESSAGE_UNK8);
+		takeActor->mHolder  = nullptr;
 		mMario->mHeldObject = nullptr;
 	}
 
