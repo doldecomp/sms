352d60a72da0d7665a1e0a620f7509069fdf411f
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 18:37:43 -0500

    worker-integration(job-67cb): mario/NPC/NpcNerve::execute__20TNerveNPCTurnToMarioCFP24TSpineBase<10TLiveActor> [checkpoint 6bb81f66]

--- src/NPC/NpcNerve.cpp
+++ src/NPC/NpcNerve.cpp
@@ -161,11 +161,12 @@ DEFINE_NERVE(TNerveNPCTurnToMario, TLiveActor)
 		return true;
 	}
 
-	if (self->getActorType() - 0x400001C > 1) {
+	u32 actorType = self->getActorType();
+	if (actorType - 0x400001C > 1) {
 		if (!self->isPeachTired()) {
 			if (self->isSunflowerReviving()) {
 				self->sunflowerReviving();
-			} else if (self->getActorType() == 0x4000006) {
+			} else if (actorType == 0x4000006) {
 				self->monteMESetAnmWhenNear();
 				self->execTurnToFirstState();
 			} else if (self->isTurnToMarioWhenApproach()) {
