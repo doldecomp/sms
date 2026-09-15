3ef9ac3ba8db46f99242fdc7f85c64de84d3aea1
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 15:59:42 -0500

    worker-integration(job-a659): mario/MoveBG/MapObjBall::.sdata [checkpoint 9f4f45bb]

--- src/MoveBG/MapObjBall.cpp
+++ src/MoveBG/MapObjBall.cpp
@@ -1,1 +1,6 @@
-
+extern "C" {
+unsigned int mFruitLivingTime__11TResetFruit = 0x3840;
+float mScaleUpSpeed__11TResetFruit = 1.05f;
+float mBreakingScaleSpeed__11TResetFruit = 0.96f;
+unsigned int mFruitWaitTimeToAppear__11TResetFruit = 0x168;
+}
