f7bb03c40d919a5ac2520f265d69adc8ece55897
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 18:47:05 -0500

    worker-integration(job-2f45): mario/Player/WaterGun::calcGunAngle__11TNozzleBaseFRC20TMarioControllerWork [checkpoint d350bb16]

--- src/Player/WaterGun.cpp
+++ src/Player/WaterGun.cpp
@@ -224,10 +224,9 @@ void TNozzleBase::calcGunAngle(const TMarioControllerWork& work)
 
 	s16 angle;
 	if (mFludd->mMario->mStatus == MARIO_STATUS_SQUAT) {
-		// TODO: Wrong reguster used, using r3 instead of r4
-		angle = unk36E
-		        + (s16)(mFludd->mMario->mGamePad->mCompSPos[0 * 2 + 1]
-		                * mEmitParams.mRButtonMult.get());
+		angle = unk36E;
+		angle += (s16)(mFludd->mMario->mGamePad->mCompSPos[0 * 2 + 1]
+		               * mEmitParams.mRButtonMult.get());
 	} else {
 		angle = -mEmitParams.mLAngleBase.get();
 	}
