421afe75e88b3ece501bdd7170a00c98c991fc03
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 00:26:28 -0500

    worker-integration(job-d628): mario/M3DUtil/SDLModel::entrySameMat__12SDLModelDataFP11J3DMaterialP15SDLDrawBufToken [checkpoint 0aa8d755]

--- src/M3DUtil/SDLModel.cpp
+++ src/M3DUtil/SDLModel.cpp
@@ -71,8 +71,8 @@ void SDLModelData::entrySameMat(J3DMaterial* material, SDLDrawBufToken* token)
 			}
 			model = model->mNextSameMat;
 		}
-		token->unk0[material->isDrawModeOpaTexEdge() ? 0 : 1]->entryImm(
-		    matPacket, 0);
+		bool drawBuffer = material->isDrawModeOpaTexEdge();
+		token->unk0[drawBuffer ? 0 : 1]->entryImm(matPacket, 0);
 	}
 }
 
