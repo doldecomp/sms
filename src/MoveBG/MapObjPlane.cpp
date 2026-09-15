d38e7a78d96d9a3a02fdc9ad8fef38968387158d
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 15:39:10 -0500

    worker-integration(job-4cae): mario/MoveBG/MapObjPlane::draw__12TMapObjPlaneFv [checkpoint 18c39033]

--- src/MoveBG/MapObjPlane.cpp
+++ src/MoveBG/MapObjPlane.cpp
@@ -75,7 +75,8 @@ void TMapObjPlane::draw()
 	for (int z = 0; z < mExtents - 1; ++z) {
 		f32 fVar1 = unkFC;
 
-		f32 worldZ = mCollision->gridToWorld(z);
+		f32 worldZ     = mCollision->gridToWorld(z);
+		f32 nextWorldZ = worldZ + fVar1;
 
 		GXBegin(GX_TRIANGLESTRIP, GX_VTXFMT0, mExtents * 2);
 		for (int x = 0; x < mExtents; ++x) {
@@ -85,16 +86,16 @@ void TMapObjPlane::draw()
 			GXNormal3f32(normalAt(x, z).x, normalAt(x, z).y, normalAt(x, z).z);
 			GXTexCoord2f32(getTexPos(x), getTexPos(z));
 
-			GXPosition3f32(worldX, heightAt(x, z + 1), worldZ + fVar1);
+			GXPosition3f32(worldX, heightAt(x, z + 1), nextWorldZ);
 			GXNormal3f32(normalAt(x, z + 1).x, normalAt(x, z + 1).y,
 			             normalAt(x, z + 1).z);
-			GXPosition2f32(getTexPos(x), getTexPos(z + 1));
+			GXTexCoord2f32(getTexPos(x), getTexPos(z + 1));
 		}
 		GXEnd();
 	}
 }
 
-f32 TMapObjPlane::getTexPos(f32 v) const { return mTexScale * v; }
+f32 TMapObjPlane::getTexPos(f32 v) const { return v * mTexScale; }
 
 void TMapObjPlane::updateCheckData(int x, int z)
 {
