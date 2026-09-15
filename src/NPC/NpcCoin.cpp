1d561003acb0e24057e35d6968e12f9e14b6d365
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 14:48:21 -0500

    worker-integration(job-cf66): mario/NPC/NpcCoin::requestAppearCoin__8TNpcCoinFRC3Vecfi [checkpoint 807f1ce0]

--- src/NPC/NpcCoin.cpp
+++ src/NPC/NpcCoin.cpp
@@ -43,7 +43,7 @@ void TNpcCoin::requestAppearCoin(const Vec& param_1, f32 param_2, int param_3)
 {
 	unk4 = param_3;
 	unk8.set(param_1);
-	unk14.set(0.0f, JMASin(75), JMACos(75));
+	unk14.set(0.0f, JMASSin(0x3552), JMASCos(0x3552));
 	s16 sangle = CLBDegToShortAngle(param_2);
 	f32 x      = unk14.x;
 	unk14.x    = x * JMASCos(sangle) + unk14.z * JMASSin(sangle);
