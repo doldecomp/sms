6c2063085fada315d622944a3101e83360aac4b1
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 15:23:22 -0500

    worker-integration(job-f7d2): mario/MoveBG/MapObjPinna::.sdata [checkpoint 176c07c4]

--- src/MoveBG/MapObjPinna.cpp
+++ src/MoveBG/MapObjPinna.cpp
@@ -1,1 +1,18 @@
+class TShellCup {
+public:
+	static float mOpenRotMax;
+	static float mShellDamageRot;
+	static float mWaterOpenAccel;
+	static float mCloseAccel;
+};
 
+class TMerrygoround {
+public:
+	static float mRotSpeed;
+};
+
+float TShellCup::mOpenRotMax = 90.0f;
+float TShellCup::mShellDamageRot = 45.0f;
+float TShellCup::mWaterOpenAccel = 5.0f;
+float TShellCup::mCloseAccel = 3.5f;
+float TMerrygoround::mRotSpeed = 0.1f;
