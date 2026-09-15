1b68d211631943528d7886002fe902612456ee87
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 01:39:53 -0500

    worker-integration(job-206a): mario/MarioUtil/LightUtil::__ct__16TLightDrawBufferFiUlPCc [checkpoint 3234ff7b]

--- src/MarioUtil/LightUtil.cpp
+++ src/MarioUtil/LightUtil.cpp
@@ -193,10 +193,13 @@ TLightDrawBuffer::TLightDrawBuffer(int param_1, u32 param_2, const char* name)
     , unk80(param_1)
 {
 	snprintf(unk1C, 0x32, "%s%s", name, "opa");
-	mOpaDrawBufferObject = new JDrama::TDrawBufObj(3, param_2, unk1C);
+	JDrama::TDrawBufObj* drawBuffer
+	    = new JDrama::TDrawBufObj(3, param_2, unk1C);
+	mOpaDrawBufferObject = drawBuffer;
 
 	snprintf(unk4E, 0x32, "%s%s", name, "xlu");
-	mXluDrawBufferObject = new JDrama::TDrawBufObj(4, param_2, unk4E);
+	drawBuffer = new JDrama::TDrawBufObj(4, param_2, unk4E);
+	mXluDrawBufferObject = drawBuffer;
 }
 #pragma dont_inline reset
 
