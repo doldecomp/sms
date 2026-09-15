349873d3a565da96fbbd5907009bbe4370cd3912
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 16:29:51 -0500

    worker-integration(job-920d): mario/MoveBG/MapObjFence::.sdata [checkpoint e8a11e9a]

--- src/MoveBG/MapObjFence.cpp
+++ src/MoveBG/MapObjFence.cpp
@@ -1,1 +1,6 @@
-
+float mSpeed__20TRevolvingFenceInner = 4.0f;
+float mWaterAccel__11TFenceWater     = 2.1f;
+float mBackSpeed__11TFenceWater      = 3.0f;
+int mTurnedWaitTime__11TFenceWater   = 600;
+float mFallHeight__10TRailFence      = 50000.0f;
+int mWaitTime__10TRailFence          = 240;
