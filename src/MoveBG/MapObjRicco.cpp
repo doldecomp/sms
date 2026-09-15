4dc5225fa0f19491010cdb0b9432c60d2db23a34
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 15:14:03 -0500

    worker-integration(job-bfd8): mario/MoveBG/MapObjRicco::.sdata [checkpoint 3b9d0c73]

--- src/MoveBG/MapObjRicco.cpp
+++ src/MoveBG/MapObjRicco.cpp
@@ -1,1 +1,16 @@
+#include <MoveBG/MapObjRicco.hpp>
 
+unsigned long mWaitTime__10TCraneRotY = 120;
+float mRotSpeed__12TCraneUpDown = 0.1f;
+unsigned long mWaitTime__12TCraneUpDown = 120;
+float mRotAccel__15TRiccoWatermill = 1.0f;
+float mRotSpeedMaxUp__15TRiccoWatermill = 3.0f;
+float mRotSpeedMaxDown__15TRiccoWatermill = 1.0f;
+float mRotDown__15TRiccoWatermill = 0.05f;
+float mSubmarineMoveRate__15TRiccoWatermill = 0.5f;
+float mSubmarineMaxTransY__15TRiccoWatermill = 750.0f;
+float mSubmarineBottomTransY__15TRiccoWatermill = -950.0f;
+unsigned long mWaitTime__15TRiccoWatermill = 600;
+float mObjSpeedXZ__14TFruitLauncher = 1.0f;
+float mObjSpeedY__14TFruitLauncher = 20.0f;
+unsigned long mFruitLiveTime__14TFruitLauncher = 4800;
