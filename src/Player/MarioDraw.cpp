e0aee8f3083b95b5c2ba8b4fc3cbdefdeb6aa26e
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 12:48:18 -0500

    worker-integration(job-eec3): mario/Player/MarioDraw::MarioWaistCtrl__FP7J3DNodei [checkpoint 66c3d306]

--- src/Player/MarioDraw.cpp
+++ src/Player/MarioDraw.cpp
@@ -479,8 +479,9 @@ static int MarioWaistCtrl(J3DNode* param_1, int param_2)
 		    && gpCamera->mCurrentTarget.mPitch > 0) {
 			*unk       = gpCamera->mCurrentTarget.mPitch;
 			s16 unk100 = -unk[2];
+			s16 unkFC  = *unk;
 			MsMtxSetRotRPH(transform, SHORTANGLE2DEG(unk100), 0.0f,
-			               SHORTANGLE2DEG(*unk));
+			               SHORTANGLE2DEG(unkFC));
 			MTXConcat(J3DSys::mCurrentMtx, transform, J3DSys::mCurrentMtx);
 			return 1;
 		} else if (gpMarioForCallBack->checkStatusType(MARIO_FLAG_HAS_FLUDD)
@@ -495,12 +496,12 @@ static int MarioWaistCtrl(J3DNode* param_1, int param_2)
 				          J3DSys::mCurrentMtx);
 				return 1;
 			}
-		} else if (gpMarioForCallBack->mAnimationId == TMario::ANIM_RUN1
-		           || gpMarioForCallBack->mAnimationId == TMario::ANIM_RUN2
-		           || gpMarioForCallBack->mAnimationId
-		                      == TMario::ANIM_RIDE_SHELL
-		                  && !gpMarioForCallBack->checkStatusType(
-		                      MARIO_FLAG_FLUDD_EMITTING)) {
+		} else if ((gpMarioForCallBack->mAnimationId == TMario::ANIM_RUN1
+		            || gpMarioForCallBack->mAnimationId == TMario::ANIM_RUN2
+		            || gpMarioForCallBack->mAnimationId
+		                   == TMario::ANIM_RIDE_SHELL)
+		           && !gpMarioForCallBack->checkFlag(
+		               MARIO_FLAG_FLUDD_EMITTING)) {
 
 			// Ah, i love storing floats, casting them to s16
 			// and then transforming them to floats again...
