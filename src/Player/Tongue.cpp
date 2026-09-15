481dd7ec3ba45bf4b56f4a8d7b33891908091927
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 02:30:34 -0500

    worker-integration(job-7ae4): mario/Player/Tongue::canGo__12TYoshiTongueFv [checkpoint f5760167]

--- src/Player/Tongue.cpp
+++ src/Player/Tongue.cpp
@@ -126,8 +126,9 @@ BOOL TYoshiTongue::canGo()
 	if (toTip.dot(mHeadDir) < 0.0f)
 		return false;
 
-	if (gpMap->isTouchedOneWallAndMoveXZ(&mTipPos.x, 10.0f + mTipPos.y,
-	                                     &mTipPos.z, 50.0f))
+	if ((int)gpMap->isTouchedOneWallAndMoveXZ(
+	        &mTipPos.x, 10.0f + mTipPos.y, &mTipPos.z, 50.0f)
+	    > 0)
 		return false;
 
 	const TBGCheckData* ground;
