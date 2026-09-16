c7a504b40cf7f039185f83cd27173695dbdd0e63
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 23:53:03 -0500

    worker-integration(job-79c9): mario/Enemy/gatekeeper::init__17TBiancoGateKeeperFP12TLiveManager [checkpoint 37d814d9]

--- src/Enemy/gatekeeper.cpp
+++ src/Enemy/gatekeeper.cpp
@@ -206,7 +206,7 @@ void TBiancoGateKeeperManager::createModelData()
 }
 
 TBGKMtxCalc::TBGKMtxCalc(TBiancoGateKeeper* owner)
-    : M3UMtxCalcSIAnmBlendQuat(false)
+    : M3UMtxCalcSIAnmBlendQuat(true)
     , mOwner(owner)
 {
 }
@@ -374,7 +374,7 @@ void TBiancoGateKeeper::init(TLiveManager* manager)
 
 	MActorAnmData* anmData = mMActorKeeper->getMActorAnmData();
 	mMultiBtk              = new TMultiBtk(2, getModel()->getModelData());
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i <= 1; i++)
 		mMultiBtk->setNthData(i, anmData->getUnk38()->getAnmPtr(i));
 
 	mObstacle = new TBGKObstacle(this, "TBGKObstacle");
