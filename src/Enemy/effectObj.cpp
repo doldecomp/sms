1d42eb03e4bebe02995509e9a3e62aa14a726695
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 21:19:38 -0500

    worker-integration(job-046f): mario/Enemy/effectObj::reset__12TEffectModelFv [checkpoint 1896beb0]

--- src/Enemy/effectObj.cpp
+++ src/Enemy/effectObj.cpp
@@ -233,7 +233,7 @@ void TEffectModel::init(TLiveManager* param_1)
 void TEffectModel::reset()
 {
 	TSpineEnemy::reset();
-	mRotation.y = MsRandF(0.0f, 360.0f);
+	mRotation.y = TMsRange<f32>(0.0f, 360.0f).rand();
 	onLiveFlag(LIVE_FLAG_UNK8);
 	onLiveFlag(LIVE_FLAG_UNK10);
 	offLiveFlag(LIVE_FLAG_DEAD);
