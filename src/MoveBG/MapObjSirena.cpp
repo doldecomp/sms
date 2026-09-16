59d2499b51d4fcfef22afd93f0581296230a5633
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 23:42:47 -0500

    worker-integration(job-bde1): mario/MoveBG/MapObjSirena::getForcastResult__13TItemSlotDrumFi [checkpoint 8f60f946]

--- src/MoveBG/MapObjSirena.cpp
+++ src/MoveBG/MapObjSirena.cpp
@@ -639,7 +639,8 @@ int TItemSlotDrum::getForcastResult(int idx)
 				break;
 		}
 	}
-	return getResultFromAng((int)(angle / unk168) * unk168);
+	angle = unk168 * (int)(angle / unk168);
+	return getResultFromAng(angle);
 }
 
 int TItemSlotDrum::getResultFromAng(f32 ang)
