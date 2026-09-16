ebfe91446b22b6a60fc1eb97e6f19d43f90a4152
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 20:47:37 -0500

    worker-integration(job-702a): mario/Enemy/effectObj::perform__17TEffectObjManagerFUlPQ26JDrama9TGraphics [checkpoint 4db2d081]

--- src/Enemy/effectObj.cpp
+++ src/Enemy/effectObj.cpp
@@ -204,7 +204,8 @@ void TEffectObjManager::perform(u32 cue, JDrama::TGraphics* graphics)
 {
 	if (cue & (CUE_DRAW | CUE_MOVE)) {
 		JGadget::TList<TEffectObjBase*>::iterator it = unk14.begin();
-		for (; it != unk14.end(); ++it)
+		JGadget::TList<TEffectObjBase*>::iterator e  = unk14.end();
+		for (; it != e; ++it)
 			(*it)->perform(cue, graphics);
 
 		for (int i = 0; i < COOL_EFFECT_OBJ_NUM; ++i)
