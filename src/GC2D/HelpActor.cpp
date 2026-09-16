2d834d50b395b0dd19072419c7f006c9b3901219
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 19:30:03 -0500

    worker-integration(job-b9e9): mario/GC2D/HelpActor::perform__10THelpActorFUlPQ26JDrama9TGraphics [checkpoint e0a95cc9]

--- src/GC2D/HelpActor.cpp
+++ src/GC2D/HelpActor.cpp
@@ -50,12 +50,14 @@ void THelpActor::perform(u32 cue, JDrama::TGraphics*)
 {
 	if (cue & CUE_MOVE) {
 		if (unk74) {
-			if (getHelpID() == -1)
+			int helpID = getHelpID();
+			if (helpID == -1)
 				if (SMSGetMarDirector()->getConsole()->startDisappearBalloon(
 				        unk68, false))
 					unk74 = false;
 		} else {
-			if (getHelpID() != -1)
+			int helpID = getHelpID();
+			if (helpID != -1)
 				if (SMSGetMarDirector()->getConsole()->startAppearBalloon(
 				        unk68, false))
 					unk74 = true;
