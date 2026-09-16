af3afc25460df05b7aa5a53923f002abc80b3daf
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 22:08:39 -0500

    worker-integration(job-d01c): mario/Enemy/enemyAttachment::perform__25TEnemyPolluteModelManagerFUlPQ26JDrama9TGraphics [checkpoint a21147b7]

--- src/Enemy/enemyAttachment.cpp
+++ src/Enemy/enemyAttachment.cpp
@@ -197,10 +197,10 @@ void TEnemyPolluteModelManager::perform(u32 cue, JDrama::TGraphics* graphics)
 {
 	if (cue & CUE_CALC_ANIM) {
 		f32 f31 = 100.0f;
+		f32 farClip = gpConductor->getCondParams().mEnemyFarClip.get();
 		SetViewFrustumClipCheckPerspective(
 		    gpCamera->getFovy(), gpCamera->getAspect(),
-		    graphics->getNearPlane(),
-		    gpConductor->getCondParams().mEnemyFarClip.get());
+		    graphics->getNearPlane(), farClip);
 
 		for (int i = 0; i < unk14; ++i) {
 			if (unk18[i]->unk5D) {
