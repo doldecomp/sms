1f6a2216bfbfa4652fb84440575e24acc307f126
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-16 03:32:34 -0500

    worker-integration(job-caad): mario/Enemy/enemy::zigzagToCurPathNode__11TSpineEnemyFffff [checkpoint 2a3dab81]

--- src/Enemy/enemy.cpp
+++ src/Enemy/enemy.cpp
@@ -522,7 +522,7 @@ void TSpineEnemy::zigzagToCurPathNode(f32 march_speed, f32 turn_speed,
 		fVar3 = 0.0f;
 	} else {
 		f32 s = JMASin(turn_speed);
-		if (fVar3 == 0.0f) {
+		if (s == 0.0f) {
 			fVar3 = 100000.0f;
 		} else {
 			fVar3 = march_speed * JMASin(90.0f - turn_speed * 0.5f) / s;
