15d87915e5cd0380da5a595a4243f93df0f167bd
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 23:02:32 -0500

    worker-integration(job-04ba): mario/Enemy/killer::.sdata [checkpoint cb6fdfd7]

--- src/Enemy/killer.cpp
+++ src/Enemy/killer.cpp
@@ -1,5 +1,24 @@
 #include <Strategic/ObjManager.hpp>
 
+class TKiller {
+public:
+	static unsigned char mSerialBomb;
+	static unsigned char mTrampleDie;
+};
+
+class TFlyEnemy {
+public:
+	static float mTestSp;
+	static int mInvalidTime;
+	static float mTestMarioSpMax;
+};
+
+unsigned char TKiller::mSerialBomb = 1;
+unsigned char TKiller::mTrampleDie = 1;
+float TFlyEnemy::mTestSp = 2.5f;
+int TFlyEnemy::mInvalidTime = 200;
+float TFlyEnemy::mTestMarioSpMax = 12.0f;
+
 static const int unk1490[] = { 0, 0, 0 };
 static const char unk2322[] = "メモリが足りません\n";
 
