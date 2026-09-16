2780aa77817d62f4fd7c2c0cde8d4bbbfb57d7f0
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 20:35:32 -0500

    worker-integration(job-dbd9): mario/Map/MapWarp::init__8TMapWarpFR20JSUMemoryInputStream [checkpoint a4eda748]

--- src/Map/MapWarp.cpp
+++ src/Map/MapWarp.cpp
@@ -86,13 +86,8 @@ void TMapWarp::initModel()
 			    ->sleep();
 }
 
-void getWarpPointNo(const char*) { }
-
-void loadWarpPointPos(JSUMemoryInputStream&, int, Vec*) { }
-
-void TMapWarp::init(JSUMemoryInputStream& stream)
+int getWarpPointNo(const char* name)
 {
-	// Fabricated
 	struct NameTableEntry {
 		const char* unk0;
 		u32 unk4;
@@ -102,45 +97,51 @@ void TMapWarp::init(JSUMemoryInputStream& stream)
 		{ "warpC1", 4 },  { "warpC0", 5 },  { "warpD1", 6 },  { "warpD0", 7 },
 		{ "warpE1", 8 },  { "warpE0", 9 },  { "warpF1", 10 }, { "warpF0", 11 },
 		{ "warpG1", 12 }, { "warpG0", 13 }, { "warpH1", 14 }, { "warpH0", 15 },
-		{ "warpI1", 16 }, { "warpI0", 17 }, { nullptr, 0 },
+		{ "warpI1", 16 }, { "warpI0", 17 }, { 0, 0 },
 	};
 
-	unk0 = stream.readU32();
+	int i = 0;
+	while (strcmp(point_name_table[i].unk0, name) != 0)
+		++i;
+	return point_name_table[i].unk4;
+}
+
+void loadWarpPointPos(JSUMemoryInputStream& stream, int count, Vec* points)
+{
+	for (int i = 0; i < count; ++i) {
+		Vec& point = points[getWarpPointNo(stream.readString())];
+		stream >> point.x >> point.y >> point.z;
+
+		u32 dummy;
+		stream >> dummy >> dummy >> dummy;
+		stream >> dummy >> dummy >> dummy;
+	}
+}
+
+void TMapWarp::init(JSUMemoryInputStream& stream)
+{
+	u32 value;
+	stream >> value;
+	unk0 = value;
 	if (!unk0)
 		return;
 
-	unk8 = stream.readU32();
+	stream >> value;
+	unk8 = value;
 	unk4 = new TMapWarpInfo[unk0 * 2];
 
-	u32 local_1d0[20];
-	u32 local_180[20];
 	JGeometry::TVec3<f32> local_130[20];
+	u32 local_180[20];
+	u32 local_1d0[20];
 
 	for (int i = 0; i < unk0; ++i) {
-		local_1d0[i] = stream.readU32();
-		local_180[i] = stream.readU32();
+		stream >> value;
+		local_1d0[i] = value;
+		stream >> value;
+		local_180[i] = value;
 	}
 
-	int cnt = unk0 * 2;
-	for (int i = 0; i < cnt; ++i) {
-		const char* str = stream.readString();
-		u32 needle      = 0;
-		while (strcmp(point_name_table[needle].unk0, str) != 0)
-			++needle;
-
-		u32 idx = point_name_table[needle].unk4;
-		stream >> local_130[idx].x;
-		stream >> local_130[idx].y;
-		stream >> local_130[idx].z;
-
-		u32 dummy;
-		stream >> dummy;
-		stream >> dummy;
-		stream >> dummy;
-		stream >> dummy;
-		stream >> dummy;
-		stream >> dummy;
-	}
+	loadWarpPointPos(stream, unk0 * 2, local_130);
 
 	for (int i = 0; i < unk0; ++i) {
 		unk4[2 * i].unk8.x = local_130[2 * i].x - local_130[2 * i + 1].x;
