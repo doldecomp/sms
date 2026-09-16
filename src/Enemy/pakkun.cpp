45d3b47606956288032f322dabdad3404c6aacac
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 21:05:05 -0500

    worker-integration(job-2aa0): mario/Enemy/pakkun::.sdata [checkpoint cf3450d7]

--- src/Enemy/pakkun.cpp
+++ src/Enemy/pakkun.cpp
@@ -1,1 +1,12 @@
+#include <Enemy/SmallEnemy.hpp>
 
+class TPakkunManager : public TSmallEnemyManager {
+public:
+	static float mRootExplosionScaleRate;
+	static float mTestFlyAngX;
+	static float mIgnoreHitWaterY;
+};
+
+float TPakkunManager::mRootExplosionScaleRate = 2.0f;
+float TPakkunManager::mTestFlyAngX             = 30.0f;
+float TPakkunManager::mIgnoreHitWaterY         = 50.0f;
