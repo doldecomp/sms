7cc93a746cb7288c415dfe0cf6a3969caf92b140
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-16 07:47:05 -0500

    worker-integration(job-f69d): mario/Enemy/bossManta::createEnemies__17TBossMantaManagerFi [checkpoint e2f3f1a6]

--- src/Enemy/bossManta.cpp
+++ src/Enemy/bossManta.cpp
@@ -1309,7 +1309,7 @@ void TBossMantaManager::createEnemies(int num)
 		num = getCapacity() - getObjNum();
 
 	if (unk38 != nullptr) {
-		u8 limit = unk38->mSLActiveEnemyNum.get();
+		u8 limit = unk38->mSLInstanceNum.get();
 		if (num + getObjNum() > limit)
 			num = limit - getObjNum();
 	}
