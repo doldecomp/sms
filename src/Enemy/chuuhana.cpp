1fe031023a45485a370946551f248833bdbeaba6
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 21:49:49 -0500

    worker-integration(job-3a3e): mario/Enemy/chuuhana::.sdata [checkpoint 20cb485c]

--- src/Enemy/chuuhana.cpp
+++ src/Enemy/chuuhana.cpp
@@ -1,1 +1,28 @@
+class TChuuHana {
+public:
+	static unsigned long mCheckOnPanelTimeRoll;
+	static unsigned long mCheckOnPanelTime;
+	static unsigned char mBodyJntIndex;
+	static unsigned char mEyeJntIndex;
+	static unsigned char mFootJntIndex;
+	static unsigned char mNewSw;
+	static unsigned char mCompareHeight;
+	static float mSmallMirrorR;
+	static float mMediumMirrorR;
+	static float mLargeMirrorR;
+	static unsigned char mAttackVersion;
+	static unsigned char mDamageSw;
+};
 
+unsigned long TChuuHana::mCheckOnPanelTimeRoll = 20;
+unsigned long TChuuHana::mCheckOnPanelTime = 400;
+unsigned char TChuuHana::mBodyJntIndex = 1;
+unsigned char TChuuHana::mEyeJntIndex = 12;
+unsigned char TChuuHana::mFootJntIndex = 5;
+unsigned char TChuuHana::mNewSw = 1;
+unsigned char TChuuHana::mCompareHeight = 1;
+float TChuuHana::mSmallMirrorR = 650.0f;
+float TChuuHana::mMediumMirrorR = 900.0f;
+float TChuuHana::mLargeMirrorR = 1100.0f;
+unsigned char TChuuHana::mAttackVersion = 1;
+unsigned char TChuuHana::mDamageSw = 1;
