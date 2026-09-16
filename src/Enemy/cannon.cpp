722ce94d55c8deb340975c24224dd027d3f030ba
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 22:03:22 -0500

    worker-integration(job-bb51): mario/Enemy/cannon::.sdata [checkpoint ea180a5d]

--- src/Enemy/cannon.cpp
+++ src/Enemy/cannon.cpp
@@ -1,1 +1,4 @@
-
+unsigned char mChorobeiJntIdx__7TCannon     = 4;
+unsigned char mChorobeiHandJntIdx__7TCannon = 4;
+float mVelocityRate__7TCannon                = 0.62f;
+float mSearchRate__7TCannon                  = 0.02f;
