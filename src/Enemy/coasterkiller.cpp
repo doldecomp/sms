1570ad85bd203f2624118e7ba11b734199c144fc
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-16 00:23:42 -0500

    worker-integration(job-b549): mario/Enemy/coasterkiller::moveCoaster__13TCoasterEnemyFv [checkpoint 77f48555]

--- src/Enemy/coasterkiller.cpp
+++ src/Enemy/coasterkiller.cpp
@@ -104,33 +104,32 @@ void TCoasterEnemy::moveCoaster()
 	JGeometry::TVec3<f32> forward;
 	mQuat.getZDir(forward);
 
-	JGeometry::TVec3<f32> axis;
-	axis.cross(forward, delta);
-
 	JGeometry::TVec3<f32> up;
 	mQuat.getYDir(up);
 
 	JGeometry::TQuat4<f32> steer;
-	steer.setRotate(forward, axis, 0.1f);
+	{
+		JGeometry::TVec3<f32> axis;
+		axis.set(delta);
+		steer.setRotate(forward, axis, 0.1f);
+	}
 	mQuat.mul(steer);
 
 	// Y-axis rotation
-	JGeometry::TVec3<f32> right;
-	right.cross(forward, JGeometry::TVec3<f32>(0.0f, 1.0f, 0.0f));
-	if (right.length() > 0.0f) {
-		right.normalize();
+	JGeometry::TVec3<f32> axis;
+	axis.cross(forward, JGeometry::TVec3<f32>(0.0f, 1.0f, 0.0f));
+	if (axis.length() > 0.0f) {
+		axis.normalize();
 
-		JGeometry::TQuat4<f32> tiltQuat;
-		tiltQuat.setRotate(right, M_PI / 2.0f);
+		JGeometry::TQuat4<f32> tilt;
+		tilt.setRotate(axis, M_PI / 2.0f);
+		tilt.rotate(forward, axis);
 
-		JGeometry::TVec3<f32> curUp;
-		tiltQuat.rotate(forward, curUp);
-
-		steer.setRotate(up, curUp, 0.1f);
+		steer.setRotate(up, axis, 0.1f);
 		mQuat.mul(steer);
 	}
 
-	static_cast<JGeometry::TVec4<f32>&>(mQuat).normalize();
+	mQuat.normalize();
 }
 
 void TCoasterEnemy::calcRootMatrix()
