4691bcc690ac73d8c245d8f136d32bb2c5c1e109
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 21:39:01 -0500

    worker-integration(job-0771): mario/Enemy/tobiPuku::.sdata [checkpoint cb56e5f7]

--- src/Enemy/tobiPuku.cpp
+++ src/Enemy/tobiPuku.cpp
@@ -1,1 +1,12 @@
+class TTobiPuku {
+public:
+	static float mLandAngle;
+	static bool mBoundSw;
+	static float mBoundVelocityY;
+	static bool mReturnLaunchSw;
+};
 
+float TTobiPuku::mLandAngle = 90.0f;
+bool TTobiPuku::mBoundSw = true;
+float TTobiPuku::mBoundVelocityY = 0.8f;
+bool TTobiPuku::mReturnLaunchSw = true;
