1d1fd013736d2dbf7183e83251e1a82baf833c89
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-16 00:53:12 -0500

    worker-integration(job-72e8): mario/Enemy/gesso::rebirth__16TGessoPolluteObjFv [checkpoint fa9e49e4]

--- src/Enemy/gesso.cpp
+++ src/Enemy/gesso.cpp
@@ -940,8 +940,11 @@ void TGessoPolluteObj::rebirth()
 
 	if (unk158 == 10) {
 		mVelocity.y = -15.0f;
-		gpPollution->stamp(1, mPosition.x, mPosition.y, mPosition.z,
-		                   TGesso::mPollRange * 32.0f * 0.5f);
+		f32 range = 0.5f * (TGesso::mPollRange * 32.0f);
+		f32 z = mPosition.z;
+		f32 y = mPosition.y;
+		f32 x = mPosition.x;
+		gpPollution->stamp(1, x, y, z, range);
 
 		((TGesso*)unk160)
 		    ->getManager()
