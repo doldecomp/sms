ac5749d07d1c89fad980f62807b78a0bf16171b9
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 21:27:16 -0500

    worker-integration(job-a99c): mario/Player/MarioParticle::kickFruitEffect__6TMarioFv [checkpoint 3ebbff82]

--- src/Player/MarioParticle.cpp
+++ src/Player/MarioParticle.cpp
@@ -771,7 +771,7 @@ void TMario::kickFruitEffect()
 		emitter->setGlobalScale(scale);
 		JGeometry::TVec3<f32> pos = mPosition;
 		pos.y += 30.0f;
-		emitter->setEmitterTranslation(pos);
+		emitter->setGlobalTranslation(pos);
 	}
 }
 
