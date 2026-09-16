7c66759ddfdec7bf9a0f37d1fc59f2697c23f65f
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 23:43:11 -0500

    worker-integration(job-2942): mario/Enemy/wireBinder::isEndWire__11TWireBinderCFRCQ29JGeometry8TVec3<f>f [checkpoint a7fce26b]

--- src/Enemy/wireBinder.cpp
+++ src/Enemy/wireBinder.cpp
@@ -109,8 +109,9 @@ bool TWireBinder::isEndWire(const JGeometry::TVec3<f32>& param_1,
 {
 	f32 posInWire = getRangePos(param_1);
 	f32 targetPos = 0.0f < param_2 ? 1.0f : 0.0f;
+	f32 fVar = posInWire - targetPos;
 
-	return fabsf(posInWire - targetPos) < 0.015f;
+	return fabsf(fVar) < 0.015f;
 }
 
 void TWireBinder::getStartRangePos(f32) { }
