6a522e9b295556f9a29936ad0f08784c1c4cf7cd
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-16 01:53:14 -0500

    worker-integration(job-92e3): mario/Enemy/hamukuri::makeCapFly__9THamuKuriFP11TMapObjBase [checkpoint e9644c16]

--- src/Enemy/hamukuri.cpp
+++ src/Enemy/hamukuri.cpp
@@ -940,52 +940,60 @@ void THamuKuri::makeCapFly(TMapObjBase* param_1)
 
 	// TODO: control flow all over the place, definitely inlines needed
 
-	TSmallEnemy* holder = getManager()->getHolder(mInstanceIndex);
-	if (holder == nullptr && mGroundPlane->checkFlag(BG_CHECK_FLAG_ILLEGAL)
-	    && mGroundPlane->isPool() && mGroundPlane->isWaterSurface()) {
-		mPosition = SMS_GetMarioPos();
-		mVelocity.set(0.0f, 10.0f, 0.0f);
-		offLiveFlag(LIVE_FLAG_UNK10);
-	} else {
-		mPosition = local_3c;
-		mPosition.y += 100.0f;
-		param_1->mPosition.y = mPosition.y;
-
-		if (param_1->receiveMessage(this, HIT_MESSAGE_TAKE)) {
-			onLiveFlag(LIVE_FLAG_DEAD);
+	THamuKuri* holder
+	    = (THamuKuri*)getManager()->getHolder(mInstanceIndex);
+	if (holder == nullptr) {
+		if (!mGroundPlane->checkFlag(BG_CHECK_FLAG_ILLEGAL)
+		    && !mGroundPlane->isPool() && !mGroundPlane->isWaterSurface()) {
+			holder = this;
 		} else {
-			reset();
-			onHaveCap();
-			mHeldObject = param_1;
-			onLiveFlag(LIVE_FLAG_HIDDEN);
-			offLiveFlag(LIVE_FLAG_DEAD);
-			offLiveFlag(LIVE_FLAG_CLIPPED_OUT);
-			onHitFlag(HIT_FLAG_NO_COLLISION);
-			getManager()->unk70 = this;
-
-			// TODO: this is an inline
-			int uVar11 = unk124->getCurGraphIndex();
-
-			int count  = MsRandF(2, 3);
-			int uVar10 = -1;
-			for (int i = 0; i < count; ++i) {
-				int next = unk124->unk0->getRandomNextIndex(uVar11, uVar10,
-				                                            0xffffffff);
-				uVar10   = uVar11;
-				uVar11   = next;
-			}
-
-			if (uVar11 < 0)
-				uVar11 = 0;
-
-			JGeometry::TVec3<f32> VStack_60;
-			unk124->getGraph()->getGraphNode(uVar11).getPoint(&VStack_60);
-
-			JGeometry::TVec3<f32> local_6c
-			    = calcVelocityToJumpToY(VStack_60, mCapSpeed, getGravityY());
-			onLiveFlag(LIVE_FLAG_AIRBORNE);
-			mVelocity = local_6c;
+			param_1->mPosition = SMS_GetMarioPos();
+			param_1->mVelocity.set(0.0f, 10.0f, 0.0f);
+			param_1->offLiveFlag(LIVE_FLAG_UNK10);
+			return;
 		}
+	}
+
+	holder->mPosition = local_3c;
+	holder->mPosition.y += 100.0f;
+	param_1->mPosition.y = holder->mPosition.y;
+
+	if (param_1->receiveMessage(holder, HIT_MESSAGE_TAKE)) {
+		holder->reset();
+		holder->onHaveCap();
+		holder->mHeldObject = param_1;
+		int uVar11;
+		int uVar10 = -1;
+		holder->onLiveFlag(LIVE_FLAG_HIDDEN);
+		holder->offLiveFlag(LIVE_FLAG_DEAD);
+		holder->offLiveFlag(LIVE_FLAG_CLIPPED_OUT);
+		holder->onHitFlag(HIT_FLAG_NO_COLLISION);
+		getManager()->unk70 = holder;
+
+		// TODO: this is an inline
+		uVar11 = unk124->getCurGraphIndex();
+
+		TMsRange<s32> range(2, 3);
+		int count = range.rand();
+		for (int i = 0; i < count; ++i) {
+			int next = unk124->unk0->getRandomNextIndex(uVar11, uVar10,
+			                                            0xffffffff);
+			uVar10   = uVar11;
+			uVar11   = next;
+		}
+
+		if (uVar11 < 0)
+			uVar11 = 0;
+
+		JGeometry::TVec3<f32> VStack_60;
+		unk124->getGraph()->getGraphNode(uVar11).getPoint(&VStack_60);
+
+		JGeometry::TVec3<f32> local_6c
+		    = calcVelocityToJumpToY(VStack_60, mCapSpeed, getGravityY());
+		holder->onLiveFlag(LIVE_FLAG_AIRBORNE);
+		holder->mVelocity = local_6c;
+	} else {
+		holder->onLiveFlag(LIVE_FLAG_DEAD);
 	}
 }
 
