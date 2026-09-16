395d0f3e72c9a18c21830b459b736930f05af07d
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-15 19:43:53 -0500

    worker-integration(job-ee7b): mario/GC2D/PauseMenu2::load__11TPauseMenu2FR20JSUMemoryInputStream [checkpoint 81f29b15]

--- src/GC2D/PauseMenu2.cpp
+++ src/GC2D/PauseMenu2.cpp
@@ -85,14 +85,14 @@ void TPauseMenu2::load(JSUMemoryInputStream& pStream)
 	mMenuPane   = mScreen->search('t_0');
 
 	for (s32 i = 0; i < 5; i++) {
-		mPauseLetters[i] = (J2DPicture*)mScreen->search('t_0' + i);
+		mPauseLetters[i] = (J2DPicture*)mScreen->search('pa00' + i);
 	}
 
 	for (s32 i = 0; i < 3; i++) {
 		mMenuItems[i] = (J2DPicture*)mScreen->search('tx_1' + i);
 
 		if (mNumItems == 2) {
-			mMenuItems[i]->add(0, 14);
+			mMenuItems[i]->add(0, 20);
 		}
 		mMenuItems[i]->mVisible = false;
 	}
