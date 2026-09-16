ccc77c8f101943e9d3670569ee6aac23fee2ac42
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-16 04:32:28 -0500

    worker-integration(job-8e5d): mario/Enemy/bosseel::moveObject__8TOilBallFv [checkpoint b2913b4b]

--- src/Enemy/bosseel.cpp
+++ src/Enemy/bosseel.cpp
@@ -631,12 +631,11 @@ void TOilBall::reset()
 
 void TOilBall::moveObject()
 {
-	TBEelTearsSaveLoadParams* params = mTearsParams;
-	f32 scale                        = mScaling.x;
-	s32 attackRadius                 = params->mSLTearsAttackRadius.get();
-	s32 attackHeight                 = params->mSLTearsAttackHeight.get();
-	s32 damageRadius                 = params->mSLTearsDamageRadius.get();
-	s32 damageHeight                 = params->mSLTearsDamageHeight.get();
+	f32 scale        = mScaling.x;
+	f32 attackRadius = mTearsParams->mSLTearsAttackRadius.get();
+	f32 attackHeight = mTearsParams->mSLTearsAttackHeight.get();
+	f32 damageRadius = mTearsParams->mSLTearsDamageRadius.get();
+	f32 damageHeight = mTearsParams->mSLTearsDamageHeight.get();
 	setHitParams(attackRadius * scale, attackHeight * scale,
 	             damageRadius * scale, damageHeight * scale);
 
@@ -651,13 +650,14 @@ void TOilBall::moveObject()
 			}
 		} else {
 			JGeometry::TVec3<f32> velocity(0.0f, 0.0f, 0.0f);
-			JGeometry::TVec3<f32> push;
-			push.sub(mPosition, actor->mPosition);
+			JGeometry::TVec3<f32> push(mPosition.x - actor->mPosition.x,
+			                           mPosition.y - actor->mPosition.y,
+			                           mPosition.z - actor->mPosition.z);
 			if (push.x == 0.0f && push.y == 0.0f && push.z == 0.0f)
 				push.x += 1.0f;
 			MsVECNormalize(&push, &push);
-			push.scale(5.0f);
-			velocity.add(push);
+			push *= 5.0f;
+			velocity += push;
 			mLinearVelocity = velocity;
 		}
 	}
