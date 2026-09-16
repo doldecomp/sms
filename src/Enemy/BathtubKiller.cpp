94ffdb1d5c46052998dafc53ff248bae95e9fa31
ford-bubba-ai <ford.lascari@gmail.com>
2026-09-16 07:11:25 -0500

    worker-integration(job-7aa7): mario/Enemy/BathtubKiller::execute__24TNerveBathtubKillerChaseCFP24TSpineBase<10TLiveActor> [checkpoint 524d2bd3]

--- src/Enemy/BathtubKiller.cpp
+++ src/Enemy/BathtubKiller.cpp
@@ -606,7 +606,33 @@ DEFINE_NERVE(TNerveBathtubKillerWander, TLiveActor)
 	return FALSE;
 }
 
-DEFINE_NERVE(TNerveBathtubKillerChase, TLiveActor) { return FALSE; }
+DEFINE_NERVE(TNerveBathtubKillerChase, TLiveActor)
+{
+	TBathtubKiller* self = (TBathtubKiller*)spine->getBody();
+
+	if (spine->getTime() == 0) {
+		self->mMActor = self->mMActorKeeper->getMActor(
+		    "bathtubdownkiller_model1.bmd");
+		self->setBckAnm(1);
+	}
+
+	if (!self->unk1CC->isKillerAttackable()) {
+		spine->pushNerve(&TNerveBathtubKillerStraight::theNerve());
+		return TRUE;
+	}
+
+	if (self->isAboided()) {
+		if (self->unk194 == 1)
+			spine->pushNerve(
+			    &TNerveBathtubKillerChaseStraight::theNerve());
+		else
+			spine->pushNerve(&TNerveBathtubKillerStraight::theNerve());
+		return TRUE;
+	}
+
+	self->moveChasing();
+	return FALSE;
+}
 
 DEFINE_NERVE(TNerveBathtubKillerChaseStraight, TLiveActor) { return FALSE; }
 
