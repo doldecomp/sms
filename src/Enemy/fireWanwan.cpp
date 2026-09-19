e57975d939fbc21bea483bcd398117edda8569b0
Astrr00 <niem.conner@gmail.com>
2026-09-19 20:42:00 +0200

    Match Enemy/fireWanwan.cpp: TFireWanwan::attackToMario
    Bug fix: nerve exclusion check compared against
    TNerveFireWanwanRecover::theNerve() instead of
    TNerveFireWanwanRecoverGraph::theNerve(). Confirmed against raw
    retail disassembly. match_percent 100.0.

--- src/Enemy/fireWanwan.cpp
+++ src/Enemy/fireWanwan.cpp
@@ -1527,7 +1527,7 @@ void TFireWanwan::attackToMario()
 	const TNerveBase<TLiveActor>* nerve = mSpine->getLatestNerve();
 	if (nerve != &TNerveFireWanwanFly::theNerve()
 	    && nerve != &TNerveFireWanwanEscape::theNerve()
-	    && nerve != &TNerveFireWanwanRecover::theNerve()) {
+	    && nerve != &TNerveFireWanwanRecoverGraph::theNerve()) {
 		if (isFreeze()) {
 			(void)nerve;
 		} else {
