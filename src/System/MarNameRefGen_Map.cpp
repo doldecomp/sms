f08dab2d438eec29ae824087a2173762a8fd54dd
Private Contributor <redacted@example.invalid>
2026-09-19 03:14:03 -0400

    System/MarNameRefGen_Map: pass name tags, add SunModel and PollutionTest
    getNameRef_Map 72.5% -> 91.8%. Every new passes the tag retail passes: TMapマップ, TMapDrawWallカメラ食い込み壁, TSky空, TShimmer<Shimmer>, TPollutionManager落書き管理, TPollutionTest落書きテスト, TSunMgr<TSunMgr>, TSunModel(false/true, <TSunModel>), TMarineSnowMarineSnow, TStickyStainManager<TViewObj>. Residual is the TNameRef inline chain in the two placement-new branches: retail calls the out-of-line ctor, ours inlines the header version. JSystem header call, left for a human.

--- src/System/MarNameRefGen_Map.cpp
+++ src/System/MarNameRefGen_Map.cpp
@@ -1,9 +1,11 @@
 
 #include "Camera/SunMgr.hpp"
+#include "Camera/SunModel.hpp"
 #include "Map/BathWaterManager.hpp"
 #include "Map/Map.hpp"
 #include "Map/MapDraw.hpp"
 #include "Map/MarineSnow.hpp"
+#include "Map/PollutionEvent.hpp"
 #include "Map/PollutionManager.hpp"
 #include "Map/Shimmer.hpp"
 #include "Map/Sky.hpp"
@@ -16,40 +18,37 @@
 JDrama::TNameRef* TMarNameRefGen::getNameRef_Map(const char* name) const
 {
 	if (strcmp(name, "Map") == 0)
-		return new TMap;
+		return new TMap("マップ");
 
 	if (strcmp(name, "MapDrawWall") == 0)
-		return new TMapDrawWall;
+		return new TMapDrawWall("カメラ食い込み壁");
 
 	if (strcmp(name, "Sky") == 0)
-		return new TSky;
+		return new TSky("空");
 
 	if (strcmp(name, "Shimmer") == 0)
-		return new TShimmer;
+		return new TShimmer("<Shimmer>");
 
 	if (strcmp(name, "Pollution") == 0)
-		return new TPollutionManager;
+		return new TPollutionManager("落書き管理");
 
-	// TODO:
-	// if ( strcmp(name, "PollutionTest" ) == 0 )
-	//     return new TPollutionTest("落書きテスト");
+	if (strcmp(name, "PollutionTest") == 0)
+		return new TPollutionTest("落書きテスト");
 
 	if (strcmp(name, "SunMgr") == 0)
-		return new TSunMgr;
+		return new TSunMgr("<TSunMgr>");
 
-	// TODO:
-	// if ( strcmp(name, "SunModel" ) == 0 )
-	//     return new TSunModel(false, "<TSunModel>");
+	if (strcmp(name, "SunModel") == 0)
+		return new TSunModel(false, "<TSunModel>");
 
-	// TODO:
-	// if ( strcmp(name, "SunsetModel" ) == 0 )
-	//     return new TSunModel(true, "<TSunModel>");
+	if (strcmp(name, "SunsetModel") == 0)
+		return new TSunModel(true, "<TSunModel>");
 
 	if (strcmp(name, "MarineSnow") == 0)
-		return new TMarineSnow;
+		return new TMarineSnow("MarineSnow");
 
 	if (strcmp(name, "StickyStain") == 0)
-		return new TStickyStainManager;
+		return new TStickyStainManager("<TViewObj>");
 
 	if (strcmp(name, "BathWater") == 0)
 		return new TBathWaterManager;
