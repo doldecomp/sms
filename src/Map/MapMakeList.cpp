6b801674f11615febed2313d42502572d198459a
Astrr00 <niem.conner@gmail.com>
2026-09-20 09:51:44 +0200

    match: TMapCollisionData::removeCheckListData stack padding

--- src/Map/MapMakeList.cpp
+++ src/Map/MapMakeList.cpp
@@ -319,6 +319,7 @@ void printList(const TBGCheckList*) { }
 
 void TMapCollisionData::removeCheckListData(u16 start, s32 count)
 {
+	char trash[0x28];
 	TBGCheckListWarp* curr;
 	int rangeEnd;
 	u32 rangeStart;
