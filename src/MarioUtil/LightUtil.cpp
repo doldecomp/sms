a5d1b35bfb8b8b368738de46c525473f0aac8559
Astrr00 <niem.conner@gmail.com>
2026-09-20 08:52:14 +0200

    match: TObjectLightWithDBSet::makeDrawBuffer byte-exact

--- src/MarioUtil/LightUtil.cpp
+++ src/MarioUtil/LightUtil.cpp
@@ -323,6 +323,7 @@ void TPlayerLightWithDBSet::makeDrawBuffer()
 
 void TObjectLightWithDBSet::makeDrawBuffer()
 {
+	char trash[32];
 	static const char lightName[] = "太陽（オブジェクト）";
 	static const char ambName[]   = "太陽アンビエント（オブジェクト）";
 
