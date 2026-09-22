4c15f670f034bd40d94511b7c6a408291871e3b7
chasem-dev <chasecool56@gmail.com>
2026-09-22 10:05:18 -0400

    Restore MapWire draw offset temporaries
    Claude-Session: Codex

--- src/Map/MapWire.cpp
+++ src/Map/MapWire.cpp
@@ -40,8 +40,10 @@ f32 TMapWire::mDrawHeight     = 6.0f;
 
 void TMapWire::drawLower() const
 {
-	f32 xOffset = mDrawAxes.x * mDrawWidth;
-	f32 zOffset = mDrawAxes.y * mDrawWidth;
+	f32 xOffset = mDrawAxes.x;
+	xOffset *= mDrawWidth;
+	f32 zOffset = mDrawAxes.y;
+	zOffset *= mDrawWidth;
 
 	GXBegin(GX_TRIANGLESTRIP, GX_VTXFMT0, (mNumActiveMapWirePoints + 2) * 2);
 
@@ -86,8 +88,10 @@ void TMapWire::drawLower() const
 
 void TMapWire::drawUpper() const
 {
-	f32 xOffset = mDrawAxes.x * mDrawWidth;
-	f32 zOffset = mDrawAxes.y * mDrawWidth;
+	f32 xOffset = mDrawAxes.x;
+	xOffset *= mDrawWidth;
+	f32 zOffset = mDrawAxes.y;
+	zOffset *= mDrawWidth;
 
 	GXBegin(GX_TRIANGLESTRIP, GX_VTXFMT0, (mNumActiveMapWirePoints + 2) * 2);
 
